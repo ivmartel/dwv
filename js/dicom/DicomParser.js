271e30ec99e94c2fc9d3c6468af29700f77a812f
ivmartel <ymartelli@cistib.upf.edu>
2011-10-05 18:13:03 +0200

    Dynamically set the image size.

--- js/dicom/DicomParser.js
+++ js/dicom/DicomParser.js
@@ -11,6 +11,8 @@ function DicomParser(inputBuffer,reader)
     this.dicomElement;
     this.pixelBuffer;
     this.pixelDataOffset;
+    this.numberOfRows;
+    this.numberOfColumns;
     // methods
     this.parseAll=parseAll;
 }
@@ -25,16 +27,18 @@ function parseAll()
 {
     var index = 0;
     // read tags
-    index=this.readTag(index, 8,0,96, 0,"modality");         // 0x0008, 0x0060
-    index=this.readTag(index,16,0,16, 0,"patientName");      // 0x0010, 0x0010
-    index=this.readTag(index,16,0,32, 0,"patientId");        // 0x0010, 0x0020
-    index=this.readTag(index,40,0,16, 0,"numberOfRows");     // 0x0028, 0x0010
-    index=this.readTag(index,40,0,17, 0,"numberOfColumns");  // 0x0028, 0x0011
-    index=this.readTag(index,40,0,48, 0,"pixelSpacing");     // 0x0028, 0x0030
-    index=this.readTag(index,40,0,80,16,"windowCenter");     // 0x0028, 0x1050
-    index=this.readTag(index,40,0,81,16,"windowWidth");      // 0x0028, 0x1051
-    index=this.readTag(index,40,0,82,16,"rescaleIntercept"); // 0x0028, 0x1052
-    index=this.readTag(index,40,0,83,16,"rescaleSlope");     // 0x0028, 0x1053
+    index = this.readTag(index, 8,0,96, 0,"modality");         // 0x0008, 0x0060
+    index = this.readTag(index,16,0,16, 0,"patientName");      // 0x0010, 0x0010
+    index = this.readTag(index,16,0,32, 0,"patientId");        // 0x0010, 0x0020
+    index = this.readTag(index,40,0,16, 0,"numberOfRows");     // 0x0028, 0x0010
+    this.numberOfRows = this.dicomElement[elementIndex-1].value[0];
+    index = this.readTag(index,40,0,17, 0,"numberOfColumns");  // 0x0028, 0x0011
+    this.numberOfColumns = this.dicomElement[elementIndex-1].value[0];
+    index = this.readTag(index,40,0,48, 0,"pixelSpacing");     // 0x0028, 0x0030
+    index = this.readTag(index,40,0,80,16,"windowCenter");     // 0x0028, 0x1050
+    index = this.readTag(index,40,0,81,16,"windowWidth");      // 0x0028, 0x1051
+    index = this.readTag(index,40,0,82,16,"rescaleIntercept"); // 0x0028, 0x1052
+    index = this.readTag(index,40,0,83,16,"rescaleSlope");     // 0x0028, 0x1053
     // read pixel data
     this.readImage(this.moveToPixelDataTag(index));    
 }
@@ -101,7 +105,7 @@ DicomParser.prototype.moveToPixelDataTag=function(index)
 
 DicomParser.prototype.readImage=function(index)
 {
-    this.pixelBuffer = new Array(512*512*2);
+    this.pixelBuffer = new Array(this.numberOfRows*this.numberOfColumns*2);
     var pixelIndex=0;
     for(var i=index; i<this.inputBuffer.length; i+=2) 
     {     
