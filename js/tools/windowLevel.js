38221b50ae7522f9225f2bae5a44c513aace57b7
ivmartel <ymartelli@cistib.upf.edu>
2011-10-11 15:53:52 +0200

    Extracted window level methods to re-enable presets.

--- js/tools/windowLevel.js
+++ js/tools/windowLevel.js
@@ -49,96 +49,6 @@ function tools_windowLevel(app)
         document.getElementById("presetSelector").style.display=str;
     };
     
-    function showHUvalue(x,y)
-    {
-        var t = (y*app.getImage().getSize()[0])+x;        
-        
-        // style
-        app.getDrawContext().clearRect(0, 0, 150, 150);
-        app.getDrawContext().fillStyle = app.getStyle().getTextColor();
-        app.getDrawContext().font = app.getStyle().getFontStr();
-        app.getDrawContext().textBaseline = "top";
-        app.getDrawContext().textAlign = "left";
-        
-        // text
-        app.getDrawContext().fillText("X = "+x, 0, 0);
-        app.getDrawContext().fillText("Y = "+y, 0, app.getStyle().getLineHeight());
-        app.getDrawContext().fillText(
-        		"HU = "+app.getLookupObj().huLookup[app.getPixelBuffer()[t]], 
-        		0, 
-        		2*app.getStyle().getLineHeight());
-    }
-
-    function showWindowingValue(windowCenter,windowWidth)
-    {
-        // style
-    	app.getDrawContext().clearRect(app.getDrawCanvas().width-150, 0, app.getDrawCanvas().width, 150);
-    	app.getDrawContext().fillStyle = app.getStyle().getTextColor();
-    	app.getDrawContext().font = app.getStyle().getFontStr();
-    	app.getDrawContext().textBaseline = "top";
-    	app.getDrawContext().textAlign = "right";
-        
-        // text
-    	app.getDrawContext().fillText("WindowCenter = "+windowCenter, app.getDrawCanvas().width, 0);
-    	app.getDrawContext().fillText("WindowWidth = "+windowWidth, app.getDrawCanvas().width, app.getStyle().getLineHeight());
-    }
-
-    function updateWindowingData(wc,ww)
-    {
-    	app.getLookupObj().setWindowingdata(wc,ww);
-        showWindowingValue(wc,ww);
-        app.generateImage();
-    }
-
-    this.changePreset = function()
-    {    
-        applyPreset(parseInt(document.getElementById("presetsMenu").options[
-            document.getElementById("presetsMenu").selectedIndex].value));
-    };
-
-    function applyPreset(preset)    
-    {    
-        var ww, wc;
-        switch (preset)
-        {
-            case 1: // default
-                wc=app.getLookupObj().defaultWindowCenter;
-                ww=app.getLookupObj().defaultWindowWidth;
-                updateWindowingData(wc,ww);
-                break;
-            
-            case 2: // abdomen
-                wc=350;
-                ww=40;
-                updateWindowingData(wc,ww);
-                break;
-            
-            case 3: // lung
-                wc=-600;
-                ww=1500;
-                updateWindowingData(wc,ww);
-                break;
-            
-            case 4: // brain
-                wc=40;
-                ww=80;
-                updateWindowingData(wc,ww);
-                break;
-            
-            case 5: // bone
-                wc=480;
-                ww=2500;
-                updateWindowingData(wc,ww);
-                break;
-            
-            case 6: // head
-                wc=90;
-                ww=350;
-                updateWindowingData(wc,ww);
-                break;
-        }
-    }
-
 } // tools_windowLevel
 
 function gGetPresetSelector()
@@ -149,7 +59,7 @@ function gGetPresetSelector()
     var selector = document.createElement("select");
     selector.id = "presetsMenu";
     selector.name = "presetsMenu";
-    selector.onchange = app.getToolBox().changePreset;
+    selector.onchange = changePreset;
     selector.selectedIndex = 1;
     paragraph.appendChild(selector);
 
@@ -166,3 +76,95 @@ function gGetPresetSelector()
     document.getElementById('presetSelector').style.display="none";
     document.getElementById('presetSelector').appendChild(paragraph);
 }
+
+function showHUvalue(x,y)
+{
+    var t = (y*app.getImage().getSize()[0])+x;        
+    
+    // style
+    app.getDrawContext().clearRect(0, 0, 150, 150);
+    app.getDrawContext().fillStyle = app.getStyle().getTextColor();
+    app.getDrawContext().font = app.getStyle().getFontStr();
+    app.getDrawContext().textBaseline = "top";
+    app.getDrawContext().textAlign = "left";
+    
+    // text
+    app.getDrawContext().fillText("X = "+x, 0, 0);
+    app.getDrawContext().fillText("Y = "+y, 0, app.getStyle().getLineHeight());
+    app.getDrawContext().fillText(
+    		"HU = "+app.getLookupObj().huLookup[app.getPixelBuffer()[t]], 
+    		0, 
+    		2*app.getStyle().getLineHeight());
+}
+
+function showWindowingValue(windowCenter,windowWidth)
+{
+    // style
+	app.getDrawContext().clearRect(app.getDrawCanvas().width-150, 0, app.getDrawCanvas().width, 150);
+	app.getDrawContext().fillStyle = app.getStyle().getTextColor();
+	app.getDrawContext().font = app.getStyle().getFontStr();
+	app.getDrawContext().textBaseline = "top";
+	app.getDrawContext().textAlign = "right";
+    
+    // text
+	app.getDrawContext().fillText("WindowCenter = "+windowCenter, app.getDrawCanvas().width, 0);
+	app.getDrawContext().fillText("WindowWidth = "+windowWidth, app.getDrawCanvas().width, app.getStyle().getLineHeight());
+}
+
+function updateWindowingData(wc,ww)
+{
+	app.getLookupObj().setWindowingdata(wc,ww);
+    showWindowingValue(wc,ww);
+    app.generateImage();
+}
+
+function changePreset()
+{    
+    applyPreset(parseInt(document.getElementById("presetsMenu").options[
+        document.getElementById("presetsMenu").selectedIndex].value));
+}
+
+function applyPreset(preset)    
+{    
+    var ww, wc;
+    switch (preset)
+    {
+        case 1: // default
+            wc=app.getLookupObj().defaultWindowCenter;
+            ww=app.getLookupObj().defaultWindowWidth;
+            updateWindowingData(wc,ww);
+            break;
+        
+        case 2: // abdomen
+            wc=350;
+            ww=40;
+            updateWindowingData(wc,ww);
+            break;
+        
+        case 3: // lung
+            wc=-600;
+            ww=1500;
+            updateWindowingData(wc,ww);
+            break;
+        
+        case 4: // brain
+            wc=40;
+            ww=80;
+            updateWindowingData(wc,ww);
+            break;
+        
+        case 5: // bone
+            wc=480;
+            ww=2500;
+            updateWindowingData(wc,ww);
+            break;
+        
+        case 6: // head
+            wc=90;
+            ww=350;
+            updateWindowingData(wc,ww);
+            break;
+    }
+}
+
+
