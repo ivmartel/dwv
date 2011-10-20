04300f8b3574e8703abacecdbddd6a688a887086
ivmartel <ymartelli@cistib.upf.edu>
2011-10-20 15:07:06 +0200

    Added space.

--- js/dicom/LookupTable.js
+++ js/dicom/LookupTable.js
@@ -19,50 +19,54 @@ function LookupTable()
     this.calculateLookup=calculateLookup;
     this.setWindowingdata=setWindowingdata;
 }
-LookupTable.prototype.setData=function(wc,ww,rs,ri)
+
+LookupTable.prototype.setData = function(wc,ww,rs,ri)
 {    
-    this.windowCenter=wc;
-    this.windowWidth=ww;
-    this.defaultWindowCenter=wc;
-    this.defaultWindowWidth=ww;
-    this.rescaleSlope=rs;
-    this.rescaleIntercept=ri;    
+    this.windowCenter = wc;
+    this.windowWidth = ww;
+    this.defaultWindowCenter = wc;
+    this.defaultWindowWidth = ww;
+    this.rescaleSlope = rs;
+    this.rescaleIntercept = ri;    
 };
+
 function setWindowingdata(wc,ww)
 {
-    this.windowCenter=wc;
-    this.windowWidth=ww;    
+    this.windowCenter = wc;
+    this.windowWidth = ww;    
 };
+
 function calculateHULookup()
 {
     this.huLookup = new Array(4096);         
-     for(var inputValue=0;inputValue<=4095;inputValue++)
-     {        
-        this.huLookup[inputValue]=inputValue*this.rescaleSlope+this.rescaleIntercept;        
-    }        
-}    
+	for(var inputValue=0; inputValue<=4095; inputValue++)
+	{        
+	    this.huLookup[inputValue] = inputValue * this.rescaleSlope + this.rescaleIntercept;        
+	}        
+}  
+
 function calculateLookup()
 {    
-    xMin=this.windowCenter-0.5-(this.windowWidth-1)/2;
-    xMax=this.windowCenter-0.5+(this.windowWidth-1)/2;    
-    yMax=255;
-    yMin=0;
-    this.ylookup=new Array(4096);
-    for(var inputValue=0;inputValue<=4095;inputValue++)
+    xMin = this.windowCenter - 0.5 - (this.windowWidth-1) / 2;
+    xMax = this.windowCenter - 0.5 + (this.windowWidth-1) / 2;    
+    yMax = 255;
+    yMin = 0;
+    this.ylookup = new Array(4096);
+    for(var inputValue=0; inputValue<=4095; inputValue++)
      {         
-         if(this.huLookup[inputValue]<=xMin)
+         if(this.huLookup[inputValue] <= xMin)
          {                            
-            this.ylookup[inputValue]=yMin;                        
+            this.ylookup[inputValue] = yMin;                        
         }
-        else if (this.huLookup[inputValue]>xMax)
+        else if (this.huLookup[inputValue] > xMax)
         {
-            this.ylookup[inputValue]=yMax;         
+            this.ylookup[inputValue] = yMax;         
         }
         else
         {                
-            var y=((this.huLookup[inputValue]-(this.windowCenter-0.5))/(this.windowWidth-1)+0.5)*(yMax-yMin)+yMin;                        
+            var y = ( (this.huLookup[inputValue] - (this.windowCenter-0.5) ) / (this.windowWidth-1) + 0.5 )
+            	* (yMax-yMin) + yMin;                        
             this.ylookup[inputValue]= parseInt(y);
         }
      }
-    
 }
