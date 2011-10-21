72eaca078d2bd3e5c56227362e2faa54e7eca883
ivmartel <ymartelli@cistib.upf.edu>
2011-10-21 17:23:02 +0200

    Intent at undo/redo.

--- js/tools/circle.js
+++ js/tools/circle.js
@@ -6,6 +6,7 @@ tool.Circle = function(app)
 {
     var self = this;
     this.started = false;
+    this.undoStack = new UndoStack();
 
     // This is called when you start holding down the mouse button.
     this.mousedown = function(ev){
@@ -21,20 +22,16 @@ tool.Circle = function(app)
             return;
         }
 
-        app.getDrawContext().clearRect(
-        		0, 0, 
-        		app.getDrawCanvas().width, 
-        		app.getDrawCanvas().height);
-        app.getDrawContext().fillStyle = app.getStyle().getLineColor();
-        app.getDrawContext().strokeStyle = app.getStyle().getLineColor();
-
         var a = Math.abs(self.x0-ev._x);
         var b = Math.abs(self.y0-ev._y);
         var radius = Math.round(Math.sqrt(a*a+b*b));
+
+        var centre = new Point2D(self.x0, self.y0);
+        var circle = new Circle(centre, radius);
         
-        app.getDrawContext().beginPath();
-        app.getDrawContext().arc(self.x0, self.y0, radius, 0, 2*Math.PI);
-        app.getDrawContext().stroke();
+        var command = new DrawCircleCommand(circle, app);
+        self.undoStack.add(command.draw);
+        command.draw();
         
         // surface
         a = a * app.getImage().getSpacing()[0];
@@ -61,5 +58,70 @@ tool.Circle = function(app)
         if( value ) tool.draw.appendColourChooserHtml(app);
         else tool.draw.clearColourChooserHtml();
     };
+    
+    this.keydown = function(event){
+    	if( event.keyCode == 85 ) // u
+		{
+    		self.undoStack.undo();
+		}
+    	else if( event.keyCode == 82 ) // r
+		{
+    		self.undoStack.redo();
+		}
+    };
 
 }; // Circle function
+
+
+Point2D = function(x,y)
+{
+    this.getX = function() { return x; };
+    this.getY = function() { return y; };
+}; // Point2D
+
+Circle = function(center, radius)
+{
+	var surface = Math.PI*radius*radius;
+
+    this.getCenter = function() { return center; };
+    this.getRadius = function() { return radius; };
+    this.getSurface = function() { return surface; };
+}; // Circle
+
+DrawCircleCommand = function(circle, app)
+{
+	this.draw = function()
+	{
+	    app.getDrawContext().clearRect(
+	    		0, 0, 
+	    		app.getDrawCanvas().width, 
+	    		app.getDrawCanvas().height);
+	    app.getDrawContext().fillStyle = app.getStyle().getLineColor();
+	    app.getDrawContext().strokeStyle = app.getStyle().getLineColor();
+	
+	    app.getDrawContext().beginPath();
+	    app.getDrawContext().arc(
+	    		circle.getCenter().getX(), 
+	    		circle.getCenter().getY(), 
+	    		circle.getRadius(),
+	    		0, 2*Math.PI);
+	    app.getDrawContext().stroke();
+	};
+}; // Circle command
+
+function UndoStack()
+{ 
+	this.stack = []; 
+	this.maxCmds = 0;
+	this.curCmd = 0;
+} 
+UndoStack.prototype.add = function(cmd) { 
+	this.stack[this.curCmd++] = cmd;
+	this.maxCmds = this.curCmd;
+};
+UndoStack.prototype.undo = function(){ 
+	this.stack[this.curCmd--]();
+}; 
+UndoStack.prototype.redo = function(){ 
+	this.stack[this.curCmd++]();
+};
