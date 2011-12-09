017199a409068ea763a588697ac140eae704c66b
ivmartel <ymartelli@cistib.upf.edu>
2011-12-09 17:20:18 +0100

    draft: pan works better but not zoom...

--- js/tools/zoom.js
+++ js/tools/zoom.js
@@ -6,56 +6,39 @@ tool.Zoom = function(app)
 {
     var self = this;
     this.started = false;
+    this.panX = 0;
+    this.panY = 0;
+    this.zoomf = 1;
 
     // This is called when you start holding down the mouse button.
     this.mousedown = function(ev){
         self.started = true;
-        self.x0 = ev._x;
-        self.y0 = ev._y;
+        self.x0 = ev._x - self.panX;
+        self.y0 = ev._y - self.panY;
     };
 
     // This function is called every time you move the mouse.
     this.mousemove = function(ev){
         if (!self.started)
         {
-            // zoom mode
-            self.x0 = ev._x;
-            self.y0 = ev._y;
             return;
         }
 
         var context = app.getImageLayer().getContext();
-        var tempContext = app.getTempLayer().getContext();
-        var tempCanvas = app.getTempLayer().getCanvas();
         
         // get the image data
-        var imageData = context.getImageData( 
-    			0, 0, 
-    			app.getImage().getSize()[0], 
-    			app.getImage().getSize()[1]); 
+        var imageData = app.getImageData();
        
-        // copy it to the temporary context
-        app.getTempLayer().clearContextRect();
-        tempContext.putImageData(imageData, 0, 0);
-        
-        // save base settings
-        context.save();
-
         // translate the base context
         app.getImageLayer().clearContextRect();
-        var tx = ev._x - self.x0;
-        var ty = ev._y - self.y0;
-        context.translate( tx, ty );
+        var tx = self.zoomf * (ev._x - self.x0);
+        var ty = self.zoomf * (ev._y - self.y0);
+        
+        self.panX = tx;
+        self.panY = ty;
 		
         // put the draw canvas in the base context
-        context.drawImage(tempCanvas, 0, 0);
-        
-        // restore base settings
-        context.restore();
-        
-        // do not cumulate
-        self.x0 = ev._x;
-        self.y0 = ev._y;
+        context.putImageData(imageData, tx, ty);
     };
 
     // This is called when you release the mouse button.
@@ -69,11 +52,11 @@ tool.Zoom = function(app)
     
     // This is called when you use the mouse wheel.
     this.DOMMouseScroll = function(ev){
-        zoom(ev.detail, self.x0, self.y0);
+        zoom(ev.detail,  ev._x, ev._y);
     };
 
     this.mousewheel = function(ev){
-        zoom(ev.wheelDelta/40, self.x0, self.y0);
+        zoom(ev.wheelDelta/40, ev._x, ev._y);
     };
     
     this.enable = function(value){
@@ -95,6 +78,7 @@ tool.Zoom = function(app)
         		0, 0, 
     			app.getImage().getSize()[0], 
     			app.getImage().getSize()[1]); 
+        //var imageData = app.getImageData();
        
         // copy it to the temporary context
         app.getTempLayer().clearContextRect();
@@ -106,13 +90,16 @@ tool.Zoom = function(app)
         // translate the base context
         app.getImageLayer().clearContextRect();
         
-        var zoom = Math.pow(1.1,step);
+        var localZoom = Math.pow(1.1, step);
+        self.zoomf *= localZoom;
         context.translate(cx, cy);
-        context.scale( zoom, zoom );
-        context.translate(-cx, -cy);
+        context.scale( localZoom, localZoom );
         
         // put the draw canvas in the base context
         context.drawImage(tempCanvas, 0, 0);
+
+        context.translate(-cx*localZoom, -cy*localZoom);
+        
         
         // restore base settings
         context.restore();
