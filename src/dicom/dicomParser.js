02251e8768fdd45e5473b693763900167d738fc5
ivmartel <ymartelli@cistib.upf.edu>
2012-04-12 12:09:56 +0200

    Intent at supporting implicit data. Added documentation.

--- src/dicom/dicomParser.js
+++ src/dicom/dicomParser.js
@@ -65,11 +65,20 @@ dwv.dicom.DicomParser = function(file)
     this.file = file;
 };
 
+/**
+ * Get the DICOM data pixel buffer.
+ * @returns The pixel buffer (as an array).
+ */
 dwv.dicom.DicomParser.prototype.getPixelBuffer=function()
 {
     return this.pixelBuffer;
 };
 
+/**
+ * Append a DICOM element to the dicomElements member object.
+ * Allows for easy retrieval of DICOM tag values from the tag name.
+ * @param element The element to add.
+ */
 dwv.dicom.DicomParser.prototype.appendDicomElement=function( element )
 {
     // find a good tag name
@@ -84,6 +93,12 @@ dwv.dicom.DicomParser.prototype.appendDicomElement=function( element )
             "value": element.value };
 };
 
+/**
+ * Read a DICOM tag.
+ * @param reader The raw data reader.
+ * @param offset The offset where to start to read.
+ * @returns An object containing the tags 'group', 'element' and 'name' (unique).
+ */
 dwv.dicom.DicomParser.prototype.readTag=function(reader, offset)
 {
     // group
@@ -103,40 +118,60 @@ dwv.dicom.DicomParser.prototype.readTag=function(reader, offset)
             name = this.dict.newDictionary[group][element][2];
         }
     }
-
     // return as hex
-    return { 'group': group, 'element': element, 'name': name };
+    return {'group': group, 'element': element, 'name': name};
 };
 
-dwv.dicom.DicomParser.prototype.readDataElement=function(reader, offset)
+/**
+ * Read a DICOM data element.
+ * @param reader The raw data reader.
+ * @param offset The offset where to start to read.
+ * @param implicit Is the DICOM VR implicit?
+ * @returns An object containing the element 'tag', 'vl', 'vr', 'data' and 'offset'.
+ */
+dwv.dicom.DicomParser.prototype.readDataElement=function(reader, offset, implicit)
 {
-    // only valid for LittleEndianExplicit (transferSyntax: 1.2.840.10008.1.2.1)
-    
     // tag: group, element
     var tag = this.readTag(reader, offset);
+    var tagOffset = 4;
     
-    var vr; // Value Representation
-    var vl; // Value Length
-    var vlOffset; // byte size of VL + VR
+    var vr; // Value Representation (VR)
+    var vl; // Value Length (VL)
+    var vrOffset = 0; // byte size of VR
+    var vlOffset = 0; // byte size of VL
     
     // (private) Item group case
     if( tag.group === "0xFFFE" ) {
         vr = "N/A";
-        vl = reader.readNumber( 4, offset+4 );
+        vrOffset = 0;
+        vl = reader.readNumber( 4, offset+tagOffset );
         vlOffset = 4;
     }
     // non Item case
     else {
-        vr = reader.readString( 2, offset+4 );
+        // implicit VR?
+        if(implicit) {
+            vr = "UN";
+            if( this.dict.newDictionary[tag.group] ) {
+                if( this.dict.newDictionary[tag.group][tag.element] ) {
+                    vr = this.dict.newDictionary[tag.group][tag.element][0];
+                }
+            }
+            vrOffset = 0;
+        }
+        else {
+            vr = reader.readString( 2, offset+tagOffset );
+            vrOffset = 2;
+        }
         // long representations
         if(vr === "OB" || vr === "OF" || vr === "SQ" || vr === "OW" || vr === "UN") {
-            vl = reader.readNumber( 4, offset+8 );
-            vlOffset = 8;
+            vl = reader.readNumber( 4, offset+tagOffset+vrOffset+2 );
+            vlOffset = 6;
         }
         // short representation
         else {
-            vl = reader.readNumber( 2, offset+6 );
-            vlOffset = 4;
+            vl = reader.readNumber( 2, offset+tagOffset+vrOffset );
+            vlOffset = 2;
         }
     }
     
@@ -149,12 +184,12 @@ dwv.dicom.DicomParser.prototype.readDataElement=function(reader, offset)
     var data;
     if( vr === "US" || vr === "UL")
     {
-        data = [reader.readNumber( vl, offset+4+vlOffset)];
+        data = [reader.readNumber( vl, offset+tagOffset+vrOffset+vlOffset)];
     }
     else if( vr === "OW" ) // should be pixel data
     {
         data = [];
-        var begin = offset+4+vlOffset;
+        var begin = offset+tagOffset+vrOffset+vlOffset;
         var end = begin + vl;
         for(var i=begin; i<end; i+=2) 
         {     
@@ -163,12 +198,12 @@ dwv.dicom.DicomParser.prototype.readDataElement=function(reader, offset)
     }
     else
     {
-        data = reader.readString( vl, offset+4+vlOffset);
+        data = reader.readString( vl, offset+tagOffset+vrOffset+vlOffset);
         data = data.split("\\");                
     }    
 
     // total element offset
-    var elementOffset = 4 + vlOffset + vl;
+    var elementOffset = tagOffset + vrOffset + vlOffset + vl;
     
     // return
     return { 
@@ -177,13 +212,18 @@ dwv.dicom.DicomParser.prototype.readDataElement=function(reader, offset)
         'offset': elementOffset};    
 };
 
+/**
+ * Parse the complete DICOM file (given as input to the class).
+ * Fills in the member object 'dicomElements'.
+ */
 dwv.dicom.DicomParser.prototype.parseAll = function()
 {
     var offset = 0;
     var i;
-    
+    var implicit = false;
     // dictionary
     this.dict.init();
+    // default readers
     var metaReader = new dwv.dicom.LittleEndianReader(this.file);
     var dataReader = new dwv.dicom.LittleEndianReader(this.file);
 
@@ -207,15 +247,22 @@ dwv.dicom.DicomParser.prototype.parseAll = function()
     for( i=metaStart; i<metaEnd; i++ ) 
     {
         // get the data element
-        dataElement = this.readDataElement(metaReader, i);
+        dataElement = this.readDataElement(metaReader, i, false);
         // check the transfer syntax
         if( dataElement.tag.name === "TransferSyntaxUID" ) {
             var val = dataElement.data[0];
-            //var str = val.substring(0, val.length-1); // get rid of ending zero-with space (u200B)
-            console.log("transfer: "+val);
+            // get rid of ending zero-width space (u200B)
+            if( val[val.length-1] === String.fromCharCode("u200B") ) {
+                val = val.substring(0, val.length-1); 
+            }
+            console.log("transfer syntax: "+val);
             if( val === "1.2.840.10008.1.2.2" ) {
-                console.log("Big endian!");
+                console.log("Big endian.");
                 dataReader = new dwv.dicom.BigEndianReader(this.file);
+            }
+            else if( val === "1.2.840.10008.1.2" ) {
+                console.log("Implicit data.");
+                implicit = true;
             }
         }            
         // store the data element
@@ -232,7 +279,7 @@ dwv.dicom.DicomParser.prototype.parseAll = function()
     for( i=metaEnd; i<this.inputBuffer.length; i++) 
     {
         // get the data element
-        dataElement = this.readDataElement(dataReader, i);
+        dataElement = this.readDataElement(dataReader, i, implicit);
         // store pixel data
         if( dataElement.tag.name === "PixelData") {
             this.pixelBuffer = dataElement.data;
@@ -248,6 +295,10 @@ dwv.dicom.DicomParser.prototype.parseAll = function()
     }
 };
 
+/**
+ * Get an Image object from the read DICOM file.
+ * @returns A new Image.
+ */
 dwv.dicom.DicomParser.prototype.getImage = function()
 {
     // size
