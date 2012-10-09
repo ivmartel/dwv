38e688a7ac050dd9d223beceb1d33e19bb504c08
ymartelli <ymartelli@cistib.upf.edu>
2012-10-09 16:58:48 +0200

    Fixed tests.

--- tests/math/shapes.test.js
+++ tests/math/shapes.test.js
@@ -3,27 +3,29 @@
  */
 $(document).ready(function(){
     test("Test Point2D.", function() {
-    	var p0 = new Point2D(1,2);
-    	// can't access x
-    	//p0.x ERROR
-    	// can't access y
-    	//p0.y ERROR
-    	// getX
-    	equal(p0.getX(), 1, "getX");
-    	// getY
-    	equal(p0.getY(), 2, "getY");
+    	var p0 = new dwv.math.Point2D(1,2);
+        // getX
+        equal(p0.getX(), 1, "getX");
+        // getY
+        equal(p0.getY(), 2, "getY");
+    	// can't modify internal x
+    	p0.x = 3;
+        equal(p0.getX(), 1, "getX after .x");
+    	// can't modify internal y
+        p0.y = 3;
+        equal(p0.getY(), 2, "getY after .y");
     	// equals: true
-    	var p1 = new Point2D(1,2);
+    	var p1 = new dwv.math.Point2D(1,2);
     	equal(p0.equals(p1), true, "equals true");
     	// equals: false
-    	var p2 = new Point2D(2,1);
+    	var p2 = new dwv.math.Point2D(2,1);
     	equal(p0.equals(p2), false, "equals false");
     	// to string
     	equal(p0.toString(), "(1, 2)", "toString");    	
 	});
 
 test("Test FastPoint2D.", function() {
-    	var p0 = new FastPoint2D(1,2);
+    	var p0 = new dwv.math.FastPoint2D(1,2);
     	// x
     	equal(p0.x, 1, "x");
     	// y
@@ -35,24 +37,24 @@ test("Test FastPoint2D.", function() {
     	p0.y = 4;
     	equal(p0.y, 4, "modified y");
     	// equals: true
-    	var p1 = new FastPoint2D(3,4);
+    	var p1 = new dwv.math.FastPoint2D(3,4);
     	equal(p0.equals(p1), true, "equals true");
     	// equals: false
-    	var p2 = new FastPoint2D(4,3);
+    	var p2 = new dwv.math.FastPoint2D(4,3);
     	equal(p0.equals(p2), false, "equals false");
     	// to string
     	equal(p0.toString(), "(3, 4)", "toString");    	
 	});
 
 test("Test Circle.", function() {
-	var center = new Point2D(0,0);
-	var c0 = new Circle(center,2);
+	var center = new dwv.math.Point2D(0,0);
+	var c0 = new dwv.math.Circle(center,2);
 	// getCenter
 	equal(c0.getCenter(), center, "getCenter");
 	// getRadius
-	equal(c0.getRadius, 2, "getRadius");
+	equal(c0.getRadius(), 2, "getRadius");
 	// getSurface
-	equal(c0.getSurface, Math.PI*2*2, "getSurface");
+	equal(c0.getSurface(), Math.PI*2*2, "getSurface");
 	// equals: true
 	equal(c0.getWorldSurface(0.5,0.5), Math.PI, "getWorldSurface");
 });
