60cf89dc25c2f15fff39e434504716284aec973c
ivmartel <ivmartel@gmail.com>
2013-03-29 11:02:33 +0100

    Made zoom the right way and less sensitive.

--- demo/mobile/src/tools/zoom.js
+++ demo/mobile/src/tools/zoom.js
@@ -68,7 +68,7 @@ dwv.tool.Zoom = function(app)
        var point0 = new dwv.math.Point2D(ev._x, ev._y);
        var point1 = new dwv.math.Point2D(ev._x1, ev._y1);
        var newLine = new dwv.math.Line(point0, point1);
-       var lineDiff = ( self.line0.getLength() - newLine.getLength() ) / 100;
+       var lineDiff = ( newLine.getLength() - self.line0.getLength() ) / 1000;
        
        if( lineDiff !== 0 )
            zoom(lineDiff, self.midPoint.getX(), self.midPoint.getY());
