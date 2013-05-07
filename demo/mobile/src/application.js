b499b752aa447a2e50e9a4924db1a4ed15947265
ivmartel <ivmartel@gmail.com>
2013-05-07 23:21:40 +0200

    Final v0.3

--- demo/mobile/src/application.js
+++ demo/mobile/src/application.js
@@ -267,7 +267,6 @@ dwv.App = function(mobile)
         if( mobile )
         {
             if( event.type === "touchstart"
-                || event.type === "touchend"
                 || event.type === "touchmove")
             {
                 event.preventDefault();
@@ -293,6 +292,7 @@ dwv.App = function(mobile)
                   handled = true;
                 }
             }
+            else if( event.type === "touchend" ) handled = true;
         }
         else
         {
@@ -312,6 +312,7 @@ dwv.App = function(mobile)
                 // set handle event flag
                 handled = true;
             }
+            else if( event.type === "keydown" ) handled = true;
         }
             
         // Call the event handler of the tool.
