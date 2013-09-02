a6ca8aae2f3a0ebe226de94ee9f053c28d6adf61
ivmartel <ivmartel@gmail.com>
2013-09-03 01:20:04 +0200

    trying diff

--- demo/trunk/src/tools/zoom.js
+++ demo/trunk/src/tools/zoom.js
@@ -66,10 +66,10 @@ dwv.tool.Zoom = function(app)
        var point0 = new dwv.math.Point2D(ev._x, ev._y);
        var point1 = new dwv.math.Point2D(ev._x1, ev._y1);
        var newLine = new dwv.math.Line(point0, point1);
-       var lineRatio = newLine.getLength() / self.line0.getLength();
+       var lineRatio = newLine.getLength() - self.line0.getLength();
        
        //if( Math.abs(lineRatio) > 0.1 )
-       zoomLayers(lineRatio-1, self.midPoint.getX(), self.midPoint.getY());
+       zoomLayers(lineRatio, self.midPoint.getX(), self.midPoint.getY());
     };
     
     // This is called when you release the mouse button.
