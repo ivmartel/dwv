d927b89b2cb6c74165bafc6a130976c2b6ac84d0
ivmartel <ivmartel@gmail.com>
2013-09-03 01:56:33 +0200

    ok, no test...

--- demo/trunk/src/tools/zoom.js
+++ demo/trunk/src/tools/zoom.js
@@ -69,8 +69,7 @@ dwv.tool.Zoom = function(app)
        var lineRatio = newLine.getLength() / self.line0.getLength();
        
        var zoom = lineRatio - 1;
-       if( (Math.abs(zoom) % 0.1) <= 0.4 )
-           zoomLayers(zoom, self.midPoint.getX(), self.midPoint.getY());
+       zoomLayers(zoom, self.midPoint.getX(), self.midPoint.getY());
     };
     
     // This is called when you release the mouse button.
