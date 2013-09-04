278f3469eee4483ff3f2a5946bffa04ee41a2ff8
ivmartel <ivmartel@gmail.com>
2013-09-05 00:44:31 +0200

    Added jshint task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,16 @@ module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
             files: ['tests/tests.html']
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     
     // Task to run tests
-    grunt.registerTask('test', 'qunit');
+    grunt.registerTask('test', ['jshint', 'qunit']);
 };
 
