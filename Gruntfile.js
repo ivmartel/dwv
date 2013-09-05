288b6d04e34939a3da6519655ef2a40dcf3f9304
ivmartel <ivmartel@gmail.com>
2013-09-05 23:52:56 +0200

    Added concat and uglify to the test grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,40 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js']
+        },
         qunit: {
             files: ['tests/tests.html']
         },
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js']
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     
     // Task to run tests
-    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('test', ['jshint', 'qunit', 'concat', 'uglify']);
 };
 
