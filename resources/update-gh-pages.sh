d9b3ed017975caa20144eb90b07d81b51906aaef
ivmartel <ivmartel@gmail.com>
2013-09-07 00:56:39 +0200

    Added travis push results to gh-pages.

---
+++ resources/update-gh-pages.sh
@@ -0,0 +1,17 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv.git gh-pages > /dev/null
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/dist/* demo/trunk
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+  echo -e "Done magic with coverage\n"
+fi
