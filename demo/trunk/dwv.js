bb3106541751cbaf06161e2970d2dd71cc663957
Travis <travis@travis-ci.org>
2013-09-09 22:27:46 +0000

    Travis build 66 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -7151,6 +7151,7 @@ dwv.tool.ToolBox = function(app)
 dwv.tool.ToolBox.prototype.enable = function(bool)
 {
     if( bool ) {
+        this.sortTools();
         dwv.gui.appendToolboxHtml();
         this.init();
     }
@@ -7178,6 +7179,18 @@ dwv.tool.ToolBox.prototype.setSelectedTool = function(name) {
 
 dwv.tool.ToolBox.prototype.hasTool = function(name) {
     return dwv.tool.tools[name];
+};
+
+dwv.tool.ToolBox.prototype.sortTools = function()
+{
+    // fiddle with order: make window level first if present
+    var tools = dwv.tool.tools;
+    dwv.tool.tools = {};
+    if( tools.windowlevel ) dwv.tool.tools.windowlevel = tools.windowlevel;
+    for( var key in tools ) {
+        if( key === "windowlevel" ) continue;
+        dwv.tool.tools[key] = tools[key];
+    }
 };
 
 dwv.tool.ToolBox.prototype.init = function()
