30046634c530007ab34125a75e73d269ac5ffcef
ivmartel <ivmartel@gmail.com>
2013-09-10 00:58:43 +0200

    Ok, no magic.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                 separator: ';'
             },
             dist: {
-                src: ['src/**/*.js','!(src/html/gui_*.js)'],
+                src: ['src/**/*.js','!src/html/gui_ui.js','!src/html/gui_mobile.js'],
                 dest: 'dist/<%= pkg.name %>.js'
             }
         },
