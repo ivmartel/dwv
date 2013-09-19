4cab02d759070b3cc61b70aa1e320445e82111e7
ivmartel <ivmartel@gmail.com>
2013-09-20 01:25:20 +0200

    Fixed typo.

--- resources/update-gh-pages.sh
+++ resources/update-gh-pages.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
   #using token clone gh-pages branch
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv.git gh-pages > /dev/null
-  #go into diractory and copy data we're interested in to that directory
+  #go into directory and copy data we're interested in to that directory
   cd gh-pages
   cp -Rf $HOME/dist/* demo/trunk
   #add, commit and push files
