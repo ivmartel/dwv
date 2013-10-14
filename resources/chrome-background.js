5598c82b04a2d25a70fd1c3e7eee381402660f6f
ivmartel <ivmartel@gmail.com>
2013-10-15 01:12:17 +0200

    Bigger start size.

--- resources/chrome-background.js
+++ resources/chrome-background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 400,
-      'height': 500
+      'width': 1024,
+      'height': 800
     }
   });
 });
