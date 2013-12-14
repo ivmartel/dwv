059532db506c40d34f31e27dfa4c9e69df9d192a
ivmartel <ivmartel@gmail.com>
2013-12-14 18:44:49 +0100

    Larger to fit in the help button.

--- resources/chrome-background.js
+++ resources/chrome-background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 1024,
+      'width': 1100,
       'height': 800
     }
   });
