aa496c0cd23eadc8b2367d2ec01118d36e0d3126
ivmartel <ivmartel@gmail.com>
2014-01-26 23:55:27 +0100

    Use relative path for help image resources.

--- src/html/gui.js
+++ src/html/gui.js
@@ -981,7 +981,7 @@ dwv.gui.appendHelpHtml = function(mobile)
                 var action = tool.getHelp()[actionType][keys[i]];
                 
                 var img = document.createElement("img");
-                img.src = "resources/"+keys[i]+".png";
+                img.src = "./resources/"+keys[i]+".png";
                 img.style.float = "left";
                 img.style.margin = "0px 15px 15px 0px";
                 
