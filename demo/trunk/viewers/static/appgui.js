92ead3533fc21628c49e4c953fd40c7287931b4e
ivmartel <ivmartel@gmail.com>
2014-01-28 22:53:41 +0100

    Updated to new trunk layout.

---
+++ demo/trunk/viewers/static/appgui.js
@@ -0,0 +1,124 @@
+/** 
+ * Application GUI.
+ */
+
+// Loaders
+dwv.gui.appendLoadboxHtml = function(){
+    dwv.gui.base.appendLoadboxHtml();
+};
+
+// File loader
+dwv.gui.appendFileLoadHtml = function(){
+    dwv.gui.base.appendFileLoadHtml();
+};
+dwv.gui.displayFileLoadHtml = function(bool){
+    dwv.gui.base.displayFileLoadHtml(bool);
+};
+
+// Url loader
+dwv.gui.appendUrlLoadHtml = function(){
+    dwv.gui.base.appendUrlLoadHtml();
+};
+dwv.gui.displayUrlLoadHtml = function(bool){
+    dwv.gui.base.displayUrlLoadHtml(bool);
+};
+
+// Toolbox 
+dwv.gui.appendToolboxHtml = function(){
+    dwv.gui.base.appendToolboxHtml();
+};
+dwv.gui.displayToolboxHtml = function(bool){
+    dwv.gui.base.displayToolboxHtml(bool);
+};
+dwv.gui.initToolboxHtml = function(){
+    dwv.gui.base.initToolboxHtml();
+};
+
+// Window/level
+dwv.gui.appendWindowLevelHtml = function(){
+    dwv.gui.base.appendWindowLevelHtml();
+};
+dwv.gui.displayWindowLevelHtml = function(bool){
+    dwv.gui.base.displayWindowLevelHtml(bool);
+};
+dwv.gui.initWindowLevelHtml = function(){
+    dwv.gui.base.initWindowLevelHtml();
+};
+
+// Draw
+dwv.gui.appendDrawHtml = function(){
+    dwv.gui.base.appendDrawHtml();
+};
+dwv.gui.displayDrawHtml = function(bool){
+    dwv.gui.base.displayDrawHtml(bool);  
+};
+dwv.gui.initDrawHtml = function(){
+    dwv.gui.base.initDrawHtml();  
+};
+
+// Livewire
+dwv.gui.appendLivewireHtml = function(){
+    dwv.gui.base.appendLivewireHtml();  
+};
+dwv.gui.displayLivewireHtml = function(bool){
+    dwv.gui.base.displayLivewireHtml(bool);
+};
+dwv.gui.initLivewireHtml = function(){
+    dwv.gui.base.initLivewireHtml();
+};
+
+// Zoom
+dwv.gui.appendZoomHtml = function(){
+    dwv.gui.base.appendZoomHtml();
+};
+dwv.gui.displayZoomHtml = function(bool){
+    dwv.gui.base.displayZoomHtml(bool);
+};
+
+// Filter
+dwv.gui.appendFilterHtml = function(){
+    dwv.gui.base.appendFilterHtml();
+};
+dwv.gui.displayFilterHtml = function(bool){
+    dwv.gui.base.displayFilterHtml(bool);
+};
+dwv.gui.initFilterHtml = function(){
+    dwv.gui.base.initFilterHtml();
+};
+
+// Threshold
+dwv.gui.filter.appendThresholdHtml = function(){
+    dwv.gui.filter.base.appendThresholdHtml();
+};
+dwv.gui.filter.displayThresholdHtml = function(bool){
+    dwv.gui.filter.base.displayThresholdHtml(bool);
+};
+dwv.gui.filter.initThresholdHtml = function(){
+    dwv.gui.filter.base.initThresholdHtml();
+};
+
+// Sharpen
+dwv.gui.filter.appendSharpenHtml = function(){
+    dwv.gui.filter.base.appendSharpenHtml();
+};
+dwv.gui.filter.displaySharpenHtml = function(bool){
+    dwv.gui.filter.base.displaySharpenHtml(bool);
+};
+
+// Sobel
+dwv.gui.filter.appendSobelHtml = function(){
+    dwv.gui.filter.base.appendSobelHtml();
+};
+dwv.gui.filter.displaySobelHtml = function(bool){
+    dwv.gui.filter.base.displaySobelHtml(bool);
+};
+
+// Undo/redo
+dwv.gui.appendUndoHtml = function(){
+    dwv.gui.base.appendUndoHtml();
+};
+
+// Help
+dwv.gui.appendHelpHtml = function(mobile){
+    dwv.gui.base.appendHelpHtml(mobile);
+};
