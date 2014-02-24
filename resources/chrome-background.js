500d228c8ff4d302e9bfb00a9686efc7067c61a9
ivmartel <ivmartel@gmail.com>
2014-02-24 21:44:23 +0100

    Updated chrome app configuration files.

--- resources/chrome-background.js
+++ resources/chrome-background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+  chrome.app.window.create('../viewers/mobile/index.html', {
     'bounds': {
       'width': 1100,
       'height': 800
