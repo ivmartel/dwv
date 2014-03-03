a881b971bab4904785289bc01f9fe494edfc3341
ivmartel <ivmartel@gmail.com>
2014-03-03 23:30:56 +0100

    jshint to include everything but externals.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js']
+            files: ['/**/*.js', '!**/ext/**']
         },
         qunit: {
             files: ['tests/tests.html']
