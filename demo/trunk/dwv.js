ee40cd9167fb23d3e78db519190f4c89e3508c7f
Travis <travis@travis-ci.org>
2014-12-09 21:24:54 +0000

    Travis build 393 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -49,7 +49,7 @@ dwv.App = function()
      * @method getVersion
      * @return {String} The version of the application.
      */
-    this.getVersion = function() { return "v0.8.0"; };
+    this.getVersion = function() { return "v0.9.0beta"; };
     
     /** 
      * Get the image.
