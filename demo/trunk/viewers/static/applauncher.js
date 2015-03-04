9a2179708cd0176f19ce8c74184994ccd179b0d4
ivmartel <ivmartel@gmail.com>
2015-03-04 23:19:22 +0100

    Moved gui setup in the appgui file.

--- demo/trunk/viewers/static/applauncher.js
+++ demo/trunk/viewers/static/applauncher.js
@@ -8,31 +8,9 @@ dwv.browser.check();
 // launch when page is loaded
 $(document).ready( function()
 {
-    $("#toggleInfoLayer").button({ icons: 
-        { primary: "ui-icon-comment" }, text: false });
-    // create dialogs
-    $("#openData").dialog({ position: 
-        {my: "left top", at: "left top", of: "#pageMain"} });
-    $("#toolbox").dialog({ position: 
-        {my: "left top+160", at: "left top", of: "#pageMain"} });
-    $("#history").dialog({ position: 
-        {my: "left top+350", at: "left top", of: "#pageMain"} });
-    $("#tags").dialog({ position: 
-        {my: "right top", at: "right top", of: "#pageMain"},
-        autoOpen: false, width: 500, height: 590 });
-    $("#help").dialog({ position: 
-        {my: "right top", at: "right top", of: "#pageMain"},
-        autoOpen: false, width: 500, height: 590 });
+    // gui setup
+    dwv.gui.setup();
     
-    // image dialog
-    $("#layerDialog").dialog({ position: 
-        {my: "left+320 top", at: "left top", of: "#pageMain"}});
-    // default size
-    $("#layerDialog").dialog({ width: "auto", resizable: false });
-    // Resizable but keep aspect ratio
-    // TODO it seems to add a border that bothers getting the cursor position...
-    //$("#layerContainer").resizable({ aspectRatio: true });
-
     // main application
     var myapp = new dwv.App();
     // initialise the application
