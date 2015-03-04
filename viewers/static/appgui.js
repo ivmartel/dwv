47ba23fd708173af188c833426c3a624945053bc
ivmartel <ivmartel@gmail.com>
2015-03-04 23:18:35 +0100

    Moved gui setup in the appgui file.

--- viewers/static/appgui.js
+++ viewers/static/appgui.js
@@ -146,3 +146,31 @@ dwv.gui.Undo = dwv.gui.base.Undo;
 dwv.gui.appendHelpHtml = dwv.gui.base.appendHelpHtml;
 // Version
 dwv.gui.appendVersionHtml = dwv.gui.base.appendVersionHtml;
+
+// special setup
+dwv.gui.setup = function () {
+    $("#toggleInfoLayer").button({ icons: 
+        { primary: "ui-icon-comment" }, text: false });
+    // create dialogs
+    $("#openData").dialog({ position: 
+        {my: "left top", at: "left top", of: "#pageMain"} });
+    $("#toolbox").dialog({ position: 
+        {my: "left top+160", at: "left top", of: "#pageMain"} });
+    $("#history").dialog({ position: 
+        {my: "left top+350", at: "left top", of: "#pageMain"} });
+    $("#tags").dialog({ position: 
+        {my: "right top", at: "right top", of: "#pageMain"},
+        autoOpen: false, width: 500, height: 590 });
+    $("#help").dialog({ position: 
+        {my: "right top", at: "right top", of: "#pageMain"},
+        autoOpen: false, width: 500, height: 590 });
+    
+    // image dialog
+    $("#layerDialog").dialog({ position: 
+        {my: "left+320 top", at: "left top", of: "#pageMain"}});
+    // default size
+    $("#layerDialog").dialog({ width: "auto", resizable: false });
+    // Resizable but keep aspect ratio
+    // TODO it seems to add a border that bothers getting the cursor position...
+    //$("#layerContainer").resizable({ aspectRatio: true });
+};
