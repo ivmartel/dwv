7a9be8a9c6262272831a34b03f4bcfef15cb8a2e
ivmartel <ivmartel@gmail.com>
2015-03-06 00:21:50 +0100

    Updated version from 0.9.0beta to 0.9.0.

--- resources/dwv-static.lua
+++ resources/dwv-static.lua
@@ -103,7 +103,7 @@ print([[
 <script type="text/javascript" src="/dwv/ext/kinetic/kinetic-v5.1.1-06.10.min.js"></script>
 
 <!-- Local -->
-<script type="text/javascript" src="/dwv/dwv-0.9.0beta.min.js"></script>
+<script type="text/javascript" src="/dwv/dwv-0.9.0.min.js"></script>
 <!-- Launch the app -->
 <script type="text/javascript" src="/dwv/viewers/static/appgui.js"></script>
 ]])
