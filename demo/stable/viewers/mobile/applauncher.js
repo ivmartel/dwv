e187f8e682058c690816ffd73fec86d4723403ba
ivmartel <ivmartel@gmail.com>
2015-03-06 22:17:59 +0100

    Promoted trunk to stable.

--- demo/stable/viewers/mobile/applauncher.js
+++ demo/stable/viewers/mobile/applauncher.js
@@ -4,71 +4,24 @@
 
 // check browser support
 dwv.browser.check();
-// main application
-var app = new dwv.App();
 
 // launch when page is loaded
 $(document).ready( function()
 {
-    // Add required loaders to the loader list
-    dwv.io.loaders = {};
-    dwv.io.loaders.file = dwv.io.File;
-    dwv.io.loaders.url = dwv.io.Url;
-
-    // append load container HTML
-    dwv.gui.appendLoadboxHtml();
-    // append loaders HTML
-    dwv.gui.appendFileLoadHtml();
-    dwv.gui.appendUrlLoadHtml();
-    dwv.gui.displayFileLoadHtml(true);
-
-    // Add tools to the tool list
-    dwv.tool.tools = {};
-    dwv.tool.tools["Window/Level"] = new dwv.tool.WindowLevel(app);
-    dwv.tool.tools["Zoom/Pan"] = new dwv.tool.ZoomAndPan(app);
-    dwv.tool.tools.scroll = new dwv.tool.Scroll(app);
-    dwv.tool.tools.draw = new dwv.tool.Draw(app);
-    dwv.tool.tools.livewire = new dwv.tool.Livewire(app);
-    dwv.tool.tools.filter = new dwv.tool.Filter(app);
-
-    // Add filters to the filter list for the filter tool
-    dwv.tool.filters = {};
-    dwv.tool.filters.threshold = new dwv.tool.filter.Threshold(app);
-    dwv.tool.filters.sharpen = new dwv.tool.filter.Sharpen(app);
-    dwv.tool.filters.sobel = new dwv.tool.filter.Sobel(app);
-
-    // Add shapes to the shape list for the draw tool
-    dwv.tool.shapes = {};
-    dwv.tool.shapes.line = dwv.tool.LineCreator;
-    dwv.tool.shapes.rectangle = dwv.tool.RectangleCreator;
-    dwv.tool.shapes.roi = dwv.tool.RoiCreator;
-    dwv.tool.shapes.ellipse = dwv.tool.EllipseCreator;
-
-    // append tool container HTML
-    dwv.gui.appendToolboxHtml();
-    // append tools HTML
-    dwv.gui.appendWindowLevelHtml();
-    dwv.gui.appendZoomAndPanHtml();
-    dwv.gui.appendScrollHtml();
-    dwv.gui.appendDrawHtml();
-    dwv.gui.appendLivewireHtml();
-    
-    // append filter container HTML
-    dwv.gui.appendFilterHtml();
-    // append filters HTML
-    dwv.gui.filter.appendThresholdHtml();
-    dwv.gui.filter.appendSharpenHtml();
-    dwv.gui.filter.appendSobelHtml();
-    
-    // append help HTML
-    dwv.gui.appendHelpHtml(true);
-    dwv.gui.appendVersionHtml();
-    dwv.gui.appendUndoHtml();
-
+    // main application
+    var myapp = new dwv.App();
     // initialise the application
-    app.init();
-    
+    myapp.init({
+        "containerDivId": "dwv",
+        "fitToWindow": true,
+        "tools": ["Scroll", "Window/Level", "Zoom/Pan", "Draw", "Livewire", "Filter"],
+        "filters": ["Threshold", "Sharpen", "Sobel"],
+        "shapes": ["Line", "Protractor", "Rectangle", "Roi", "Ellipse"],
+        "gui": ["tool", "load", "help", "undo", "version", "tags"],
+        "isMobile": true
+    });
+
     var size = dwv.gui.getWindowSize();
-    $("#layerContainer").height(size.height);
+    $(".layerContainer").height(size.height);
 
 });
--- demo/stable/viewers/static/applauncher.js
+++ demo/stable/viewers/static/applauncher.js
@@ -4,94 +4,25 @@
 
 // check browser support
 dwv.browser.check();
-// main application
-var app = new dwv.App();
 
 // launch when page is loaded
 $(document).ready( function()
 {
-    $("#toggleInfoLayer").button({ icons: 
-        { primary: "ui-icon-comment" }, text: false });
-    // create dialogs
-    $("#openData").dialog({ position: 
-        {my: "left top", at: "left top", of: "#pageMain"} });
-    $("#toolbox").dialog({ position: 
-        {my: "left top+160", at: "left top", of: "#pageMain"} });
-    $("#history").dialog({ position: 
-        {my: "left top+350", at: "left top", of: "#pageMain"} });
-    $("#tags").dialog({ position: 
-        {my: "right top", at: "right top", of: "#pageMain"},
-        autoOpen: false, width: 500, height: 590 });
-    $("#help").dialog({ position: 
-        {my: "right top", at: "right top", of: "#pageMain"},
-        autoOpen: false, width: 500, height: 590 });
+    // gui setup
+    dwv.gui.setup();
     
-    // image dialog
-    $("#layerDialog").dialog({ position: 
-        {my: "left+320 top", at: "left top", of: "#pageMain"}});
-    // default size
-    $("#layerDialog").dialog({ width: "auto", resizable: false });
-    // Resizable but keep aspect ratio
-    // TODO it seems to add a border that bothers getting the cursor position...
-    //$("#layerContainer").resizable({ aspectRatio: true });
-
-    // Add required loaders to the loader list
-    dwv.io.loaders = {};
-    dwv.io.loaders.file = dwv.io.File;
-    dwv.io.loaders.url = dwv.io.Url;
-
-    // append load container HTML
-    dwv.gui.appendLoadboxHtml();
-    // append loaders HTML
-    dwv.gui.appendFileLoadHtml();
-    dwv.gui.appendUrlLoadHtml();
-    dwv.gui.displayFileLoadHtml(true);
-
-    // Add tools to the tool list
-    dwv.tool.tools = {};
-    dwv.tool.tools["Window/Level"] = new dwv.tool.WindowLevel(app);
-    dwv.tool.tools["Zoom/Pan"] = new dwv.tool.ZoomAndPan(app);
-    dwv.tool.tools.scroll = new dwv.tool.Scroll(app);
-    dwv.tool.tools.draw = new dwv.tool.Draw(app);
-    dwv.tool.tools.livewire = new dwv.tool.Livewire(app);
-    dwv.tool.tools.filter = new dwv.tool.Filter(app);
-
-    // Add filters to the filter list for the filter tool
-    dwv.tool.filters = {};
-    dwv.tool.filters.threshold = new dwv.tool.filter.Threshold(app);
-    dwv.tool.filters.sharpen = new dwv.tool.filter.Sharpen(app);
-    dwv.tool.filters.sobel = new dwv.tool.filter.Sobel(app);
-
-    // Add shapes to the shape list for the draw tool
-    dwv.tool.shapes = {};
-    dwv.tool.shapes.line = dwv.tool.LineCreator;
-    dwv.tool.shapes.rectangle = dwv.tool.RectangleCreator;
-    dwv.tool.shapes.roi = dwv.tool.RoiCreator;
-    dwv.tool.shapes.ellipse = dwv.tool.EllipseCreator;
-
-    // append tool container HTML
-    dwv.gui.appendToolboxHtml();
-    // append tools HTML
-    dwv.gui.appendWindowLevelHtml();
-    dwv.gui.appendZoomAndPanHtml();
-    dwv.gui.appendScrollHtml();
-    dwv.gui.appendDrawHtml();
-    dwv.gui.appendLivewireHtml();
-    
-    // append filter container HTML
-    dwv.gui.appendFilterHtml();
-    // append filters HTML
-    dwv.gui.filter.appendThresholdHtml();
-    dwv.gui.filter.appendSharpenHtml();
-    dwv.gui.filter.appendSobelHtml();
-    
-    // append help HTML
-    dwv.gui.appendHelpHtml(false);
-    dwv.gui.appendVersionHtml();
-    dwv.gui.appendUndoHtml();
-
+    // main application
+    var myapp = new dwv.App();
     // initialise the application
-    app.init();
+    myapp.init({
+        "containerDivId": "dwv",
+        "fitToWindow": true,
+        "tools": ["Scroll", "Window/Level", "Zoom/Pan", "Draw", "Livewire", "Filter"],
+        "filters": ["Threshold", "Sharpen", "Sobel"],
+        "shapes": ["Line", "Protractor", "Rectangle", "Roi", "Ellipse"],
+        "gui": ["tool", "load", "help", "undo", "version", "tags"],
+        "isMobile": false
+    });
     
     // help
     // TODO Seems accordion only works when at end...
