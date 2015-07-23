c48298f12a1bfe16e160ea85e0c48983acceeb8e
ivmartel <ivmartel@gmail.com>
2015-07-24 01:25:58 +0200

    Added missing usage of the QUnit assert.

--- tests/dicom/dicomParser.test.js
+++ tests/dicom/dicomParser.test.js
@@ -2,7 +2,7 @@
  * Tests for the 'dicom/dicomParser.js' file.
  */
 // Do not warn if these variables were not defined before.
-/* global QUnit, module, equal */
+/* global QUnit, module */
 module("dicomParser");
 
 QUnit.test("Test DICOM parsing.", 3, function (assert) {
@@ -28,10 +28,10 @@ QUnit.test("Test DICOM parsing.", 3, function (assert) {
         dicomParser.parse(this.response);
         var tags = dicomParser.getDicomElements();
         // check values
-        equal(tags.getFromName("Rows"), 256, "Number of rows");
-        equal(tags.getFromName("Columns"), 256, "Number of columns");
+        assert.equal(tags.getFromName("Rows"), 256, "Number of rows");
+        assert.equal(tags.getFromName("Columns"), 256, "Number of columns");
         // ReferencedImageSequence - ReferencedSOPInstanceUID 
-        equal(tags.getFromName("ReferencedImageSequence")[0].x00081155.value[0], 
+        assert.equal(tags.getFromName("ReferencedImageSequence")[0].x00081155.value[0], 
             "1.3.12.2.1107.5.2.32.35162.2012021515511672669154094", 
             "ReferencedImageSequence SQ");
         // start async test
