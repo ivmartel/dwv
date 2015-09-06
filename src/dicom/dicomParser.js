82ee957fe8d8cfaad56c31d57ba221d158ee0f15
ivmartel <ivmartel@gmail.com>
2015-09-07 00:40:27 +0200

    Added support for jpeg baseline 8bit encoded data. Added test. Relates to #61.

--- src/dicom/dicomParser.js
+++ src/dicom/dicomParser.js
@@ -326,25 +326,15 @@ dwv.dicom.splitGroupElementKey = function (key)
 };
 
 /**
- * Tell if a given syntax is a JPEG baseline 8bit one.
- * @method isJpegBaseline8bitTransferSyntax
+ * Tell if a given syntax is a JPEG baseline one.
+ * @method isJpegBaselineTransferSyntax
  * @param {String} The transfer syntax to test.
- * @returns {Boolean} True if a jpeg baseline 8bit syntax.
+ * @returns {Boolean} True if a jpeg baseline syntax.
  */
-dwv.dicom.isJpegBaseline8bitTransferSyntax = function(syntax)
+dwv.dicom.isJpegBaselineTransferSyntax = function(syntax)
 {
-    return syntax === "1.2.840.10008.1.2.4.50";
-};
-
-/**
- * Tell if a given syntax is a JPEG baseline 12bit one.
- * @method isJpegBaseline8bitTransferSyntax
- * @param {String} The transfer syntax to test.
- * @returns {Boolean} True if a jpeg baseline 12bit syntax.
- */
-dwv.dicom.isJpegBaseline12bitTransferSyntax = function(syntax)
-{
-    return syntax === "1.2.840.10008.1.2.4.51";
+    return syntax === "1.2.840.10008.1.2.4.50" ||
+        syntax === "1.2.840.10008.1.2.4.51";
 };
 
 /**
@@ -356,8 +346,7 @@ dwv.dicom.isJpegBaseline12bitTransferSyntax = function(syntax)
 dwv.dicom.isJpegNonSupportedTransferSyntax = function(syntax)
 {
     return ( syntax.match(/1.2.840.10008.1.2.4.5/) !== null &&
-        !dwv.dicom.isJpegBaseline8bitTransferSyntax() &&
-        !dwv.dicom.isJpegBaseline12bitTransferSyntax() &&
+        !dwv.dicom.isJpegBaselineTransferSyntax() &&
         !dwv.dicom.isJpegLosslessTransferSyntax() ) ||
         syntax.match(/1.2.840.10008.1.2.4.6/) !== null;
 };
@@ -421,13 +410,9 @@ dwv.dicom.getTransferSyntaxName = function (syntax)
     else if( syntax === "1.2.840.10008.1.2.2" ) {
         name = "Big Endian Explicit";
     }
-    // JPEG baseline 8bit
-    else if( dwv.dicom.isJpegBaseline8bitTransferSyntax(syntax) ) {
-        name = "JPEG Baseline 8bit";
-    }
-    // JPEG baseline 12bit
-    else if( dwv.dicom.isJpegBaseline12bitTransferSyntax(syntax) ) {
-        name = "JPEG Baseline 12bit";
+    // JPEG baseline
+    else if( dwv.dicom.isJpegBaselineTransferSyntax(syntax) ) {
+        name = "JPEG Baseline";
     }
     // JPEG Lossless
     else if( dwv.dicom.isJpegLosslessTransferSyntax(syntax) ) {
@@ -790,8 +775,7 @@ dwv.dicom.DicomParser.prototype.parse = function(buffer)
 {
     var offset = 0;
     var implicit = false;
-    var isJpegBaseline8bit = false;
-    var isJpegBaseline12bit = false;
+    var isJpegBaseline = false;
     var isJpegLossless = false;
     var isJpeg2000 = false;
     // default readers
@@ -845,15 +829,10 @@ dwv.dicom.DicomParser.prototype.parse = function(buffer)
     else if( syntax === "1.2.840.10008.1.2.2" ) {
         dataReader = new dwv.dicom.DataReader(buffer,false);
     }
-    // JPEG baseline 8bit
-    else if( dwv.dicom.isJpegBaseline8bitTransferSyntax(syntax) ) {
-        isJpegBaseline8bit = true;
-        throw new Error("Unsupported DICOM transfer syntax (JPEG basline 8bit): "+syntax);
-    }
-    // JPEG baseline 12bit
-    else if( dwv.dicom.isJpegBaseline12bitTransferSyntax(syntax) ) {
-        isJpegBaseline12bit = true;
-        console.log("JPEG Baseline 12bit compressed DICOM data: " + syntax);
+    // JPEG baseline
+    else if( dwv.dicom.isJpegBaselineTransferSyntax(syntax) ) {
+        isJpegBaseline = true;
+        console.log("JPEG Baseline compressed DICOM data: " + syntax);
     }
     // JPEG Lossless
     else if( dwv.dicom.isJpegLosslessTransferSyntax(syntax) ) {
@@ -995,7 +974,7 @@ dwv.dicom.DicomParser.prototype.parse = function(buffer)
         var decoded = decoder.decode();
         this.pixelBuffer = new Uint16Array(decoded.buffer);
     }
-    else if ( isJpegBaseline12bit ) {
+    else if ( isJpegBaseline ) {
         decoder = new JpegImage();
         decoder.parse( this.pixelBuffer );
         this.pixelBuffer = decoder.getData(decoder.width,decoder.height);
