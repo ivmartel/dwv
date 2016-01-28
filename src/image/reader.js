2563582f897b1deb0644f39e10462a41a7ea6ef9
ivmartel <ivmartel@gmail.com>
2016-01-29 00:04:06 +0100

    Moved thread classes to their own file.

--- src/image/reader.js
+++ src/image/reader.js
@@ -69,85 +69,7 @@ dwv.image.getDataFromImage = function(image)
 };
 
 
-
-function ThreadPool(size) {
-    var self = this;
- 
-    // set some defaults
-    this.taskQueue = [];
-    this.workerQueue = [];
-    this.poolSize = size;
- 
-    this.addWorkerTask = function(workerTask) {
-        if (self.workerQueue.length > 0) {
-            // get the worker from the front of the queue
-            var workerThread = self.workerQueue.shift();
-            workerThread.run(workerTask);
-        } else {
-            // no free workers,
-            self.taskQueue.push(workerTask);
-        }
-    };
- 
-    this.init = function() {
-        // create 'size' number of worker threads
-        for (var i = 0 ; i < size ; i++) {
-            self.workerQueue.push(new WorkerThread(self));
-        }
-    };
- 
-    this.freeWorkerThread = function(workerThread) {
-        if (self.taskQueue.length > 0) {
-            // don't put back in queue, but execute next task
-            var workerTask = self.taskQueue.shift();
-            workerThread.run(workerTask);
-        } else {
-            self.workerQueue.push(workerThread);
-        }
-    };
-}
- 
-// runner work tasks in the pool
-function WorkerThread(parentPool) {
- 
-    var self = this;
- 
-    this.parentPool = parentPool;
-    this.workerTask = {};
- 
-    this.run = function(workerTask) {
-        this.workerTask = workerTask;
-        // create a new web worker
-        if (this.workerTask.script !== null) {
-            var worker = new Worker(workerTask.script);
-            worker.addEventListener('message', dummyCallback, false);
-            worker.postMessage(workerTask.startMessage);
-        }
-    };
- 
-    // for now assume we only get a single callback from a worker
-    // which also indicates the end of this worker.
-    function dummyCallback(event) {
-        // pass to original callback
-        self.workerTask.callback(event);
- 
-        // we should use a separate thread to add the worker
-        self.parentPool.freeWorkerThread(self);
-        
-        this.terminate();
-    }
- 
-}
- 
-// task to run
-function WorkerTask(script, callback, msg) {
- 
-    this.script = script;
-    this.callback = callback;
-    this.startMessage = msg;
-}
-
-var pool = new ThreadPool(15);
+var pool = new dwv.utils.ThreadPool(15);
 pool.init();
 
 
@@ -187,7 +109,7 @@ dwv.image.getDataFromDicomBuffer = function(buffer, onLoad)
     }
     
     if ( script !== null ) {
-        var workerTask = new WorkerTask(script,callback,startMessage);
+        var workerTask = new dwv.utils.WorkerTask(script,callback,startMessage);
         pool.addWorkerTask(workerTask);
     }
     else {
