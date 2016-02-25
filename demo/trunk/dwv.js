f3c1113a94a6a92ccf76d37e83a4ea64bd982fcb
Travis <travis@travis-ci.org>
2016-02-25 23:03:21 +0000

    Travis build 637 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -12999,6 +12999,12 @@ dwv.io.Url.prototype.load = function (ioArray, requestHeaders)
     // text request
     var onLoadTextRequest = function (/*event*/)
     {
+        // status 200: "OK"
+        if (this.status !== 200) {
+            this.onerror();
+            return;
+        }
+
         try {
             onLoad( this.responseText );
         } catch (error) {
@@ -13009,6 +13015,12 @@ dwv.io.Url.prototype.load = function (ioArray, requestHeaders)
     // binary request
     var onLoadBinaryRequest = function (/*event*/)
     {
+        // status 200: "OK"
+        if (this.status !== 200) {
+            this.onerror();
+            return;
+        }
+        
         // find the image type from its signature
         var view = new DataView(this.response);
         var isJpeg = view.getUint32(0) === 0xffd8ffe0;
