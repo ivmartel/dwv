12de5570a1127fa20c6aa4b4a89c8e02330b4b73
ivmartel <ivmartel@gmail.com>
2016-02-27 01:50:45 +0100

    Allow for http 0 status for testing.

--- src/io/url.js
+++ src/io/url.js
@@ -220,8 +220,9 @@ dwv.io.Url.prototype.load = function (ioArray, requestHeaders)
     // load text buffer
     var onLoadTextBuffer = function (/*event*/)
     {
-        // status 200: "OK"
-        if (this.status !== 200) {
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Response_codes
+        // status 200: "OK"; status 0: "debug"
+        if (this.status !== 200 && this.status !== 0) {
             this.onerror();
             return;
         }
@@ -236,8 +237,9 @@ dwv.io.Url.prototype.load = function (ioArray, requestHeaders)
     // load binary buffer
     var onLoadBinaryBuffer = function (/*event*/)
     {
-        // status 200: "OK"
-        if (this.status !== 200) {
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Response_codes
+        // status 200: "OK"; status 0: "debug"
+        if (this.status !== 200 && this.status !== 0) {
             this.onerror();
             return;
         }
