9f55aa015c19113fc617184aaa06e4eb586342a0
ivmartel <ivmartel@gmail.com>
2016-03-02 00:51:57 +0100

    Updated to head.

--- demo/trunk/viewers/mobile/applauncher.js
+++ demo/trunk/viewers/mobile/applauncher.js
@@ -10,6 +10,31 @@ $(document).ready( function()
 {
     // main application
     var myapp = new dwv.App();
+
+    // display loading time
+    var listener = function (event) { 
+        if (event.type === "load-start") {
+            console.time("load-data");
+        }
+        else {
+            console.timeEnd("load-data");
+        }
+    };
+    // before myapp.init since it does the url load
+    myapp.addEventListener("load-start", listener);
+    myapp.addEventListener("load-end", listener);
+    
+    // also available:
+    //myapp.addEventListener("load-progress", listener);
+    //myapp.addEventListener("draw-create", listener);
+    //myapp.addEventListener("draw-move", listener);
+    //myapp.addEventListener("draw-change", listener);
+    //myapp.addEventListener("draw-delete", listener);
+    //myapp.addEventListener("wl-change", listener);
+    //myapp.addEventListener("colour-change", listener);
+    //myapp.addEventListener("position-change", listener);
+    //myapp.addEventListener("slice-change", listener);
+
     // initialise the application
     myapp.init({
         "containerDivId": "dwv",
@@ -23,18 +48,6 @@ $(document).ready( function()
         "useWebWorkers": true
     });
 
-    // example app listening
-    //var listener = function (event) { console.log("event: "+event.type); };
-    //myapp.addEventListener("load-end", listener);
-    //myapp.addEventListener("load-progress", listener);
-    //myapp.addEventListener("draw-create", listener);
-    //myapp.addEventListener("draw-move", listener);
-    //myapp.addEventListener("draw-change", listener);
-    //myapp.addEventListener("draw-delete", listener);
-    //myapp.addEventListener("wl-change", listener);
-    //myapp.addEventListener("colour-change", listener);
-    //myapp.addEventListener("position-change", listener);
-    //myapp.addEventListener("slice-change", listener);
 
     var size = dwv.gui.getWindowSize();
     $(".layerContainer").height(size.height);
