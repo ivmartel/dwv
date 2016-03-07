914b787dad978a27b4db4c48bb4392b6f77cd94d
ivmartel <ivmartel@gmail.com>
2016-03-07 22:41:48 +0100

    Bumped version from 0.14.0-beta to 0.14.0.

--- resources/dwv-mobile.lua
+++ resources/dwv-mobile.lua
@@ -103,7 +103,7 @@ print([[
 <script type="text/javascript" src="/dwv/ext/kinetic/kinetic-v5.1.1-06.10.min.js"></script>
 
 <!-- Local -->
-<script type="text/javascript" src="/dwv/dwv-0.14.0-beta.0.min.js"></script>
+<script type="text/javascript" src="/dwv/dwv-0.14.0.min.js"></script>
 <!-- Launch the app -->
 <script type="text/javascript" src="/dwv/viewers/mobile/appgui.js"></script>
 ]])
--- resources/dwv-simple.lua
+++ resources/dwv-simple.lua
@@ -95,7 +95,7 @@ print([[
 <script type="text/javascript" src="/dwv/ext/notmasteryet/jpg.js"></script>
 
 <!-- Local -->
-<script type="text/javascript" src="/dwv/dwv-0.14.0-beta.0.min.js"></script>
+<script type="text/javascript" src="/dwv/dwv-0.14.0.min.js"></script>
 <!-- Launch the app -->
 <script type="text/javascript" src="/dwv/viewers/simple/appgui.js"></script>
 ]])
