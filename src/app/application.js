5a66b297d008abecc0ae2894bbda4a9b5f5f2464
Rafael <rafa_soyyo@hotmail.com>
2016-04-26 19:16:29 +0200

    avoid error on different layout

--- src/app/application.js
+++ src/app/application.js
@@ -1051,8 +1051,10 @@ dwv.App = function ()
         self.initWLDisplay();
         // update preset select
         var select = self.getElement("presetSelect");
-        select.selectedIndex = 0;
-        dwv.gui.refreshElement(select);
+        if (select) {
+            select.selectedIndex = 0;
+            dwv.gui.refreshElement(select);
+        }
     };
 
 
