751671cfb5b73c07673b28aaa7c3ae6b4c452eb3
ivmartel <ivmartel@gmail.com>
2016-06-02 23:19:46 +0200

    Initial test data generator. Updated dicomWriter.

--- src/dicom/dicomWriter.js
+++ src/dicom/dicomWriter.js
@@ -98,8 +98,9 @@ dwv.dicom.DicomWriter = function (dicomElements) {
     // names: 'default', tagName or groupName
     // priority: tagName, groupName, default
     var rules = {
-        'default': {'action': 'remove', 'value': null },
+        'default': {'action': 'copy', 'value': null },
         'PatientName': {'action': 'replace', 'value': 'Anonymized'}, // tag
+        'Meta Element' : {'action': 'copy', 'value': null }, // group 'x0002'
         'Acquisition' : {'action': 'copy', 'value': null }, // group 'x0018'
         'Image Presentation' : {'action': 'copy', 'value': null }, // group 'x0028'
         'Procedure' : {'action': 'copy', 'value': null }, // group 'x0040'
@@ -130,67 +131,95 @@ dwv.dicom.DicomWriter = function (dicomElements) {
         }
         return result;
     };
+    
+    this.writeElement = function (element, writer, offset) {
+        // group
+        writer.writeHex(offset, element.group);
+        offset += 2;
+        // element
+        writer.writeHex(offset, element.element);
+        offset += 2;
+        // VR
+        writer.writeString(offset, element.vr);
+        offset += 2;
+        // VL
+        var isOtherVR = (element.vr[0] === 'O');
+        if ( isOtherVR ) {
+            offset += 2;
+            writer.writeUint32(offset, element.vl);
+            offset += 4;
+        }
+        else {
+            writer.writeUint16(offset, element.vl);
+            offset += 2;
+        }
+        // data
+        if ( element.vr === "OB") {
+            writer.writeUint8Array(offset, element.value);
+        }
+        else if ( element.vr === "US" || element.vr === "OW") {
+            writer.writeUint16Array(offset, element.value);
+        }
+        else if ( element.vr === "UL") {
+            writer.writeUint32Array(offset, element.value);
+        }
+        else {
+            writer.writeStringArray(offset, element.value);
+        }
+        offset += parseInt(element.vl, 10);
+        
+        return offset;
+    };
 
     this.getBuffer = function () {
         var keys = Object.keys(dicomElements);
         var element;
+        
+        // calculate buffer size
         var isOtherVR;
         var size = 128 + 4; // DICM
-        for ( var i = 0 ; i < keys.length; ++i ) {
+        for ( var i = 0; i < keys.length; ++i ) {
             element = this.getElementToWrite(dicomElements[keys[i]]);
             if ( element !== null ) {
                 isOtherVR = (element.vr[0] === 'O');
                 if ( isOtherVR ) {
                     size += 4;
                 }
-                size += 8 + element.vl;
+                size += 8 + parseInt(element.vl, 10);
             }
         }
         console.log("size: "+size);
 
+        // split meta and non meta elements
+        var metaElements = [];
+        var rawElements = [];
+        var groupName;
+        for ( var k = 0; k < keys.length; ++k ) {
+            element = this.getElementToWrite(dicomElements[keys[k]]);
+            if ( element !== null ) {
+                groupName = dwv.dicom.TagGroups[element.group.substr(1)]; // remove first 0
+                if ( groupName === 'Meta Element' ) {
+                    metaElements.push(element);
+                }
+                else {
+                    rawElements.push(element);
+                }
+            }
+        }
+        
+        // create buffer
         var buffer = new ArrayBuffer(size);
         var writer = new dwv.dicom.DataWriter(buffer);
         var offset = 128;
         writer.writeString(offset, "DICM");
         offset += 4;
-        for ( var j = 0 ; j < keys.length; ++j ) {
-            element = this.getElementToWrite(dicomElements[keys[j]]);
-            if ( element !== null ) {
-                // group
-                writer.writeHex(offset, element.group);
-                offset += 2;
-                // element
-                writer.writeHex(offset, element.element);
-                offset += 2;
-                // VR
-                writer.writeString(offset, element.vr);
-                offset += 2;
-                // VL
-                isOtherVR = (element.vr[0] === 'O');
-                if ( isOtherVR ) {
-                    offset += 2;
-                    writer.writeUint32(offset, element.vl);
-                    offset += 4;
-                }
-                else {
-                    writer.writeUint16(offset, element.vl);
-                    offset += 2;
-                }
-                // data
-                if ( element.vr === "OB") {
-                    writer.writeUint8Array(offset, element.value);
-                }
-                else if ( element.vr === "US" || element.vr === "OW") {
-                    writer.writeUint16Array(offset, element.value);
-                }
-                else if ( element.vr === "UL") {
-                    writer.writeUint32Array(offset, element.value);
-                }
-                else {
-                    writer.writeStringArray(offset, element.value);
-                }
-                offset += element.vl;
-            }
+        // write meta
+        for ( var l = 0; l < metaElements.length; ++l ) {
+            offset = this.writeElement(metaElements[l], writer, offset);
+        }
+        // write non meta
+        for ( var j = 0; j < rawElements.length; ++j ) {
+            offset = this.writeElement(rawElements[j], writer, offset);
         }
         return buffer;
     };
