0776b60ad1c40deeb31c74e83126e79b3d0c84ce
Travis <travis@travis-ci.org>
2016-07-21 00:15:06 +0000

    Travis build 796 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -8418,16 +8418,47 @@ dwv.browser.hasTypedArray = function()
     return "Uint8Array" in window && "Uint16Array" in window;
 };
 
+//only check at startup (since we propose a replacement)
+dwv.browser._hasTypedArraySlice = (typeof Uint8Array.prototype.slice !== "undefined");
+
+/**
+ * Browser check for typed array slice method.
+ * Missing in Internet Explorer 11.
+ */
+dwv.browser.hasTypedArraySlice = function()
+{
+    return dwv.browser._hasTypedArraySlice;
+};
+
+// only check at startup (since we propose a replacement)
+dwv.browser._hasFloat64Array = ("Float64Array" in window);
+
+/**
+ * Browser check for Float64Array array.
+ * Missing in PhantomJS 1.9.20 (on Travis).
+ */
+dwv.browser.hasFloat64Array = function()
+{
+    return dwv.browser._hasFloat64Array;
+};
+
+//only check at startup (since we propose a replacement)
+dwv.browser._hasClampedArray = ("Uint8ClampedArray" in window);
+
 /**
  * Browser check for clamped array.
+ * Missing in 
+ * - Safari 5.1.7 for Windows
+ * - PhantomJS 1.9.20 (on Travis).
  */
 dwv.browser.hasClampedArray = function()
 {
-    return "Uint8ClampedArray" in window;
+    return dwv.browser._hasClampedArray;
 };
 
 /**
  * Browser checks to see if it can run dwv. Throws an error if not.
+ * Silently replaces basic functions.
  * @todo Maybe use {@link http://modernizr.com/}.
  */
 dwv.browser.check = function()
@@ -8452,10 +8483,44 @@ dwv.browser.check = function()
         alert(message+appnorun);
         throw new Error(message);
     }
+    // Check typed array slice
+    if( !dwv.browser.hasTypedArraySlice() ) {
+        // silent fail with warning
+        console.warn("The TypedArray.slice method is not supported in this browser. This may impair performance. ");
+        // basic Uint16Array implementation
+        Uint16Array.prototype.slice = function (begin, end) {
+            var size = end - begin;
+            var cloned = new Uint16Array(size);
+            for (var i = 0; i < size; i++) {
+                cloned[i] = this[begin + i];
+            }
+            return cloned;
+        };
+        // basic Uint8Array implementation
+        Uint8Array.prototype.slice = function (begin, end) {
+            var size = end - begin;
+            var cloned = new Uint8Array(size);
+            for (var i = 0; i < size; i++) {
+                cloned[i] = this[begin + i];
+            }
+            return cloned;
+        };
+    }
     // check clamped array
     if( !dwv.browser.hasClampedArray() ) {
-        // silent fail since IE does not support it...
+        // silent fail with warning
         console.warn("The Uint8ClampedArray is not supported in this browser. This may impair performance. ");
+        // Use Uint8Array instead... Not good
+        // TODO Find better replacement!
+        window.Uint8ClampedArray = window.Uint8Array;
+    }
+    // check Float64 array
+    if( !dwv.browser.hasFloat64Array() ) {
+        // silent fail with warning
+        console.warn("The Float64Array is not supported in this browser. This may impair performance. ");
+        // Use Float32Array instead... Not good
+        // TODO Find better replacement!
+        window.Float64Array = window.Float32Array;
     }
 };
 ;// namespaces
@@ -12046,19 +12111,10 @@ dwv.image.lut.Window = function (rescaleLut, isSigned)
 {
     /**
      * The internal array: Uint8ClampedArray clamps between 0 and 255.
-     * (not supported on travis yet... using basic array, be sure not to overflow!)
      * @private
      * @type Array
      */
-    var windowLut = null;
-
-    // check Uint8ClampedArray support
-    if ( !dwv.browser.hasClampedArray() ) {
-        windowLut = new Uint8Array(rescaleLut.getLength());
-    }
-    else {
-        windowLut = new Uint8ClampedArray(rescaleLut.getLength());
-    }
+    var windowLut = new Uint8ClampedArray(rescaleLut.getLength());
 
     /**
      * The window center.
