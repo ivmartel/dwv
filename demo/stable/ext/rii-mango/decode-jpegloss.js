5177357316c0dd1ec5bc301ff23766474689d98e
ivmartel <ivmartel@gmail.com>
2016-08-17 01:38:11 +0200

    Fix decoder update...

--- demo/stable/ext/rii-mango/decode-jpegloss.js
+++ demo/stable/ext/rii-mango/decode-jpegloss.js
@@ -6,14 +6,32 @@
 
 importScripts('lossless-min.js'); 
 
-self.addEventListener('message', function (e) {
+self.addEventListener('message', function (event) {
     
+    // bytes per element
+    var bpe = event.data.bitsAllocated / 8;
     // decode DICOM buffer
-    var buf = new Uint8Array(e.data);
-    var decoder = new jpeg.lossless.Decoder(buf.buffer);
-    var decoded = decoder.decode();
+    var buf = new Uint8Array(event.data.buffer);
+    var decoder = new jpeg.lossless.Decoder();
+    var decoded = decoder.decode(buf.buffer, 0, buf.buffer.byteLength, bpe);
     // post decoded data
-    var res = new Uint16Array(decoded.buffer);
-    self.postMessage(res);
+    var res = null;
+    if (event.data.bitsAllocated === 8) {
+        if (event.data.isSigned) {
+            res = new Int8Array(decoded.buffer);
+        }
+        else {
+            res = new Uint8Array(decoded.buffer);
+        }
+    }
+    else if (event.data.bitsAllocated === 16) {
+        if (event.data.isSigned) {
+            res = new Int16Array(decoded.buffer);
+        }
+        else {
+            res = new Uint16Array(decoded.buffer);
+        }
+    }
+    self.postMessage([res]);
     
 }, false);
