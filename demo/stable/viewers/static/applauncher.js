547fbd02137071c3755a1b852fc2f93cf203663f
ivmartel <ivmartel@gmail.com>
2016-08-23 01:16:11 +0200

    Updated stable to v0.16.0

--- demo/stable/viewers/static/applauncher.js
+++ demo/stable/viewers/static/applauncher.js
@@ -26,6 +26,13 @@ function startApp() {
     $("#accordion").accordion({ collapsible: "true", active: "false", heightStyle: "content" });
 }
 
+// Image decoders (for web workers)
+dwv.image.decoderScripts = {
+    "jpeg2000": "../../ext/pdfjs/decode-jpeg2000.js",
+    "jpeg-lossless": "../../ext/rii-mango/decode-jpegloss.js",
+    "jpeg-baseline": "../../ext/notmasteryet/decode-jpegbaseline.js"
+};
+
 // check browser support
 dwv.browser.check();
 // initialise i18n
