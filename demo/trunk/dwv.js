f43882f7a69ee68d405bafcfb157050e0c31b98a
Travis <travis@travis-ci.org>
2016-09-26 22:29:47 +0000

    Travis build 826 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -3214,23 +3214,23 @@ dwv.dicom.DicomElementsWrapper = function (dicomElements) {
             }
             // name
             if ( dictElement !== null ) {
-                row.name = dictElement[2];
+                row[dwv.i18n("basics.name")] = dictElement[2];
             }
             else {
-                row.name = "Unknown Tag & Data";
+                row[dwv.i18n("basics.name")] = "Unknown Tag & Data";
             }
             // value
             if ( dicomElement.tag.name !== "x7FE00010" ) {
-                row.value = dicomElement.value;
+                row[dwv.i18n("basics.value")] = dicomElement.value;
             }
             else {
-                row.value = "...";
+                row[dwv.i18n("basics.value")] = "...";
             }
             // others
-            row.group = dicomElement.tag.group;
-            row.element = dicomElement.tag.element;
-            row.vr = dicomElement.vr;
-            row.vl = dicomElement.vl;
+            row[dwv.i18n("basics.group")] = dicomElement.tag.group;
+            row[dwv.i18n("basics.element")] = dicomElement.tag.element;
+            row[dwv.i18n("basics.vr")] = dicomElement.vr;
+            row[dwv.i18n("basics.vl")] = dicomElement.vl;
 
             table.push( row );
         }
@@ -8898,7 +8898,7 @@ dwv.html.appendHCell = function (row, text)
     if ( text !== "value" && text !== "name" ) {
         cell.setAttribute("data-priority", "1");
     }
-    cell.appendChild(document.createTextNode(dwv.i18n("basics." + text)));
+    cell.appendChild(document.createTextNode(text));
     row.appendChild(cell);
 };
 
@@ -19588,8 +19588,6 @@ dwv.browser.check = function()
 };
 ;// namespaces
 var dwv = dwv || {};
-/** @namespace */
-dwv.utils = dwv.utils || {};
 // external
 var i18next = i18next || {};
 var i18nextXHRBackend = i18nextXHRBackend || {};
@@ -19600,16 +19598,16 @@ var i18nextBrowserLanguageDetector = i18nextBrowserLanguageDetector || {};
 
 /**
  * Initialise i18n.
- * @param {String} language The language to translate to. Defaults to 'auto' and 
+ * @param {String} language The language to translate to. Defaults to 'auto' and
  *   gets the language from the browser.
- * @param {String} localesPath Path the locales directory.
+ * @param {String} localesPath Path to the locales directory.
  */
 dwv.i18nInitialise = function (language, localesPath)
 {
     var lng = (typeof language === "undefined") ? "auto" : language;
     var lpath = (typeof localesPath === "undefined") ? "" : localesPath;
     // i18n options: default 'en' language and
-    //  only load language, not specialised (for ex en-GB)  
+    //  only load language, not specialised (for ex en-GB)
     var options = {
         fallbackLng: "en",
         load: "languageOnly",
@@ -19627,13 +19625,37 @@ dwv.i18nInitialise = function (language, localesPath)
     // init i18n: will be ready when the 'loaded' event is fired
     i18n.init(options);
 };
-    
+
 /**
  * Handle i18n load event.
  * @param {Object} callback The callback function to call when i18n is loaded.
+ *  It can take one argument that will be replaced with the loaded languages.
  */
 dwv.i18nOnLoaded = function (callback) {
     i18next.on('loaded', callback);
+};
+
+/**
+ * Stop handling i18n load event.
+ */
+dwv.i18nOffLoaded = function () {
+    i18next.off('loaded');
+};
+
+/**
+ * Handle i18n failed load event.
+ * @param {Object} callback The callback function to call when i18n is loaded.
+ *  It can take three arguments: lng, ns and msg.
+ */
+dwv.i18nOnFailedLoad = function (callback) {
+    i18next.on('failedLoading', callback);
+};
+
+/**
+ * Stop handling i18n failed load event.
+ */
+dwv.i18nOffFailedLoad = function () {
+    i18next.off('failedLoading');
 };
 
 /**
@@ -19655,7 +19677,7 @@ dwv.i18nExists = function (key, options) {
 };
 
 /**
- * Translate all data-i18n tags in the current html page. If an html tag defines the 
+ * Translate all data-i18n tags in the current html page. If an html tag defines the
  * data-i18n attribute, its value will be used as key to find its corresponding text
  * and will replace the content of the html tag.
  */
@@ -19663,7 +19685,7 @@ dwv.i18nPage = function () {
     // get all elements
     var elements = document.getElementsByTagName("*");
     // if the element defines data-i18n, replace its content with the tranlation
-    for (var i = 0; i < elements.length; ++i) { 
+    for (var i = 0; i < elements.length; ++i) {
         if (typeof elements[i].dataset.i18n !== "undefined") {
             elements[i].innerHTML = dwv.i18n(elements[i].dataset.i18n);
         }
