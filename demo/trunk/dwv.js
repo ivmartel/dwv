aa670709eb3f8e010a096f67e771b7b73d6ed74f
Travis <travis@travis-ci.org>
2017-04-06 22:13:44 +0000

    Travis build 991 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -34,6 +34,13 @@
     i18nextBrowserLanguageDetector,
     Konva) {
 
+    // similar to what browserify does but reversed
+    //https://www.contentful.com/blog/2017/01/17/the-global-object-in-javascript/
+    var window = typeof window !== 'undefined' ? 
+        window : typeof self !== 'undefined' ? 
+        self : typeof global !== 'undefined' ?
+        global : {};
+
 /** @namespace */
 var dwv = dwv || {};
 
