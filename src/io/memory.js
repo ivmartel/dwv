a972d984899b333e67044a07778e55bbb6989508
ivmartel <ivmartel@gmail.com>
2017-05-05 23:59:18 +0200

    First draft of a memory buffer loader.

---
+++ src/io/memory.js
@@ -0,0 +1,156 @@
+// namespaces
+var dwv = dwv || {};
+dwv.io = dwv.io || {};
+
+/**
+ * Memory loader.
+ * @constructor
+ */
+dwv.io.Memory = function ()
+{
+    /**
+     * CLosure to self.
+     * @private
+     * @type Object
+     */
+    var self = this;
+
+    /**
+     * Number of data to load.
+     * @private
+     * @type Number
+     */
+    var nToLoad = 0;
+    /**
+     * Number of loaded data.
+     * @private
+     * @type Number
+     */
+    var nLoaded = 0;
+
+    /**
+     * The default character set (optional).
+     * @private
+     * @type String
+     */
+    var defaultCharacterSet;
+
+    /**
+     * Get the default character set.
+     * @return {String} The default character set.
+     */
+    this.getDefaultCharacterSet = function () {
+        return defaultCharacterSet;
+    };
+
+    /**
+     * Set the default character set.
+     * @param {String} characterSet The character set.
+     */
+    this.setDefaultCharacterSet = function (characterSet) {
+        defaultCharacterSet = characterSet;
+    };
+
+    /**
+     * Set the number of data to load.
+     * @param {Number} n The number of data to load.
+     */
+    this.setNToLoad = function (n) {
+        nToLoad = n;
+    };
+
+    /**
+     * Increment the number of loaded data
+     * and call onloadend if loaded all data.
+     */
+    this.addLoaded = function () {
+        nLoaded++;
+        if ( nLoaded === nToLoad ) {
+            self.onloadend();
+        }
+    };
+
+}; // class Memory
+
+/**
+ * Handle a load event.
+ * @param {Object} event The load event, 'event.target'
+ *  should be the loaded data.
+ * Default does nothing.
+ */
+dwv.io.Memory.prototype.onload = function (/*event*/) {};
+/**
+ * Handle a load end event.
+ * Default does nothing.
+ */
+dwv.io.Memory.prototype.onloadend = function () {};
+/**
+ * Handle a progress event.
+ * @param {Object} event The progress event.
+ * Default does nothing.
+ */
+dwv.io.Memory.prototype.onprogress = function (/*event*/) {};
+/**
+ * Handle an error event.
+ * @param {Object} event The error event, 'event.message'
+ *  should be the error message.
+ * Default does nothing.
+ */
+dwv.io.Memory.prototype.onerror = function (/*event*/) {};
+
+/**
+ * Load a list of buffers.
+ * @param {Array} ioArray The list of buffers to load.
+ */
+dwv.io.Memory.prototype.load = function (ioArray)
+{
+    // closure to self for handlers
+    var self = this;
+    // set the number of data to load
+    this.setNToLoad( ioArray.length );
+
+    var mproghandler = new dwv.utils.MultiProgressHandler(self.onprogress);
+    mproghandler.setNToLoad( ioArray.length );
+
+    // get loaders
+    var loaders = [];
+    for (var m = 0; m < dwv.io.loaderList.length; ++m) {
+        loaders.push( new dwv.io[dwv.io.loaderList[m]]() );
+    }
+
+    // set loaders callbacks
+    var loader = null;
+    for (var k = 0; k < loaders.length; ++k) {
+        loader = loaders[k];
+        loader.onload = self.onload;
+        loader.addLoaded = self.addLoaded;
+        loader.onerror = self.onerror;
+        loader.setOptions({
+            'defaultCharacterSet': this.getDefaultCharacterSet()
+        });
+        loader.onprogress = mproghandler.getUndefinedMonoProgressHandler(1);
+    }
+
+    // loop on I/O elements
+    for (var i = 0; i < ioArray.length; ++i)
+    {
+        var iodata = ioArray[i];
+
+        // find a loader
+        var foundLoader = false;
+        for (var l = 0; l < loaders.length; ++l) {
+            loader = loaders[l];
+            if (loader.canLoadUrl(iodata.filename)) {
+                foundLoader = true;
+                // read
+                loader.load(iodata.data, iodata.filename, i);
+                // next file
+                break;
+            }
+        }
+        // TODO: throw?
+        if (!foundLoader) {
+            throw new Error("No loader found for file: "+iodata.filename);
+        }
+    }
+};
