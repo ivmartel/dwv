02bf3a7b953f63a0bae7143e988cbfe0872b688b
ivmartel <ivmartel@gmail.com>
2017-07-03 23:38:27 +0200

    Loop optimisation.

--- src/dicom/dicomParser.js
+++ src/dicom/dicomParser.js
@@ -387,7 +387,7 @@ dwv.dicom.DataReader = function (buffer, isLittleEndian)
         if (textDecoder) {
             result = textDecoder.decode(buffer);
         } else {
-            for ( var i = 0; i < buffer.length; ++i ) {
+            for ( var i = 0, leni = buffer.length; i < leni; ++i ) {
                 result += String.fromCharCode( buffer[ i ] );
             }
         }
@@ -1089,7 +1089,7 @@ dwv.dicom.DicomParser.prototype.readDataElement = function (reader, offset, impl
         var raw = reader.readUint16Array( offset, vl );
         offset += vl;
         data = [];
-        for ( var i = 0; i < raw.length; i+=2 ) {
+        for ( var i = 0, leni = raw.length; i < leni; i+=2 ) {
             var stri = raw[i].toString(16);
             var stri1 = raw[i+1].toString(16);
             var str = "(";
@@ -1380,7 +1380,7 @@ dwv.dicom.DicomElementsWrapper = function (dicomElements) {
         var dicomElement = null;
         var dictElement = null;
         var row = null;
-        for ( var i = 0 ; i < keys.length; ++i ) {
+        for ( var i = 0, leni = keys.length; i < leni; ++i ) {
             dicomElement = dicomElements[keys[i]];
             row = {};
             // dictionnary entry (to get name)
@@ -1428,7 +1428,7 @@ dwv.dicom.DicomElementsWrapper = function (dicomElements) {
         }
         var dicomElement = null;
         var checkHeader = true;
-        for ( var i = 0 ; i < keys.length; ++i ) {
+        for ( var i = 0, leni = keys.length; i < leni; ++i ) {
             dicomElement = dicomElements[keys[i]];
             if ( checkHeader && dicomElement.tag.group !== "0x0002" ) {
                 result += "\n";
@@ -1501,7 +1501,7 @@ dwv.dicom.DicomElementsWrapper.prototype.getElementValueAsString = function ( di
             dicomElement.vr === "FD" ||
             dicomElement.vr === "DS");
         var valueStr = "";
-        for ( var k = 0; k < dicomElement.value.length; ++k ) {
+        for ( var k = 0, lenk = dicomElement.value.length; k < lenk; ++k ) {
             valueStr = "";
             if ( k !== 0 ) {
                 valueStr += "\\";
@@ -1675,7 +1675,7 @@ dwv.dicom.DicomElementsWrapper.prototype.getElementAsString = function ( dicomEl
     // continue for sequence
     if ( dicomElement.vr === 'SQ' ) {
         var item = null;
-        for ( var l = 0; l < dicomElement.value.length; ++l ) {
+        for ( var l = 0, lenl = dicomElement.value.length; l < lenl; ++l ) {
             item = dicomElement.value[l];
             var itemKeys = Object.keys(item);
             if ( itemKeys.length === 0 ) {
@@ -1698,7 +1698,7 @@ dwv.dicom.DicomElementsWrapper.prototype.getElementAsString = function ( dicomEl
             line += "\n";
             line += this.getElementAsString(itemElement, prefix + "  ");
 
-            for ( var m = 0; m < itemKeys.length; ++m ) {
+            for ( var m = 0, lenm = itemKeys.length; m < lenm; ++m ) {
                 if ( itemKeys[m] !== "xFFFEE000" ) {
                     line += "\n";
                     line += this.getElementAsString(item[itemKeys[m]], prefix + "    ");
@@ -1738,7 +1738,7 @@ dwv.dicom.DicomElementsWrapper.prototype.getElementAsString = function ( dicomEl
     // pixel sequence
     else if ( isPixSequence ) {
         var pixItem = null;
-        for ( var n = 0; n < dicomElement.value.length; ++n ) {
+        for ( var n = 0, lenn = dicomElement.value.length; n < lenn; ++n ) {
             pixItem = dicomElement.value[n];
             line += "\n";
             pixItem.vr = 'pi';
@@ -1785,14 +1785,16 @@ dwv.dicom.DicomElementsWrapper.prototype.getFromName = function ( name )
    var keys0 = Object.keys(dict);
    var keys1 = null;
    var k0 = 0;
+   var lenk0 = 0;
    var k1 = 0;
+   var lenk1 = 0;
    // label for nested loop break
    outLabel:
    // search through dictionary
-   for ( k0 = 0; k0 < keys0.length; ++k0 ) {
+   for ( k0 = 0, lenk0 = keys0.length; k0 < lenk0; ++k0 ) {
        group = keys0[k0];
        keys1 = Object.keys( dict[group] );
-       for ( k1 = 0; k1 < keys1.length; ++k1 ) {
+       for ( k1 = 0, lenk1 = keys1.length; k1 < lenk1; ++k1 ) {
            element = keys1[k1];
            if ( dict[group][element][2] === name ) {
                break outLabel;
