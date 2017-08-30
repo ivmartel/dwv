1905246601451fe97e8d84ddf1381226500a320f
ivmartel <ivmartel@gmail.com>
2017-08-30 21:17:04 +0200

    Update konva to v1.6.8. Fixes #380.
    Remove version from file name.

--- resources/conquest/dwv-mobile.lua
+++ resources/conquest/dwv-mobile.lua
@@ -100,7 +100,7 @@ print([[
 <script type="text/javascript" src="/dwv/ext/pdfjs/arithmetic_decoder.js"></script>
 <script type="text/javascript" src="/dwv/ext/pdfjs/jpg.js"></script>
 <script type="text/javascript" src="/dwv/ext/rii-mango/lossless-min.js"></script>
-<script type="text/javascript" src="/dwv/ext/ext/konva/konva-1.6.0.min.js"></script>
+<script type="text/javascript" src="/dwv/ext/ext/konva/konva.min.js"></script>
 
 <!-- Local -->
 <script type="text/javascript" src="/dwv/dwv-0.21.0-beta.min.js"></script>
