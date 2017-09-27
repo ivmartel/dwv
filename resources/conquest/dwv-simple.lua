7d8bde0e1802ae7271f0441878cbdbbfb5bfdda6
ivmartel <ivmartel@gmail.com>
2017-09-27 23:59:23 +0200

    Bumped version from 0.21.0-beta to 0.21.0.

--- resources/conquest/dwv-simple.lua
+++ resources/conquest/dwv-simple.lua
@@ -96,7 +96,7 @@ print([[
 <script type="text/javascript" src="/dwv/decoders/rii-mango/lossless-min.js"></script>
 
 <!-- Local -->
-<script type="text/javascript" src="/dwv/dwv-0.21.0-beta.min.js"></script>
+<script type="text/javascript" src="/dwv/dwv-0.21.0.min.js"></script>
 <!-- Launch the app -->
 <script type="text/javascript" src="/dwv/viewers/simple/appgui.js"></script>
 ]])
--- src/app/application.js
+++ src/app/application.js
@@ -77,7 +77,7 @@ dwv.App = function ()
      * Get the version of the application.
      * @return {String} The version of the application.
      */
-    this.getVersion = function () { return "v0.21.0-beta"; };
+    this.getVersion = function () { return "v0.21.0"; };
 
     /**
      * Get the image.
