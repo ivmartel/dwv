5c59cdc4d79572622aa502e4a18dcf35bde8d1cb
ivmartel <ivmartel@gmail.com>
2017-10-16 01:00:42 +0200

    More modular tests.

--- tests/dicom/dicomWriter.test.js
+++ tests/dicom/dicomWriter.test.js
@@ -6,6 +6,10 @@
 /* global QUnit */
 QUnit.module("dicomWriter");
 
+var dwv = dwv || {};
+dwv.utils = dwv.utils || {};
+dwv.utils.test = dwv.utils.test || {};
+
 /**
  * Tests for {@link dwv.dicom.DicomWriter} using simple DICOM data.
  * Using remote file for CI integration.
@@ -119,56 +123,98 @@ QUnit.test("Test patient anonymisation", function (assert) {
     request.send(null);
 });
 
-QUnit.test("Test synthetic dicom", function (assert) {
+/**
+ * Get a string representation of an object.
+ * TypedArray.toString can return '[object Uint8Array]' on old browsers
+ * (such as in PhantomJs).
+ * @param {Object} obj The input object
+ * @return {String} The string.
+ */
+dwv.utils.test.toString = function ( obj ) {
+    var res = obj.toString();
+    if ( res.substr(0,7) === "[object" &&
+        res.substr((res.length - 6),6) === "Array]") {
+        res = "";
+        for ( var i = 0; i < obj.length; ++i ) {
+            res += obj[i];
+            if ( i !== obj.length - 1 ) {
+                res += ",";
+            }
+        }
+    }
+    return res;
+};
+
+/**
+ * Compare JSON tags and DICOM elements
+ * @param {Object} jsonTags The JSON tags.
+ * @param {Object} dicomElements The DICOM elements
+ * @param {String} name The name of the test.
+ * @param {Object} comaprator An object with an equal function (such as Qunit assert).
+ */
+dwv.utils.test.compare = function ( jsonTags, dicomElements, name, comparator ) {
+    var keys = Object.keys(jsonTags);
+    for ( var k = 0; k < keys.length; ++k ) {
+        var tag = keys[k];
+        var tagGE = dwv.dicom.getGroupElementFromName(tag);
+        var tagKey = dwv.dicom.getGroupElementKey(tagGE.group, tagGE.element);
+        var element = dicomElements.getDEFromKey(tagKey);
+        var value = dicomElements.getFromKey(tagKey, true);
+        if ( element.vr !== "SQ" ) {
+            comparator.equal(dwv.utils.test.toString(value), jsonTags[tag], name + " - " + tag);
+        } else {
+            // supposing same order of subkeys and indices...
+            var subKeys = Object.keys(jsonTags[tag]);
+            var index = 0;
+            for ( var sk = 0; sk < subKeys.length; ++sk ) {
+                if ( subKeys[sk] !== "explicitLength" ) {
+                    var wrap = new dwv.dicom.DicomElementsWrapper(value[index]);
+                    dwv.utils.test.compare(jsonTags[tag][subKeys[sk]], wrap, name, comparator);
+                    ++index;
+                }
+            }
+        }
+    }
+};
+
+/**
+ * Test a JSON config.
+ * @param {Object} config A JSON config representing DICOM tags.
+ * @param {Object} assert A Qunit assert.
+ */
+dwv.utils.test.testConfig = function (config, assert) {
+    // convert JSON to DICOM element object
+    var res = dwv.dicom.getElementsFromJSONTags(config.tags);
+    var dicomElements = res.elements;
+    // pixels: small gradient square
+    dicomElements.x7FE00010 = dwv.dicom.generatePixelDataFromJSONTags(config.tags, res.offset);
+
+    // create DICOM buffer
+    var writer = new dwv.dicom.DicomWriter();
+    var dicomBuffer = null;
+    try {
+        dicomBuffer = writer.getBuffer(dicomElements);
+    } catch (error) {
+        assert.ok(false, "Caught error: "+error);
+        return;
+    }
+
+    // parse the buffer
+    var dicomParser = new dwv.dicom.DicomParser();
+    dicomParser.parse(dicomBuffer);
+    var elements = dicomParser.getDicomElements();
+
+    // compare contents
+    dwv.utils.test.compare(config.tags, elements, config.name, assert);
+}
+
+QUnit.test("Test synthetic dicom explicit", function (assert) {
     var done = assert.async();
-
-    // TypedArray.toString can return '[object Uint8Array]' on old browsers
-    //  (such as in PhantomJs)
-    var toString = function ( obj ) {
-        var res = obj.toString();
-        if ( res.substr(0,7) === "[object" &&
-            res.substr((res.length - 6),6) === "Array]") {
-            res = "";
-            for ( var i = 0; i < obj.length; ++i ) {
-                res += obj[i];
-                if ( i !== obj.length - 1 ) {
-                    res += ",";
-                }
-            }
-        }
-        return res;
-    };
-
-    // create string from JSON tags and DICOM elements
-    var compare = function ( jsonTags, dicomElements, name ) {
-        var keys = Object.keys(jsonTags);
-        for ( var k = 0; k < keys.length; ++k ) {
-            var tag = keys[k];
-            var tagGE = dwv.dicom.getGroupElementFromName(tag);
-            var tagKey = dwv.dicom.getGroupElementKey(tagGE.group, tagGE.element);
-            var element = dicomElements.getDEFromKey(tagKey);
-            var value = dicomElements.getFromKey(tagKey, true);
-            if ( element.vr !== "SQ" ) {
-                assert.equal(toString(value), jsonTags[tag], name);
-            } else {
-                // supposing same order of subkeys and indices...
-                var subKeys = Object.keys(jsonTags[tag]);
-                var index = 0;
-                for ( var sk = 0; sk < subKeys.length; ++sk ) {
-                    if ( subKeys[sk] !== "explicitLength" ) {
-                        var wrap = new dwv.dicom.DicomElementsWrapper(value[index]);
-                        compare(jsonTags[tag][subKeys[sk]], wrap, name);
-                        ++index;
-                    }
-                }
-            }
-        }
-    };
 
     // get the list of configs
     var request = new XMLHttpRequest();
     var urlRoot = "https://raw.githubusercontent.com/ivmartel/dwv/343-dicom-write";
-    var url = urlRoot + "/tests/dicom/synthetic-data.json";
+    var url = urlRoot + "/tests/dicom/synthetic-data_explicit.json";
     request.open('GET', url, true);
     request.onerror = function (event) {
         console.error(event);
@@ -176,32 +222,31 @@ QUnit.test("Test synthetic dicom", function (assert) {
     request.onload = function (/*event*/) {
         var configs = JSON.parse(this.responseText);
         for (var i = 0; i < configs.length; ++i ) {
-
-            // convert JSON to DICOM element object
-            var res = dwv.dicom.getElementsFromJSONTags(configs[i].tags);
-            var dicomElements = res.elements;
-            // pixels: small gradient square
-            dicomElements.x7FE00010 = dwv.dicom.generatePixelDataFromJSONTags(configs[i].tags, res.offset);
-
-            // create DICOM buffer
-            var writer = new dwv.dicom.DicomWriter();
-            var dicomBuffer = null;
-            try {
-                dicomBuffer = writer.getBuffer(dicomElements);
-            } catch (error) {
-                assert.ok(false, "Caught error: "+error);
-                return;
-            }
-
-            // parse the buffer
-            var dicomParser = new dwv.dicom.DicomParser();
-            dicomParser.parse(dicomBuffer);
-            var elements = dicomParser.getDicomElements();
-
-            // compare contents
-            compare(configs[i].tags, elements, configs[i].name);
+            dwv.utils.test.testConfig(configs[i], assert);
         }
+        // finish async test
+        done();
+    };
+    request.send(null);
 
+});
+
+QUnit.test("Test synthetic dicom implicit", function (assert) {
+    var done = assert.async();
+
+    // get the list of configs
+    var request = new XMLHttpRequest();
+    var urlRoot = "https://raw.githubusercontent.com/ivmartel/dwv/343-dicom-write";
+    var url = urlRoot + "/tests/dicom/synthetic-data_implicit.json";
+    request.open('GET', url, true);
+    request.onerror = function (event) {
+        console.error(event);
+    };
+    request.onload = function (/*event*/) {
+        var configs = JSON.parse(this.responseText);
+        for (var i = 0; i < configs.length; ++i ) {
+            dwv.utils.test.testConfig(configs[i], assert);
+        }
         // finish async test
         done();
     };
