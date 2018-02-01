bdbbdb2ab99f9b94f23a0fb2d4760c9403d00924
ivmartel <ivmartel@gmail.com>
2018-02-01 20:52:01 +0100

    Fix v0.2 state upgrading.

--- tests/state/state.test.js
+++ tests/state/state.test.js
@@ -7,30 +7,53 @@
 QUnit.module("state");
 
 /**
- * Create a XMLHttpRequest to retrieve state test data.
- * @param {String} filePath The state file path from the repository root.
- * @return {Object} The XMLHttpRequest.
+ * Test a state file.
+ * @param {String} version The state version.
+ * @param {String} type The type of drawing.
+ * @param {Object} assert The qunit assert.
  */
-dwv.utils.test.CreateStateTestRequest = function ( filePath ) {
+dwv.utils.test.TestState = function ( version, type, assert ) {
+    var done = assert.async();
+
     var request = new XMLHttpRequest();
     var urlRoot = "https://raw.githubusercontent.com/ivmartel/dwv/442-less-canvas";
-    var url = urlRoot + filePath;
+    var url = urlRoot + "/tests/state/v" + version + "/state-" + type;
+    if ( type === "arrow" ) {
+        url += "-yellow";
+    }
+    url += ".json";
     request.open('GET', url, true);
     request.onerror = function (event) {
         console.log(event);
     };
-    return request;
+    request.onload = function (/*event*/) {
+        // read state
+        var state = new dwv.State();
+        var jsonData = state.fromJSON( this.responseText );
+        // check drawings values
+        dwv.utils.test.CheckDrawings(
+            jsonData.drawings, jsonData.drawingsDetails, type, assert );
+        // delete drawing to allow simple equal check
+        delete jsonData.drawings;
+        delete jsonData.drawingsDetails;
+        // check values expect drawings
+        dwv.utils.test.CheckStateHeader( jsonData, version, assert );
+        // finish async test
+        done();
+    };
+    // send request
+    request.send(null);
 };
 
 /**
  * Check state header.
- * @param {Object} jsonData The input data to ckeck
+ * @param {Object} jsonData The input data to ckeck.
+ * @param {String} version The state version.
  * @param {Object} assert The qunit assert.
  */
-dwv.utils.test.CheckStateHeader = function (jsonData, assert) {
+dwv.utils.test.CheckStateHeader = function (jsonData, version, assert) {
     // header data
     var headerData = {
-        "version": "0.3",
         "window-center": 441,
         "window-width": 911,
         "position": { "i": 0, "j": 0, "k": 0 },
@@ -48,7 +71,7 @@ dwv.utils.test.CheckStateHeader = function (jsonData, assert) {
  * @param {Object} assert The qunit assert.
  * @param {String} type The type of drawing.
  */
-dwv.utils.test.CheckDrawings = function (drawings, details, assert, type) {
+dwv.utils.test.CheckDrawings = function (drawings, details, type, assert) {
     // first level: layer
     assert.equal( drawings.className, "Layer", "State drawings is a layer.");
     assert.equal( drawings.children.length, 1, "State drawings has one kid.");
@@ -61,11 +84,10 @@ dwv.utils.test.CheckDrawings = function (drawings, details, assert, type) {
     // third level: shape group
     var posGroupKid = layerKid.children[0];
     assert.equal( posGroupKid.className, "Group", "Position group first level is a group.");
-    assert.equal( posGroupKid.attrs.id, "pf8zteo5r4", "Position group first level has the proper id.");
-    assert.notEqual( typeof details.pf8zteo5r4, "undefined", "Details should contain data for id.");
+
     // shape specific checks
-    if ( type === "line" ) {
-        dwv.utils.test.CheckLineDrawing( posGroupKid, details.pf8zteo5r4, assert );
+    if ( type === "arrow" ) {
+        dwv.utils.test.CheckArrowDrawing( posGroupKid, details, assert );
     }
 };
 
@@ -75,22 +97,29 @@ dwv.utils.test.CheckDrawings = function (drawings, details, assert, type) {
  * @param {Object} details The drawing details
  * @param {Object} assert The qunit assert.
  */
-dwv.utils.test.CheckLineDrawing = function (posGroupKid, details, assert) {
+dwv.utils.test.CheckArrowDrawing = function (posGroupKid, details, assert) {
     // check group name
     assert.equal( posGroupKid.attrs.name, "line-group", "Shape group is a line group.");
+    assert.ok( posGroupKid.attrs.draggable,  "Shape group must be draggable.");
     assert.equal( posGroupKid.children.length, 3, "Shape group has 3 kids.");
+    assert.equal( posGroupKid.attrs.id, "pf8zteo5r4", "Position group first level has the proper id.");
+    assert.notEqual( typeof details.pf8zteo5r4, "undefined", "Details should contain data for id.");
+
     // real shape
     var shapeGroupKid0 = posGroupKid.children[0];
     assert.equal( shapeGroupKid0.className, "Line", "Shape group first level is a line.");
     assert.equal( shapeGroupKid0.attrs.name, "shape", "Shape group first level is a shape.");
+    assert.notOk( shapeGroupKid0.attrs.draggable,  "Shape group first level must not be draggable.");
     assert.deepEqual( shapeGroupKid0.attrs.points, [53, 136, 139, 89], "Line has the proper points.");
     assert.equal( shapeGroupKid0.attrs.stroke, "#ffff80", "Line has the proper colour.");
     // shape extra
     var shapeGroupKid1 = posGroupKid.children[1];
     assert.equal( shapeGroupKid1.className, "RegularPolygon", "Shape group second level is a polygon.");
+    assert.notOk( shapeGroupKid1.attrs.draggable,  "Shape group second level must not be draggable.");
     // label
     var shapeGroupKid2 = posGroupKid.children[2];
     assert.equal( shapeGroupKid2.className, "Label", "Shape group third level is a label.");
+    assert.notOk( shapeGroupKid2.attrs.draggable,  "Shape group third level must not be draggable.");
     assert.equal( shapeGroupKid2.children.length, 2, "Label has 2 kids.");
     var labelGroupKid0 = shapeGroupKid2.children[0];
     assert.equal( labelGroupKid0.className, "Text", "Label group first level is a text.");
@@ -99,37 +128,23 @@ dwv.utils.test.CheckLineDrawing = function (posGroupKid, details, assert) {
     assert.equal( labelGroupKid1.className, "Tag", "Label group second level is a tag.");
 
     // details
-    assert.equal( details.textExpr, "Eye", "Details textExpr has the proper value.");
-    assert.equal( details.longText, "", "Details longText has the proper value.");
+    var details0 = details.pf8zteo5r4;
+    assert.equal( details0.textExpr, "Eye", "Details textExpr has the proper value.");
+    assert.equal( details0.longText, "", "Details longText has the proper value.");
 };
 
 /**
- * Tests for {@link dwv.State}.
+ * Tests for {@link dwv.State} v0.2 containing an arrow.
  * @function module:tests/state
  */
-QUnit.test("Test read state.", function (assert) {
-    var done = assert.async();
-    // create request
-    var request = dwv.utils.test.CreateStateTestRequest(
-        "/tests/state/v0.3/state-arrow-yellow.json" );
-    // add request onload
-    request.onload = function (/*event*/) {
-        // read state
-        var state = new dwv.State();
-        var jsonData = state.fromJSON( this.responseText );
+QUnit.test("Test read v0.2 state: arrow.", function (assert) {
+    dwv.utils.test.TestState( "0.2", "arrow", assert );
+});
 
-        // check drawings values
-        dwv.utils.test.CheckDrawings(
-            jsonData.drawings, jsonData.drawingsDetails, assert, "line" );
-        // delete drawing to allow simple equal check
-        delete jsonData.drawings;
-        delete jsonData.drawingsDetails;
-        // check values expect drawings
-        dwv.utils.test.CheckStateHeader( jsonData, assert );
-
-        // finish async test
-        done();
-    };
-    // send request
-    request.send(null);
+/**
+ * Tests for {@link dwv.State} v0.3 containing an arrow.
+ * @function module:tests/state
+ */
+QUnit.test("Test read v0.3 state: arrow.", function (assert) {
+    dwv.utils.test.TestState( "0.3", "arrow", assert );
 });
