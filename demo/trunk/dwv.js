bc54c9d4122ad1365b81c7c7d6220817268dfd44
Travis <travis@travis-ci.org>
2018-03-02 23:02:50 +0000

    Travis build 1379 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -136,9 +136,6 @@ dwv.App = function ()
     // listeners
     var listeners = {};
 
-    // help resources path
-    var helpResourcesPath = "./";
-
     /**
      * Get the image.
      * @return {Image} The associated image.
@@ -227,14 +224,6 @@ dwv.App = function ()
      * @return {Object} The app style.
      */
     this.getStyle = function () { return style; };
-
-    /**
-     * Get the help resources path.
-     * @return {String} The path.
-     */
-    this.getHelpResourcesPath = function () {
-        return helpResourcesPath;
-    };
 
     /**
      * Add a command to the undo stack.
@@ -367,10 +356,6 @@ dwv.App = function ()
                 var isMobile = true;
                 if ( config.isMobile !== "undefined" ) {
                     isMobile = config.isMobile;
-                }
-                // help resources path
-                if ( typeof config.helpResourcesPath !== "undefined" ) {
-                    helpResourcesPath = config.helpResourcesPath;
                 }
                 dwv.gui.appendHelpHtml( toolboxController.getToolList(), isMobile, this );
             }
@@ -11010,6 +10995,10 @@ dwv.gui.base.appendHelpHtml = function(toolList, mobile, app)
 
     var toolHelpDiv = document.createElement("div");
 
+    // current location
+    var loc = window.location.pathname;
+    var dir = loc.substring(0, loc.lastIndexOf('/'));
+
     var tool = null;
     var tkeys = Object.keys(toolList);
     for ( var t=0; t < tkeys.length; ++t )
@@ -11032,7 +11021,7 @@ dwv.gui.base.appendHelpHtml = function(toolList, mobile, app)
                 var action = tool.getHelp()[actionType][keys[i]];
 
                 var img = document.createElement("img");
-                img.src = app.getHelpResourcesPath() + "/" + keys[i] + ".png";
+                img.src = dir + "/../../resources/help/"+keys[i]+".png";
                 img.style.float = "left";
                 img.style.margin = "0px 15px 15px 0px";
 
