3d4d311761b21b456e2803bafbae4b48bfa5a2c8
ivmartel <ivmartel@gmail.com>
2018-03-07 01:24:48 +0100

    Restrict linting to viewer src.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'viewers/**/*.js', 'tests/**/*.js'],
+            files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js', 'viewers/mobile/src/*.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
