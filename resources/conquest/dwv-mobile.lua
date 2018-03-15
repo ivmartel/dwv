ba87b60213bb92562f4d876b44a17b9845f1b2e3
ivmartel <ivmartel@gmail.com>
2018-03-15 23:13:23 +0100

    Bump version from 0.23.0 to 0.23.1-beta

--- resources/conquest/dwv-mobile.lua
+++ resources/conquest/dwv-mobile.lua
@@ -140,7 +140,7 @@ print([[
 
 print([[
 <!-- Local -->
-<script type="text/javascript" src="/dwv/dwv-0.23.0.min.js"></script>
+<script type="text/javascript" src="/dwv/dwv-0.23.1-beta.min.js"></script>
 <!-- Launch the app -->
 <script type="text/javascript" src="/dwv/viewers/mobile/appgui.js"></script>
 ]])
