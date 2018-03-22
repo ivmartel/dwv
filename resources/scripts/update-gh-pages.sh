c62b2e40c6a86728718b1202c7fbbe4aefb4c4c9
ivmartel <ivmartel@gmail.com>
2018-03-22 21:46:23 +0100

    Protect gh-pages update. Adapted to new tasks.

--- resources/scripts/update-gh-pages.sh
+++ resources/scripts/update-gh-pages.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 #Script to push build results on the repository gh-pages branch.
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
-  #copy data we're interested in to other place
-  cp -R build $HOME/built
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv.git gh-pages > /dev/null
-  #go into directory and copy data we're interested in to that directory
-  cd gh-pages
-  #clean doc dir
-  rm -Rf demo/trunk/doc/*
-  #copy new build
-  cp -Rf $HOME/built/dist/* demo/trunk
-  cp -Rf $HOME/built/doc demo/trunk
-  #add, commit and push files
-  git add -Af .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
-  echo -e "Done updating.\n"
-fi
+# we should be in /home/travis/build/ivmartel/dwv
+echo -e "Starting to update gh-pages\n"
+
+# build deploy version (result in ./build/dist)
+yarn run build || exit 1
+# create doc
+yarn run doc
+
+# send coverage to coveralls
+grunt coveralls
+
+# go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+# using token, clone gh-pages branch
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv.git gh-pages
+# clean up
+rm -Rf $HOME/gh-pages/demo/trunk/*
+# copy new build and doc
+cp -Rf $HOME/build/ivmartel/dwv/dist/* $HOME/gh-pages/demo/trunk
+cp -Rf $HOME/build/ivmartel/dwv/doc $HOME/gh-pages/demo/trunk
+# move to root of repo
+cd $HOME/gh-pages
+# add, commit and push files
+git add -Af .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push -fq origin gh-pages
+
+echo -e "Done updating.\n"
