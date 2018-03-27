90ec67511c6b218c3d9f619a689d61059ce4e73e
ivmartel <ivmartel@gmail.com>
2018-03-28 00:48:20 +0200

    Bump version from 0.23.3 to 0.24.0-beta.

--- src/dicom/dicomParser.js
+++ src/dicom/dicomParser.js
@@ -7,7 +7,7 @@ dwv.dicom = dwv.dicom || {};
  * Get the version of the library.
  * @return {String} The version of the library.
  */
-dwv.getVersion = function () { return "0.23.3"; };
+dwv.getVersion = function () { return "0.24.0-beta"; };
 
 /**
  * Clean string: trim and remove ending.
