342b477e8941b60f24b109a9fa787aa1107f586f
ivmartel <ivmartel@gmail.com>
2018-06-05 00:45:03 +0200

    Add access to the DICOM tags. Relates to #498.

--- src/app/application.js
+++ src/app/application.js
@@ -48,6 +48,7 @@ dwv.App = function ()
     var infoController = null;
 
     // Dicom tags gui
+    var tags = null;
     var tagsGui = null;
 
     // Drawing list gui
@@ -809,6 +810,15 @@ dwv.App = function ()
     };
 
     /**
+     * Get the data tags.
+     * @return {Object} The list of DICOM tags.
+     */
+    this.getTags = function ()
+    {
+        return tags;
+    };
+
+    /**
      * Get a list of drawing store details.
      * @return {Object} A list of draw details including id, text, quant...
      */
@@ -1374,6 +1384,7 @@ dwv.App = function ()
         viewController = new dwv.ViewController(view);
 
         // append the DICOM tags table
+        tags = data.info;
         if ( tagsGui ) {
             tagsGui.update(data.info);
         }
