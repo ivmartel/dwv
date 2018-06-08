f42e87de293196508d868c6aaa332e69378bd5a4
Travis <travis@travis-ci.org>
2018-06-08 21:40:54 +0000

    Travis build 1536 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -1,4 +1,4 @@
-/*! dwv 0.24.0-beta 2018-06-08 21:35:21 */
+/*! dwv 0.24.0-beta 2018-06-08 21:40:25 */
 // Inspired from umdjs
 // See https://github.com/umdjs/umd/blob/master/templates/returnExports.js
 (function (root, factory) {
@@ -20319,7 +20319,7 @@ dwv.tool.ArrowFactory.prototype.create = function (points, style/*, image*/)
     var verticalLine = new dwv.math.Line(line.getBegin(), beginTy);
     var angle = dwv.math.getAngle(line, verticalLine);
     var angleRad = angle * Math.PI / 180;
-    var radius = 5;
+    var radius = 5 * style.getScaledStrokeWidth();
     var kpoly = new Konva.RegularPolygon({
         x: line.getBegin().getX() + radius * Math.sin(angleRad),
         y: line.getBegin().getY() + radius * Math.cos(angleRad),
@@ -24120,8 +24120,10 @@ dwv.tool.RulerFactory.prototype.create = function (points, style, image)
         name: "shape"
     });
 
+    var tickLen = 10 * style.getScaledStrokeWidth();
+
     // tick begin
-    var linePerp0 = dwv.math.getPerpendicularLine( line, points[0], 10 );
+    var linePerp0 = dwv.math.getPerpendicularLine( line, points[0], tickLen );
     var ktick0 = new Konva.Line({
         points: [linePerp0.getBegin().getX(), linePerp0.getBegin().getY(),
                  linePerp0.getEnd().getX(), linePerp0.getEnd().getY() ],
@@ -24131,7 +24133,7 @@ dwv.tool.RulerFactory.prototype.create = function (points, style, image)
     });
 
     // tick end
-    var linePerp1 = dwv.math.getPerpendicularLine( line, points[1], 10 );
+    var linePerp1 = dwv.math.getPerpendicularLine( line, points[1], tickLen );
     var ktick1 = new Konva.Line({
         points: [linePerp1.getBegin().getX(), linePerp1.getBegin().getY(),
                  linePerp1.getEnd().getX(), linePerp1.getEnd().getY() ],
