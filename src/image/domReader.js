8b2e4c489cd911b9f9322e64313e4f5e47a5c9d5
frickt <thomasfrick001@gmail.com>
2018-09-25 08:34:46 +0200

    increase performance in internet explorer

--- src/image/domReader.js
+++ src/image/domReader.js
@@ -12,11 +12,12 @@ dwv.image.imageDataToBuffer = function (imageData) {
     // TODO support passing the full image data
     var dataLen = imageData.data.length;
     var buffer = new Uint8Array( (dataLen / 4) * 3);
+    var data = imageData.data;
     var j = 0;
     for( var i = 0; i < dataLen; i+=4 ) {
-        buffer[j] = imageData.data[i];
-        buffer[j+1] = imageData.data[i+1];
-        buffer[j+2] = imageData.data[i+2];
+        buffer[j] = data[i];
+        buffer[j+1] = data[i+1];
+        buffer[j+2] = data[i+2];
         j+=3;
     }
     return [buffer];
