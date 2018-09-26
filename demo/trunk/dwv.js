cbd4a235d66c63c2d517d89267e6f6bd968dfbd7
Travis <travis@travis-ci.org>
2018-09-26 22:48:27 +0000

    Travis build 1660 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -1,4 +1,4 @@
-/*! dwv 0.24.1 2018-09-26 22:37:56 */
+/*! dwv 0.24.1 2018-09-26 22:48:02 */
 // Inspired from umdjs
 // See https://github.com/umdjs/umd/blob/master/templates/returnExports.js
 (function (root, factory) {
