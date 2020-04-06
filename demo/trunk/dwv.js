8c12c7666326509c74ff415ac771b1f650cf65cc
Travis <travis@travis-ci.org>
2020-04-06 21:15:49 +0000

    Travis build 2118 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -1,4 +1,4 @@
-/*! dwv 0.28.0-beta 2020-04-05 22:14:36 */
+/*! dwv 0.28.0-beta 2020-04-06 21:15:25 */
 // Inspired from umdjs
 // See https://github.com/umdjs/umd/blob/master/templates/returnExports.js
 (function (root, factory) {
