1efcb3fe53d3ffc1665f4ec413200c7e5e193d5f
Travis <travis@travis-ci.org>
2020-04-11 22:38:16 +0000

    Travis build 2135 pushed to gh-pages

--- demo/trunk/dwv.js
+++ demo/trunk/dwv.js
@@ -1,4 +1,4 @@
-/*! dwv 0.28.0-beta 2020-04-11 22:22:40 */
+/*! dwv 0.28.0-beta 2020-04-11 22:37:51 */
 // Inspired from umdjs
 // See https://github.com/umdjs/umd/blob/master/templates/returnExports.js
 (function (root, factory) {
@@ -13433,212 +13433,6 @@ dwv.image = dwv.image || {};
 dwv.image.lut = dwv.image.lut || {};
 
 /**
- * Rescale LUT class.
- * Typically converts from integer to float.
- * @constructor
- * @param {Object} rsi The rescale slope and intercept.
- * @param {Number} bitsStored The number of bits used to store the data.
- */
-dwv.image.lut.Rescale = function (rsi, bitsStored)
-{
-    /**
-     * The internal array.
-     * @private
-     * @type Float32Array
-     */
-    var lut = null;
-
-    /**
-     * Flag to know if the lut is ready or not.
-     * @private
-     * @type Boolean
-     */
-    var isReady = false;
-
-    /**
-     * The size of the LUT array.
-     * @private
-     * @type Number
-     */
-    var length = Math.pow(2, bitsStored);
-
-    /**
-     * Get the Rescale Slope and Intercept (RSI).
-     * @return {Object} The rescale slope and intercept object.
-     */
-    this.getRSI = function () { return rsi; };
-
-    /**
-     * Is the lut ready to use or not? If not, the user must
-     * call 'initialise'.
-     * @return {Boolean} True if the lut is ready to use.
-     */
-    this.isReady = function () { return isReady; };
-
-    /**
-     * Initialise the LUT.
-     */
-    this.initialise = function ()
-    {
-        // check if already initialised
-        if (isReady) {
-            return;
-        }
-        // create lut and fill it
-        lut = new Float32Array(length);
-        for ( var i = 0; i < length; ++i ) {
-            lut[i] = rsi.apply(i);
-        }
-        // update ready flag
-        isReady = true;
-    };
-
-    /**
-     * Get the length of the LUT array.
-     * @return {Number} The length of the LUT array.
-     */
-    this.getLength = function () { return length; };
-
-    /**
-     * Get the value of the LUT at the given offset.
-     * @param {Number} offset The input offset in [0,2^bitsStored] range.
-     * @return {Number} The float32 value of the LUT at the given offset.
-     */
-    this.getValue = function (offset)
-    {
-        return lut[ offset ];
-    };
-};
-
-/**
- * Window LUT class.
-  * Typically converts from float to integer.
- * @constructor
- * @param {Number} rescaleLut The associated rescale LUT.
- * @param {Boolean} isSigned Flag to know if the data is signed or not.
- */
-dwv.image.lut.Window = function (rescaleLut, isSigned)
-{
-    /**
-     * The internal array: Uint8ClampedArray clamps between 0 and 255.
-     * @private
-     * @type Uint8ClampedArray
-     */
-    var lut = null;
-
-    /**
-     * The window level.
-     * @private
-     * @type {Object}
-     */
-    var windowLevel = null;
-
-    /**
-     * Flag to know if the lut is ready or not.
-     * @private
-     * @type Boolean
-     */
-    var isReady = false;
-
-    /**
-     * Shift for signed data.
-     * @private
-     * @type Number
-     */
-    var signedShift = 0;
-
-    /**
-     * Get the window / level.
-     * @return {Object} The window / level.
-     */
-    this.getWindowLevel = function () { return windowLevel; };
-    /**
-     * Get the signed flag.
-     * @return {Boolean} The signed flag.
-     */
-    this.isSigned = function () { return isSigned; };
-    /**
-     * Get the rescale lut.
-     * @return {Object} The rescale lut.
-     */
-    this.getRescaleLut = function () { return rescaleLut; };
-
-    /**
-     * Is the lut ready to use or not? If not, the user must
-     * call 'update'.
-     * @return {Boolean} True if the lut is ready to use.
-     */
-    this.isReady = function () { return isReady; };
-
-    /**
-     * Set the window center and width.
-     * @param {Object} wl The window level.
-     */
-    this.setWindowLevel = function (wl)
-    {
-        // store the window values
-        windowLevel = wl;
-        // possible signed shift
-        signedShift = 0;
-        windowLevel.setSignedOffset(0);
-        if ( isSigned ) {
-            var size = rescaleLut.getLength();
-            signedShift = size / 2;
-            windowLevel.setSignedOffset(rescaleLut.getRSI().getSlope() * signedShift);
-        }
-        // update ready flag
-        isReady = false;
-    };
-
-    /**
-     * Update the lut if needed..
-     */
-    this.update = function ()
-    {
-        // check if we need to update
-        if ( isReady ) {
-            return;
-        }
-
-        // check rescale lut
-        if (!rescaleLut.isReady()) {
-            rescaleLut.initialise();
-        }
-        // create window lut
-        var size = rescaleLut.getLength();
-        if (!lut) {
-            // use clamped array (polyfilled in env.js)
-            lut = new Uint8ClampedArray(size);
-        }
-        // by default WindowLevel returns a value in the [0,255] range
-        // this is ok with regular Arrays and ClampedArray.
-        for ( var i = 0; i < size; ++i )
-        {
-            lut[i] = windowLevel.apply( rescaleLut.getValue(i) );
-        }
-
-        // update ready flag
-        isReady = true;
-    };
-
-    /**
-     * Get the length of the LUT array.
-     * @return {Number} The length of the LUT array.
-     */
-    this.getLength = function () { return lut.length; };
-
-    /**
-     * Get the value of the LUT at the given offset.
-     * @param {Number} offset The input offset in [0,2^bitsStored] range.
-     * @return {Number} The integer value (default [0,255]) of the LUT at the given offset.
-     */
-    this.getValue = function (offset)
-    {
-        return lut[ offset + signedShift ];
-    };
-};
-
-/**
 * Lookup tables for image colour display.
 */
 
@@ -13802,6 +13596,86 @@ dwv.image.lut.test = {
    "green": dwv.image.lut.buildLut(dwv.image.lut.id),
    "blue":  dwv.image.lut.buildLut(dwv.image.lut.id)
 };*/
+
+// namespaces
+var dwv = dwv || {};
+dwv.image = dwv.image || {};
+
+/**
+ * Rescale LUT class.
+ * Typically converts from integer to float.
+ * @constructor
+ * @param {Object} rsi The rescale slope and intercept.
+ * @param {Number} bitsStored The number of bits used to store the data.
+ */
+dwv.image.RescaleLut = function (rsi, bitsStored)
+{
+    /**
+     * The internal array.
+     * @private
+     * @type Float32Array
+     */
+    var lut = null;
+
+    /**
+     * Flag to know if the lut is ready or not.
+     * @private
+     * @type Boolean
+     */
+    var isReady = false;
+
+    /**
+     * The size of the LUT array.
+     * @private
+     * @type Number
+     */
+    var length = Math.pow(2, bitsStored);
+
+    /**
+     * Get the Rescale Slope and Intercept (RSI).
+     * @return {Object} The rescale slope and intercept object.
+     */
+    this.getRSI = function () { return rsi; };
+
+    /**
+     * Is the lut ready to use or not? If not, the user must
+     * call 'initialise'.
+     * @return {Boolean} True if the lut is ready to use.
+     */
+    this.isReady = function () { return isReady; };
+
+    /**
+     * Initialise the LUT.
+     */
+    this.initialise = function () {
+        // check if already initialised
+        if (isReady) {
+            return;
+        }
+        // create lut and fill it
+        lut = new Float32Array(length);
+        for (var i = 0; i < length; ++i) {
+            lut[i] = rsi.apply(i);
+        }
+        // update ready flag
+        isReady = true;
+    };
+
+    /**
+     * Get the length of the LUT array.
+     * @return {Number} The length of the LUT array.
+     */
+    this.getLength = function () { return length; };
+
+    /**
+     * Get the value of the LUT at the given offset.
+     * @param {Number} offset The input offset in [0,2^bitsStored] range.
+     * @return {Number} The float32 value of the LUT at the given offset.
+     */
+    this.getValue = function (offset) {
+        return lut[offset];
+    };
+};
 
 // namespaces
 var dwv = dwv || {};
@@ -14305,10 +14179,10 @@ dwv.image.View = function (image)
             // create the first lut if none exists
             if (keys.length === 0) {
                 // create the rescale lookup table
-                var rescaleLut = new dwv.image.lut.Rescale(
+                var rescaleLut = new dwv.image.RescaleLut(
                     image.getRescaleSlopeAndIntercept(0), image.getMeta().BitsStored );
                 // create the window lookup table
-                var windowLut = new dwv.image.lut.Window(rescaleLut, image.getMeta().IsSigned);
+                var windowLut = new dwv.image.WindowLut(rescaleLut, image.getMeta().IsSigned);
                 this.addWindowLut(windowLut);
             }
 
@@ -14907,6 +14781,134 @@ dwv.image.WindowLevel.prototype.equals = function (rhs) {
  */
 dwv.image.WindowLevel.prototype.toString = function () {
     return (this.getCenter() + ", " + this.getWidth());
+};
+
+// namespaces
+var dwv = dwv || {};
+dwv.image = dwv.image || {};
+
+/**
+ * Window LUT class.
+  * Typically converts from float to integer.
+ * @constructor
+ * @param {Number} rescaleLut The associated rescale LUT.
+ * @param {Boolean} isSigned Flag to know if the data is signed or not.
+ */
+dwv.image.WindowLut = function (rescaleLut, isSigned)
+{
+    /**
+     * The internal array: Uint8ClampedArray clamps between 0 and 255.
+     * @private
+     * @type Uint8ClampedArray
+     */
+    var lut = null;
+
+    /**
+     * The window level.
+     * @private
+     * @type {Object}
+     */
+    var windowLevel = null;
+
+    /**
+     * Flag to know if the lut is ready or not.
+     * @private
+     * @type Boolean
+     */
+    var isReady = false;
+
+    /**
+     * Shift for signed data.
+     * @private
+     * @type Number
+     */
+    var signedShift = 0;
+
+    /**
+     * Get the window / level.
+     * @return {Object} The window / level.
+     */
+    this.getWindowLevel = function () { return windowLevel; };
+    /**
+     * Get the signed flag.
+     * @return {Boolean} The signed flag.
+     */
+    this.isSigned = function () { return isSigned; };
+    /**
+     * Get the rescale lut.
+     * @return {Object} The rescale lut.
+     */
+    this.getRescaleLut = function () { return rescaleLut; };
+
+    /**
+     * Is the lut ready to use or not? If not, the user must
+     * call 'update'.
+     * @return {Boolean} True if the lut is ready to use.
+     */
+    this.isReady = function () { return isReady; };
+
+    /**
+     * Set the window center and width.
+     * @param {Object} wl The window level.
+     */
+    this.setWindowLevel = function (wl) {
+        // store the window values
+        windowLevel = wl;
+        // possible signed shift
+        signedShift = 0;
+        windowLevel.setSignedOffset(0);
+        if (isSigned) {
+            var size = rescaleLut.getLength();
+            signedShift = size / 2;
+            windowLevel.setSignedOffset(rescaleLut.getRSI().getSlope() * signedShift);
+        }
+        // update ready flag
+        isReady = false;
+    };
+
+    /**
+     * Update the lut if needed..
+     */
+    this.update = function () {
+        // check if we need to update
+        if (isReady) {
+            return;
+        }
+
+        // check rescale lut
+        if (!rescaleLut.isReady()) {
+            rescaleLut.initialise();
+        }
+        // create window lut
+        var size = rescaleLut.getLength();
+        if (!lut) {
+            // use clamped array (polyfilled in env.js)
+            lut = new Uint8ClampedArray(size);
+        }
+        // by default WindowLevel returns a value in the [0,255] range
+        // this is ok with regular Arrays and ClampedArray.
+        for (var i = 0; i < size; ++i) {
+            lut[i] = windowLevel.apply( rescaleLut.getValue(i) );
+        }
+
+        // update ready flag
+        isReady = true;
+    };
+
+    /**
+     * Get the length of the LUT array.
+     * @return {Number} The length of the LUT array.
+     */
+    this.getLength = function () { return lut.length; };
+
+    /**
+     * Get the value of the LUT at the given offset.
+     * @param {Number} offset The input offset in [0,2^bitsStored] range.
+     * @return {Number} The integer value (default [0,255]) of the LUT at the given offset.
+     */
+    this.getValue = function (offset) {
+        return lut[offset + signedShift];
+    };
 };
 
 // namespaces
