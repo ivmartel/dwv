f2f1a3951b90247c405fb721eb97d60a11f6baf8
ivmartel <ivmartel@gmail.com>
2021-02-18 13:00:36 +0100

    Add doc.

--- src/math/ellipse.js
+++ src/math/ellipse.js
@@ -92,6 +92,9 @@ dwv.math.Ellipse.prototype.getWorldSurface = function (spacingX, spacingY) {
 
 /**
  * Get the rounded limits of the ellipse.
+ * (see https://en.wikipedia.org/wiki/Ellipse#Standard_equation)
+ * Ellipse formula: x*x / a*a + y*y / b*b = 1
+ * => y = (+-)(b/a) * sqrt(a*a - x*x)
  *
  * @returns {Array} The rounded limits.
  */
