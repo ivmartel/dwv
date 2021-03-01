7fe1b927b8805822af117e3e402397b6e902dd2c
ivmartel <ivmartel@gmail.com>
2021-03-01 16:52:46 +0100

    Add quantify test for ellipse.

--- tests/math/ellipse.test.js
+++ tests/math/ellipse.test.js
@@ -39,3 +39,42 @@ QUnit.test('Test Ellipse.', function (assert) {
   // equals: true
   assert.equal(e0.getWorldSurface(0.5, 0.25), Math.PI, 'getWorldSurface');
 });
+
+/**
+ * Tests for {@link dwv.math.Ellipse} quantification.
+ *
+ * @function module:tests/math~Ellipse
+ */
+QUnit.test('Test Ellipse quantify.', function (assert) {
+  var center0 = new dwv.math.Point2D(2, 2);
+  var e0 = new dwv.math.Ellipse(center0, 1, 2);
+  // view controller
+  var mockVc0 = {
+    canQuantifyImage: function () {
+      return true;
+    },
+    get2DSpacing: function () {
+      return [1, 1];
+    },
+    getCurrentPosition: function () {
+      return {k: 0};
+    },
+    getImageVariableRegionValues: function () {
+      return [0, 1, 1, 0, 0, 1, 1, 0];
+    }
+  };
+  var theoQuant0 = {
+    min: {value: 0, unit: ''},
+    max: {value: 1, unit: ''},
+    mean: {value: 0.5, unit: ''},
+    stdDev: {value: 0.5, unit: ''},
+    surface: {value: 0.06283185307179587, unit: undefined}
+  };
+  var resQuant0 = e0.quantify(mockVc0);
+  assert.equal(resQuant0.min.value, theoQuant0.min.value, 'quant min');
+  assert.equal(resQuant0.max.value, theoQuant0.max.value, 'quant max');
+  assert.equal(resQuant0.mean.value, theoQuant0.mean.value, 'quant mean');
+  assert.equal(resQuant0.stdDev.value, theoQuant0.stdDev.value, 'quant stdDev');
+  assert.equal(
+    resQuant0.surface.value, theoQuant0.surface.value, 'quant surface');
+});
