1da4f71d7e4f6c17e237aa083e051fdfcbea4fcf
ivmartel <ivmartel@gmail.com>
2021-05-12 11:05:05 +0200

    Add cielab colour conversion utils.

--- src/utils/colour.js
+++ src/utils/colour.js
@@ -20,3 +20,90 @@ dwv.utils.ybrToRgb = function (y, cb, cr) {
     b: y + 1.772 * (cb - 128)
   };
 };
+
+/**
+ * Unsigned int CIE LAB value to CIE LAB value.
+ *
+ * @param {object} triplet CIE LAB triplet as {l,a,b} with unsigned range.
+ * @returns {object} CIE XYZ triplet as {x,y,z} with CIE LAB range.
+ */
+dwv.utils.uintLabToLab = function (triplet) {
+  return {
+    l: triplet.l * 100 / 65536,
+    a: (triplet.a * 255 / 65536) - 128,
+    b: (triplet.b * 255 / 65536) - 128,
+  };
+};
+
+/**
+ * Convert CIE LAB to CIE XYZ (standard illuminant D65, 2degree 1931).
+ *
+ * @see https://en.wikipedia.org/wiki/CIELAB_color_space#From_CIELAB_to_CIEXYZ
+ *
+ * @param {object} triplet CIE LAB triplet as {l,a,b}.
+ * @returns {object} CIE XYZ triplet as {x,y,z}.
+ */
+dwv.utils.cielabToCiexyz = function (triplet) {
+  var func = function (x) {
+    var res = null;
+    if (x > 0.206896552) {
+      res = Math.pow(x, 3);
+    } else {
+      res = 0.128418549 * x - 0.017712903;
+    }
+    return res;
+  };
+
+  var l0 = (triplet.l + 16) / 116;
+  var d65 = {
+    x: 95.0489,
+    y: 100,
+    z: 108.884
+  };
+
+  return {
+    x: d65.x * func(l0 + triplet.a / 500),
+    y: d65.y * func(l0),
+    z: d65.z * func(l0 - triplet.b / 200)
+  };
+};
+
+/**
+ * Convert CIE XYZ to sRGB.
+ *
+ * @see https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_(CIE_XYZ_to_sRGB)
+ *
+ * @param {object} triplet CIE XYZ triplet as {l,a,b}.
+ * @returns {object} sRGB triplet as {r,g,b}.
+ */
+dwv.utils.ciexyzToSrgb = function (triplet) {
+  var gammaFunc = function (x) {
+    var res = null;
+    if (x <= 0.0031308) {
+      res = 12.92 * x;
+    } else {
+      res = 1.055 * Math.pow(x, 1 / 2.4) - 0.055;
+    }
+    return res;
+  };
+
+  var x = triplet.x / 100;
+  var y = triplet.y / 100;
+  var z = triplet.z / 100;
+
+  return {
+    r: Math.round(255 * gammaFunc(3.2406 * x - 1.5372 * y - 0.4986 * z)),
+    g: Math.round(255 * gammaFunc(-0.9689 * x + 1.8758 * y + 0.0415 * z)),
+    b: Math.round(255 * gammaFunc(0.0557 * x - 0.2040 * y + 1.0570 * z))
+  };
+};
+
+/**
+ * Convert CIE LAB to sRGB (standard illuminant D65).
+ *
+ * @param {object} triplet CIE LAB triplet as {l,a,b}.
+ * @returns {object} sRGB triplet as {x,y,z}.
+ */
+dwv.utils.cielabToSrgb = function (triplet) {
+  return dwv.utils.ciexyzToSrgb(dwv.utils.cielabToCiexyz(triplet));
+};
--- tests/utils/colour.test.js
+++ tests/utils/colour.test.js
@@ -6,6 +6,10 @@
 /* global QUnit */
 QUnit.module('utils');
 
+var isSimilar = function (a, b) {
+  return Math.abs(a - b) < 1e-6;
+};
+
 /**
  * Tests for {@link dwv.utils.ybrToRgb}.
  *
@@ -14,7 +18,7 @@ QUnit.module('utils');
 QUnit.test('Test ybrToRgb.', function (assert) {
   var rgb00 = dwv.utils.ybrToRgb(0, 0, 0);
   assert.equal(rgb00.r, -179.456, 'ybr 0,0,0: red');
-  assert.ok((rgb00.g - 135.459939) < 1e-6, 'ybr 0,0,0: green');
+  assert.ok(isSimilar(rgb00.g, 135.459839), 'ybr 0,0,0: green');
   assert.equal(rgb00.b, -226.816, 'ybr 0,0,0: blue');
 
   var rgb01 = dwv.utils.ybrToRgb(128, 128, 128);
@@ -26,4 +30,64 @@ QUnit.test('Test ybrToRgb.', function (assert) {
   assert.equal(rgb02.r, 433.054, 'ybr 255,255,255: red');
   assert.equal(rgb02.g, 120.59844, 'ybr 255,255,255: green');
   assert.equal(rgb02.b, 480.044, 'ybr 255,255,255: blue');
+});
+
+/**
+ * Tests for {@link dwv.utils.cielabToCiexyz}.
+ * ref: https://www.easyrgb.com/en/convert.php
+ *
+ * @function module:tests/utils~cielabToCiexyz
+ */
+QUnit.test('Test cielab to ciexyz.', function (assert) {
+  var lab00 = {l: 0, a: 0, b: 0};
+  var xyz00 = dwv.utils.cielabToCiexyz(lab00);
+  assert.ok(isSimilar(xyz00.x, 0), 'lab 0,0,0: x');
+  assert.ok(isSimilar(xyz00.y, 0), 'lab 0,0,0: y');
+  assert.ok(isSimilar(xyz00.z, 0), 'lab 0,0,0: z');
+
+  var lab01 = {l: 100, a: 0, b: 0};
+  var xyz01 = dwv.utils.cielabToCiexyz(lab01);
+  assert.equal(xyz01.x, 95.0489, 'lab 100,0,0: x');
+  assert.equal(xyz01.y, 100, 'lab 100,0,0: y');
+  assert.equal(xyz01.z, 108.884, 'lab 100,0,0: z');
+});
+
+/**
+ * Tests for {@link dwv.utils.ciexyzToSrgb}.
+ * ref: https://www.easyrgb.com/en/convert.php
+ *
+ * @function module:tests/utils~ciexyzToSrgb
+ */
+QUnit.test('Test ciexyz to srgb.', function (assert) {
+  var xyz00 = {x: 0, y: 0, z: 0};
+  var rgb00 = dwv.utils.ciexyzToSrgb(xyz00);
+  assert.equal(rgb00.r, 0, 'xyz 0,0,0: r');
+  assert.equal(rgb00.g, 0, 'xyz 0,0,0: g');
+  assert.equal(rgb00.b, 0, 'xyz 0,0,0: b');
+
+  var xyz01 = {x: 95.0489, y: 100, z: 108.884};
+  var rgb01 = dwv.utils.ciexyzToSrgb(xyz01);
+  assert.equal(rgb01.r, 255, 'xyz D65: r');
+  assert.equal(rgb01.g, 255, 'xyz D65: g');
+  assert.equal(rgb01.b, 255, 'xyz D65: b');
+});
+
+/**
+ * Tests for {@link dwv.utils.cielabToSrgb}.
+ * ref: https://www.easyrgb.com/en/convert.php
+ *
+ * @function module:tests/utils~cielabToSrgb
+ */
+QUnit.test('Test cielab to rgb.', function (assert) {
+  var lab00 = {l: 0, a: 0, b: 0};
+  var rgb00 = dwv.utils.cielabToSrgb(lab00);
+  assert.equal(rgb00.r, 0, 'lab 0,0,0: r');
+  assert.equal(rgb00.g, 0, 'lab 0,0,0: g');
+  assert.equal(rgb00.b, 0, 'lab 0,0,0: b');
+
+  var lab01 = {l: 100, a: 0, b: 0};
+  var rgb01 = dwv.utils.cielabToSrgb(lab01);
+  assert.equal(rgb01.r, 255, 'lab 100,0,0: r');
+  assert.equal(rgb01.g, 255, 'lab 100,0,0: g');
+  assert.equal(rgb01.b, 255, 'lab 100,0,0: b');
 });
