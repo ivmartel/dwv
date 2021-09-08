f621f9ca6e2e408339462b6ae8636d37086b61de
ivmartel <ivmartel@gmail.com>
2021-09-08 10:17:38 +0200

    Lint decoders

--- decoders/pdfjs/decode-jpegbaseline.js
+++ decoders/pdfjs/decode-jpegbaseline.js
@@ -2,17 +2,17 @@
  * JPEG Baseline decoder worker.
  */
 // Do not warn if these variables were not defined before.
-/* global importScripts, self, JpegImage */
+/* global importScripts, JpegImage */
 
-importScripts('jpg.js'); 
+importScripts('jpg.js');
 
 self.addEventListener('message', function (event) {
-    
-    // decode DICOM buffer
-    var decoder = new JpegImage();
-    decoder.parse( event.data.buffer );
-    // post decoded data
-    var res = decoder.getData(decoder.width,decoder.height);
-    self.postMessage([res]);
-    
+
+  // decode DICOM buffer
+  var decoder = new JpegImage();
+  decoder.parse(event.data.buffer);
+  // post decoded data
+  var res = decoder.getData(decoder.width, decoder.height);
+  self.postMessage([res]);
+
 }, false);
