0a4b9fa4af8a8edadcf2a7a3c8e2a9cd23c4bc73
ivmartel <ivmartel@gmail.com>
2022-05-06 17:13:53 +0200

    Update warn message

--- src/image/view.js
+++ src/image/view.js
@@ -723,7 +723,7 @@ dwv.image.View.prototype.getWindowLevelMinMax = function () {
   var width = max - min;
   // full black / white images, defaults to 1.
   if (width < 1) {
-    dwv.logger.warn('Zero or negative width, defaulting to one.');
+    dwv.logger.warn('Zero or negative window width, defaulting to one.');
     width = 1;
   }
   var center = min + width / 2;
