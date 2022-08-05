44f5a755519625f53c8daf0fa1113affa484ba38
ivmartel <ivmartel@gmail.com>
2022-08-05 12:48:35 +0200

    Use let and const

--- src/tools/brush.js
+++ src/tools/brush.js
@@ -1,5 +1,9 @@
 // namespaces
-var dwv = dwv || {};
+// import * as DicomWebViewer from 'dwv';
+// export const dwv = DicomWebViewer || {};
+
+// namespaces
+// var dwv = dwv || {};
 dwv.tool = dwv.tool || {};
 
 /**
@@ -13,7 +17,7 @@ dwv.tool = dwv.tool || {};
  * @returns {Array} The indices of the circle.
  */
 dwv.tool.getCircleIndices = function (geometry, position, radiuses, dims) {
-  var centerIndex = geometry.worldToIndex(position);
+  const centerIndex = geometry.worldToIndex(position);
   return dwv.math.getEllipseIndices(centerIndex, radiuses, dims);
 };
 
@@ -26,22 +30,22 @@ dwv.tool.getCircleIndices = function (geometry, position, radiuses, dims) {
  * @returns {Array} An array of origins (dwv.math.Point3D).
  */
 dwv.tool.getOriginsFromIndices = function (geometry, allOrigins, indices) {
-  var sorted = indices.sort(dwv.math.getIndexCompareFunction(2));
+  const sorted = indices.sort(dwv.math.getIndexCompareFunction(2)); //NOSONAR
 
-  var origin0 = geometry.indexToWorld(sorted[0]);
-  var origin1 = geometry.indexToWorld(sorted[sorted.length - 1]);
+  const origin0 = geometry.indexToWorld(sorted[0]);
+  const origin1 = geometry.indexToWorld(sorted[sorted.length - 1]);
 
-  var getEqualZ = function (point) {
+  const getEqualZ = function (point) {
     return function equalZ(x) {
       return x.getZ() === point.get(2);
     };
   };
 
-  var iStart = allOrigins.findIndex(getEqualZ(origin0));
+  let iStart = allOrigins.findIndex(getEqualZ(origin0));
   if (iStart === -1) {
     iStart = 0;
   }
-  var iEnd = allOrigins.findIndex(getEqualZ(origin1));
+  let iEnd = allOrigins.findIndex(getEqualZ(origin1));
   if (iEnd === -1) {
     iEnd = allOrigins.length - 1;
   }
@@ -57,9 +61,9 @@ dwv.tool.getOriginsFromIndices = function (geometry, allOrigins, indices) {
  * @returns {Array} An array of offsets.
  */
 dwv.tool.getOffsetsFromIndices = function (geometry, indices) {
-  var imageSize = geometry.getSize();
-  var offsets = [];
-  for (var i = 0; i < indices.length; ++i) {
+  const imageSize = geometry.getSize();
+  const offsets = [];
+  for (let i = 0; i < indices.length; ++i) {
     offsets.push(imageSize.indexToOffset(indices[i]));
   }
   return offsets;
@@ -78,7 +82,7 @@ dwv.tool.Brush = function (app) {
    * @private
    * @type {dwv.tool.Brush}
    */
-  var self = this;
+  const self = this;
 
   /**
    * Interaction start flag.
@@ -87,14 +91,14 @@ dwv.tool.Brush = function (app) {
    */
   this.started = false;
 
-  var mask = null;
-  var maskId = null;
+  let mask = null;
+  let maskId = null;
 
-  var brushSize = 10;
-  var brushColor = {r: 255, g: 0, b: 0};
-  var brushMode = 'add';
+  let brushSize = 2;
+  let brushColor = {r: 255, g: 0, b: 0};
+  let brushMode = 'add';
 
-  var uid = 0;
+  let uid = 0;
 
   /**
    * Get a mask slice.
@@ -105,19 +109,19 @@ dwv.tool.Brush = function (app) {
    */
   function createMaskImage(geometry, origin, meta) {
     // create data
-    var sizeValues = geometry.getSize().getValues();
+    const sizeValues = geometry.getSize().getValues();
     sizeValues[2] = 1;
-    var maskSize = new dwv.image.Size(sizeValues);
-    var maskGeometry = new dwv.image.Geometry(
+    const maskSize = new dwv.image.Size(sizeValues);
+    const maskGeometry = new dwv.image.Geometry(
       origin,
       maskSize,
       geometry.getSpacing(),
       geometry.getOrientation());
-    var values = new Uint8Array(maskSize.getDimSize(2) * 3);
+    const values = new Uint8Array(maskSize.getDimSize(2) * 3);
     values.fill(0);
     ++uid;
-    var uids = [uid];
-    var maskSlice = new dwv.image.Image(maskGeometry, values, uids);
+    const uids = [uid];
+    const maskSlice = new dwv.image.Image(maskGeometry, values, uids);
     maskSlice.setMeta(meta);
     maskSlice.setPhotometricInterpretation('RGB');
 
@@ -139,22 +143,22 @@ dwv.tool.Brush = function (app) {
     baseGeometry, allOrigins,
     maskGeometry, position, circleDims, radiuses, sliceMeta) {
     // circle indices in the image geometry
-    var circleIndices =
+    const circleIndices =
       dwv.tool.getCircleIndices(baseGeometry, position, radiuses, circleDims);
-    var origins =
+    const origins =
       dwv.tool.getOriginsFromIndices(baseGeometry, allOrigins, circleIndices);
     if (origins.length === 0) {
       throw new Error('No brush origins...');
     }
 
     // get origins that need to be added
-    var origins0 = [];
-    var originsBelow = [];
-    var isAbove = true;
-    var hasCommon = false;
+    const origins0 = [];
+    const originsBelow = [];
+    let isAbove = true;
+    let hasCommon = false;
     // origins start from the top of the circle, first ones (if any)
     // are above mask origins
-    for (var i = 0; i < origins.length; ++i) {
+    for (let i = 0; i < origins.length; ++i) {
       if (!maskGeometry.includesOrigin(origins[i])) {
         if (isAbove) {
           origins0.push(origins[i]);
@@ -168,7 +172,7 @@ dwv.tool.Brush = function (app) {
     }
 
     // append slices if needed
-    var originsToAdd = [];
+    let originsToAdd = [];
 
     if (hasCommon) {
       // common slice case
@@ -176,30 +180,30 @@ dwv.tool.Brush = function (app) {
       originsToAdd = origins0.reverse().concat(originsBelow);
     } else {
       // non common slice case: add in between slices
-      var maskOrigins = maskGeometry.getOrigins();
-      var firstMask = maskOrigins[0];
-      var lastMask = maskOrigins[maskOrigins.length - 1];
-      var firstOrigin = origins0[0];
-      var lastOrigin = origins0[origins0.length - 1];
-      var distanceAbove = firstMask.getDistance(lastOrigin);
-      var distanceBelow = lastMask.getDistance(firstOrigin);
+      const maskOrigins = maskGeometry.getOrigins();
+      const firstMask = maskOrigins[0];
+      const lastMask = maskOrigins[maskOrigins.length - 1];
+      const firstOrigin = origins0[0];
+      const lastOrigin = origins0[origins0.length - 1];
+      const distanceAbove = firstMask.getDistance(lastOrigin);
+      const distanceBelow = lastMask.getDistance(firstOrigin);
 
-      var imageOrigins = app.getImage(0).getGeometry().getOrigins();
+      const imageOrigins = app.getImage(0).getGeometry().getOrigins();
       if (distanceAbove < distanceBelow) {
         // in between above
-        var i00 = imageOrigins.findIndex(
+        const i00 = imageOrigins.findIndex(
           dwv.math.getEqualPoint3DFunction(lastOrigin));
-        var i01 = imageOrigins.findIndex(
+        const i01 = imageOrigins.findIndex(
           dwv.math.getEqualPoint3DFunction(firstMask));
 
         // reverse to go from existing mask to new one
         originsToAdd = imageOrigins.slice(i00 + 1, i01).reverse().concat(
-          origins0.reverse());
+          origins0.reverse()); //NOSONAR
       } else {
         // in between bellow
-        var i10 = imageOrigins.findIndex(
+        const i10 = imageOrigins.findIndex(
           dwv.math.getEqualPoint3DFunction(lastMask));
-        var i11 = imageOrigins.findIndex(
+        const i11 = imageOrigins.findIndex(
           dwv.math.getEqualPoint3DFunction(firstOrigin));
 
         originsToAdd = imageOrigins.slice(i10 + 1, i11).concat(origins0);
@@ -208,7 +212,7 @@ dwv.tool.Brush = function (app) {
 
     // append slices
     // TODO: add image multi slice append?
-    for (var l = 0; l < originsToAdd.length; ++l) {
+    for (let l = 0; l < originsToAdd.length; ++l) {
       mask.getMeta().numberOfFiles += 1;
       mask.appendSlice(
         createMaskImage(maskGeometry, originsToAdd[l], sliceMeta));
@@ -223,9 +227,9 @@ dwv.tool.Brush = function (app) {
    * @param {Array} offsets The mask offsets.
    */
   function paintMaskAtOffsets(offsets) {
-    var buff = mask.getBuffer();
-    for (var i = 0; i < offsets.length; ++i) {
-      var offset = offsets[i] * 3;
+    const buff = mask.getBuffer();
+    for (let i = 0; i < offsets.length; ++i) {
+      const offset = offsets[i] * 3;
       buff[offset] = brushMode === 'add' ? brushColor.r : 0;
       buff[offset + 1] = brushMode === 'add' ? brushColor.g : 0;
       buff[offset + 2] = brushMode === 'add' ? brushColor.b : 0;
@@ -243,30 +247,30 @@ dwv.tool.Brush = function (app) {
    * @returns {Array} The array of offset to paint.
    */
   function getMaskOffsets(event) {
-    var layerDetails = dwv.gui.getLayerDetailsFromEvent(event);
-    var layerGroup = app.getLayerGroupById(layerDetails.groupId);
+    const layerDetails = dwv.gui.getLayerDetailsFromEvent(event);
+    const layerGroup = app.getLayerGroupById(layerDetails.groupId);
 
     // reference image related vars
-    var viewLayer = layerGroup.getActiveViewLayer();
-    var planePos = viewLayer.displayToPlanePos(event._x, event._y);
-    var viewController = viewLayer.getViewController();
-    var position = viewController.getPositionFromPlanePoint(planePos);
+    const viewLayer = layerGroup.getActiveViewLayer();
+    const planePos = viewLayer.displayToPlanePos(event._x, event._y);
+    const viewController = viewLayer.getViewController();
+    const position = viewController.getPositionFromPlanePoint(planePos);
 
-    var sliceMeta = {
+    const sliceMeta = {
       Modality: 'SEG',
       IsSigned: false,
       numberOfFiles: 1,
       BitsStored: 8
     };
 
-    var searchMaskMeta = {
+    const searchMaskMeta = {
       Modality: 'SEG'
     };
-    var isMaskVc = viewController.equalImageMeta(searchMaskMeta);
+    const isMaskVc = viewController.equalImageMeta(searchMaskMeta);
 
     // base image geometry
-    var baseImage = app.getImage(0);
-    var baseGeometry = baseImage.getGeometry();
+    const baseImage = app.getImage(0);
+    let baseGeometry = baseImage.getGeometry();
 
     if (isMaskVc) {
       // udpate an existing mask
@@ -276,12 +280,12 @@ dwv.tool.Brush = function (app) {
     }
 
     // create mask if not done yet
-    var maskVl = viewLayer;
-    var maskVc = viewController;
+    let maskVl = viewLayer;
+    let maskVc = viewController;
     if (!mask) {
-      var imgK = baseGeometry.worldToIndex(position).get(2);
+      const imgK = baseGeometry.worldToIndex(position).get(2);
 
-      var firstSliceMeta = sliceMeta;
+      const firstSliceMeta = sliceMeta;
       firstSliceMeta.SeriesInstanceUID =
         baseImage.getMeta().SeriesInstanceUID;
       firstSliceMeta.ImageOrientationPatient =
@@ -293,7 +297,7 @@ dwv.tool.Brush = function (app) {
       maskId = app.addNewImage(mask, {});
 
       // newly create mask case: find the SEG view layer
-      var maskViewLayers = layerGroup.searchViewLayers(searchMaskMeta);
+      const maskViewLayers = layerGroup.searchViewLayers(searchMaskMeta);
       if (maskViewLayers.length === 0) {
         console.warn('No mask view layers');
       } else if (maskViewLayers.length !== 1) {
@@ -307,15 +311,15 @@ dwv.tool.Brush = function (app) {
     sliceMeta.ImageOrientationPatient =
       mask.getMeta().ImageOrientationPatient;
 
-    var maskGeometry = mask.getGeometry();
+    const maskGeometry = mask.getGeometry();
 
-    var scrollIndex = viewController.getScrollIndex();
-    var circleDims;
-    var radiuses;
-    var spacing = baseGeometry.getSpacing(baseGeometry.getOrientation());
-    var r0 = Math.round(brushSize / spacing.get(0));
-    var r1 = Math.round(brushSize / spacing.get(1));
-    var r2 = Math.round(brushSize / spacing.get(2));
+    const scrollIndex = viewController.getScrollIndex();
+    let circleDims;
+    let radiuses;
+    const spacing = baseGeometry.getSpacing(baseGeometry.getOrientation());
+    const r0 = Math.round(brushSize / spacing.get(0));
+    const r1 = Math.round(brushSize / spacing.get(1));
+    const r2 = Math.round(brushSize / spacing.get(2));
     if (scrollIndex === 0) {
       circleDims = [1, 2];
       radiuses = [r1, r2];
@@ -327,7 +331,7 @@ dwv.tool.Brush = function (app) {
       radiuses = [r0, r1];
     }
 
-    var addedSlices = addMaskSlices(
+    const addedSlices = addMaskSlices(
       baseGeometry, baseImage.getGeometry().getOrigins(),
       maskGeometry, position, circleDims, radiuses, sliceMeta);
 
@@ -337,9 +341,9 @@ dwv.tool.Brush = function (app) {
     }
 
     // circle indices in the mask geometry
-    var maskPlanePos = maskVl.displayToPlanePos(event._x, event._y);
-    var maskPosition = maskVc.getPositionFromPlanePoint(maskPlanePos);
-    var maskCircleIndices = dwv.tool.getCircleIndices(
+    const maskPlanePos = maskVl.displayToPlanePos(event._x, event._y);
+    const maskPosition = maskVc.getPositionFromPlanePoint(maskPlanePos);
+    const maskCircleIndices = dwv.tool.getCircleIndices(
       maskGeometry, maskPosition, radiuses, circleDims);
 
     return dwv.tool.getOffsetsFromIndices(maskGeometry, maskCircleIndices);
@@ -369,8 +373,8 @@ dwv.tool.Brush = function (app) {
     if (!self.started) {
       return;
     }
-    var diffX = Math.abs(event._x - self.x0);
-    var diffY = Math.abs(event._y - self.y0);
+    const diffX = Math.abs(event._x - self.x0);
+    const diffY = Math.abs(event._y - self.y0);
     if (diffX > brushSize / 2 || diffY > brushSize / 2) {
       paintMaskAtOffsets(getMaskOffsets(event));
       self.x0 = event._x;
