5ccc469cbcec9ef2d35d97d00ac4ad50523f937f
ivmartel <ivmartel@gmail.com>
2023-02-23 16:34:28 +0100

    Copy decoders folder, update viewer.js

--- resources/scripts/prep-deploy.sh
+++ resources/scripts/prep-deploy.sh
@@ -42,20 +42,37 @@ fi
 
 info "Preparing deploy for '$fileName'"
 
-# start source comment
-a0="\(<!-- dwv sources -->\)"
-b0="\1<!--"
-sed -i "s/${a0}/${b0}/g" $fileName
-# end source comment, remove start build comment
-a1="\(<!-- dwv build -->\)\(<!--\)"
-b1="-->\1"
-sed -i "s/${a1}/${b1}/g" $fileName
-# remove end build comment
-a2="\(-->\)\(<!-- local -->\)"
-b2="\2"
-sed -i "s/${a2}/${b2}/g" $fileName
+if [ "$(grep -c "<!-- dwv sources -->" $fileName)" -eq 1 ]
+then
+  info "Switching dwv source to dwv build"
+  # start source comment
+  a0="\(<!-- dwv sources -->\)"
+  b0="\1<!--"
+  sed -i "s/${a0}/${b0}/g" $fileName
+  # end source comment, remove start build comment
+  a1="\(<!-- dwv build -->\)\(<!--\)"
+  b1="-->\1"
+  sed -i "s/${a1}/${b1}/g" $fileName
+  # remove end build comment
+  a2="\(-->\)\(<!-- local -->\)"
+  b2="\2"
+  sed -i "s/${a2}/${b2}/g" $fileName
+fi
 
-# change path to node_modules
-a3="src=\"..\/..\/node_modules\/"
-b3="src=\".\/node_modules\/"
-sed -i "s/${a3}/${b3}/g" $fileName
+if [ "$(grep -c "../../node_modules" $fileName)" -eq 3 ]
+then
+  info "Move to local node_modules"
+  # change path to node_modules
+  a3="src=\"..\/..\/node_modules\/"
+  b3="src=\".\/node_modules\/"
+  sed -i "s/${a3}/${b3}/g" $fileName
+fi
+
+if [ "$(grep -c ": '../../decoders" $fileName)" -eq 4 ]
+then
+  info "Move to local decoders"
+  # change path to decoders
+  a4=": '..\/..\/decoders\/"
+  b4=": '.\/decoders\/"
+  sed -i "s/${a4}/${b4}/g" $fileName
+fi
