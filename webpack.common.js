4e21a758a1349e2f33b6397283f0c3dac04939ae
ivmartel <ivmartel@gmail.com>
2023-03-22 10:38:26 +0100

    Webpack config for library

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,21 @@ module.exports = {
   output: {
     filename: 'dwv.min.js',
     library: 'dwv',
+    libraryTarget: 'umd',
+    globalObject: 'this',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  externals: {
+    konva: {
+      commonjs: 'konva',
+      commonjs2: 'konva',
+      amd: 'konva'
+    },
+    jszip: {
+      commonjs: 'jszip',
+      commonjs2: 'jszip',
+      amd: 'jszip'
+    }
+  }
 };
