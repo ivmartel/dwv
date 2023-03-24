cbeed8264dc65629eb03d9b1a699952a46785c6b
ivmartel <ivmartel@gmail.com>
2023-03-24 19:06:48 +0100

    Use webpack html to cenerate test pages

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,53 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    open: '/tests',
     static: [
-      './dist',
-    ]
+      {
+        directory: './dist',
+        publicPath: 'dist'
+      },
+      {
+        directory: './tests',
+        publicPath: '/tests'
+      },
+      {
+        directory: './decoders',
+        publicPath: '/decoders'
+      }
+    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './tests/pacs/dcmweb.html',
+      filename: 'tests/pacs/dcmweb.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './tests/pacs/viewer.html',
+      filename: 'tests/pacs/viewer.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './tests/visual/index-jpeg.html',
+      filename: 'tests/visual/index-jpeg.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './tests/visual/index-jpeg2000.html',
+      filename: 'tests/visual/index-jpeg2000.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './tests/visual/index-rle.html',
+      filename: 'tests/visual/index-rle.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './tests/visual/index.html',
+      filename: 'tests/visual/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -30,6 +69,10 @@ module.exports = merge(common, {
             plugins: ['istanbul']
           }
         }
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
       },
       {
         test: /\.json/,
