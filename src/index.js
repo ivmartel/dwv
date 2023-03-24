b3adfd05496de7601cc0640f48c57c6c532f3924
ivmartel <ivmartel@gmail.com>
2023-03-24 19:07:15 +0100

    Revise exports, keep groups

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import {
   getDwvVersion,
   DicomParser
-} from './dicom/dicomParser';
+} from './dicom/dicomParser.js';
+import {DicomWriter} from './dicom/dicomWriter';
+import {App} from './app/application';
+import {loadFromUri} from './utils/uri';
+import {precisionRound} from './utils/string';
+import {Point} from './math/point';
+import {decoderScripts} from './image/decoder';
+import {buildMultipart} from './utils/array';
+import {logger} from './utils/logger';
 
-export {getDwvVersion, DicomParser};
+const dicom = {
+  DicomParser,
+  DicomWriter
+};
+const image = {
+  decoderScripts
+};
+const math = {
+  Point
+};
+const utils = {
+  loadFromUri,
+  precisionRound,
+  buildMultipart
+};
+
+export {
+  getDwvVersion,
+  logger,
+  App,
+  dicom,
+  image,
+  math,
+  utils
+};
