6035795725385fb0724215fe6358224b07e98d9d
ivmartel <ivmartel@gmail.com>
2023-05-03 18:38:08 +0200

    Fix jsdoc tag-lines

--- .eslint-full.js
+++ .eslint-full.js
@@ -9,6 +9,9 @@ module.exports = {
   rules: {
     // no undefined type
     // https://github.com/gajus/eslint-plugin-jsdoc#user-content-eslint-plugin-jsdoc-rules-no-undefined-types
-    'jsdoc/no-undefined-types': 'off'
+    'jsdoc/no-undefined-types': 'off',
+    // tag lines
+    // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-tag-lines
+    'jsdoc/tag-lines': ['error', 'any', {'startLines': 1}]
   }
 };
