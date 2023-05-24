a1042e757180cd07375e3fc5bfee6910354fb409
ivmartel <ivmartel@gmail.com>
2023-05-24 12:17:51 +0200

    Extract get data and time methods, add tests

--- src/dicom/dicomElementsWrapper.js
+++ src/dicom/dicomElementsWrapper.js
@@ -219,29 +219,12 @@ export class DicomElementsWrapper {
       dicomElement.undefinedLength) {
       str = '(PixelSequence)';
     } else if (dicomElement.vr === 'DA' && pretty) {
-      const daValue = dicomElement.value[0];
-      // Two possible date formats:
-      // - standard 'YYYYMMDD'
-      // - non-standard 'YYYY.MM.DD' (previous ACR-NEMA)
-      let monthBeginIndex = 4;
-      let dayBeginIndex = 6;
-      if (daValue.length !== 8) {
-        monthBeginIndex = 5;
-        dayBeginIndex = 8;
-      }
-      const da = new Date(
-        parseInt(daValue.substring(0, 4), 10),
-        parseInt(daValue.substring(
-          monthBeginIndex, monthBeginIndex + 2), 10) - 1, // 0-11 range
-        parseInt(daValue.substring(
-          dayBeginIndex, dayBeginIndex + 2), 10));
+      const daObj = getDate(dicomElement);
+      const da = new Date(daObj.year, daObj.monthIndex, daObj.day);
       str = da.toLocaleDateString();
     } else if (dicomElement.vr === 'TM' && pretty) {
-      const tmValue = dicomElement.value[0];
-      const tmHour = tmValue.substring(0, 2);
-      const tmMinute = tmValue.length >= 4 ? tmValue.substring(2, 4) : '00';
-      const tmSeconds = tmValue.length >= 6 ? tmValue.substring(4, 6) : '00';
-      str = tmHour + ':' + tmMinute + ':' + tmSeconds;
+      const tmObj = getTime(dicomElement);
+      str = tmObj.hours + ':' + tmObj.minutes + ':' + tmObj.seconds;
     } else {
       let isOtherVR = false;
       if (dicomElement.vr.length !== 0) {
@@ -597,6 +580,106 @@ export function getPixelUnit(elements) {
     }
   }
   return unit;
+}
+
+/**
+ * Get a 'date' object with {year, monthIndex, day} ready for the
+ *   Date constructor from a DICOM element with vr=DA.
+ *
+ * @param {object} element The DICOM element with date information.
+ * @returns {{year, monthIndex, day}|undefined} The 'date' object.
+ */
+export function getDate(element) {
+  if (typeof element === 'undefined') {
+    return undefined;
+  }
+  if (element.value.length !== 1) {
+    return undefined;
+  }
+  const daValue = element.value[0];
+  // Two possible formats:
+  // - standard 'YYYYMMDD'
+  // - non-standard 'YYYY.MM.DD' (previous ACR-NEMA)
+  let monthBeginIndex = 4;
+  let dayBeginIndex = 6;
+  if (daValue.length === 10) {
+    monthBeginIndex = 5;
+    dayBeginIndex = 8;
+  }
+  const daYears = parseInt(daValue.substring(0, 4), 10);
+  // 0-11 range
+  const daMonthIndex = daValue.length >= monthBeginIndex + 2
+    ? parseInt(daValue.substring(
+      monthBeginIndex, monthBeginIndex + 2), 10) - 1 : 0;
+  const daDay = daValue.length === dayBeginIndex + 2
+    ? parseInt(daValue.substring(
+      dayBeginIndex, dayBeginIndex + 2), 10) : 0;
+  return {
+    year: daYears,
+    monthIndex: daMonthIndex,
+    day: daDay
+  };
+}
+
+/**
+ * Get a time object with {hours, minutes, seconds} ready for the
+ *   Date constructor from a DICOM element with vr=TM.
+ *
+ * @param {object} element The DICOM element with date information.
+ * @returns {{hours, minutes, seconds, milliseconds}|undefined} The time object.
+ */
+export function getTime(element) {
+  if (typeof element === 'undefined') {
+    return undefined;
+  }
+  if (element.value.length !== 1) {
+    return undefined;
+  }
+  // format: HH[MMSS.FFFFFF]
+  const tmValue = element.value[0];
+  const tmHours = parseInt(tmValue.substring(0, 2), 10);
+  const tmMinutes = tmValue.length >= 4
+    ? parseInt(tmValue.substring(2, 4), 10) : 0;
+  const tmSeconds = tmValue.length >= 6
+    ? parseInt(tmValue.substring(4, 6), 10) : 0;
+  const tmFracSecondsStr = tmValue.length >= 8
+    ? tmValue.substring(7, 10) : 0;
+  const tmMilliSeconds = tmFracSecondsStr === 0 ? 0
+    : parseInt(tmFracSecondsStr, 10) *
+      Math.pow(10, 3 - tmFracSecondsStr.length);
+  return {
+    hours: tmHours,
+    minutes: tmMinutes,
+    seconds: tmSeconds,
+    milliseconds: tmMilliSeconds
+  };
+}
+
+/**
+ * Get a 'dateTime' object with {date, time} ready for the
+ *   Date constructor from a DICOM element with vr=DT.
+ *
+ * @param {object} element The DICOM element with date-time information.
+ * @returns {{date, time}|undefined} The time object.
+ */
+export function getDateTime(element) {
+  if (typeof element === 'undefined') {
+    return undefined;
+  }
+  if (element.value.length !== 1) {
+    return undefined;
+  }
+  // format: YYYYMMDDHHMMSS.FFFFFF&ZZXX
+  const dtFullValue = element.value[0];
+  // remove offset (&ZZXX)
+  const dtValue = dtFullValue.split('&')[0];
+  const dtDate = getDate({value: [dtValue.substring(0, 8)]});
+  const dtTime = dtValue.length >= 9
+    ? getTime({value: [dtValue.substring(8)]}) : undefined;
+  return {
+    date: dtDate,
+    time: dtTime
+  };
 }
 
 /**
