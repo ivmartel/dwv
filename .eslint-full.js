123176c4e5a368caf07e4431d3d2c11666c108f2
ivmartel <ivmartel@gmail.com>
2023-06-06 16:47:06 +0200

    Update jsdoc lint configuration

--- .eslint-full.js
+++ .eslint-full.js
@@ -4,12 +4,9 @@ module.exports = {
   ],
   extends: [
     '.eslintrc.js',
-    'plugin:jsdoc/recommended',
+    'plugin:jsdoc/recommended-typescript-flavor',
   ],
   rules: {
-    // no undefined type
-    // https://github.com/gajus/eslint-plugin-jsdoc#user-content-eslint-plugin-jsdoc-rules-no-undefined-types
-    'jsdoc/no-undefined-types': 'off',
     // tag lines
     // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-tag-lines
     'jsdoc/tag-lines': ['error', 'any', {'startLines': 1}]
