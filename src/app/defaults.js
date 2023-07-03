8823094d2f4891e58f7ca381d4218c9cb2eb06ac
ivmartel <ivmartel@gmail.com>
2023-07-03 16:13:40 +0200

    Add comment

--- src/app/defaults.js
+++ src/app/defaults.js
@@ -1,3 +1,10 @@
+// linting 'type {Object.<bla, bla>}' will give:
+// warning  Use object shorthand or index signatures instead of `Object`,
+//   e.g., `{[key: string]: string}` jsdoc/check-types
+// pb: jsdoc does not support the object shorthand
+//   and ignoring will give vscode warning since the doc linting is not
+//   activated by default.
+
 // Overridable default object.
 export const defaults = {
   /**
