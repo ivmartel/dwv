2635b8c1d1916bce4839510ae7d67f46c40c6fa1
ivmartel <ivmartel@gmail.com>
2024-07-17 12:35:06 +0200

    Make app propagate data events

--- src/app/application.js
+++ src/app/application.js
@@ -614,10 +614,51 @@ export class App {
     this.#dataController.addEventListener('dataremove', this.#fireEvent);
     this.#dataController.addEventListener('dataimageset', this.#fireEvent);
     this.#dataController.addEventListener('dataupdate', this.#fireEvent);
+    // propage individual data events
+    this.#dataController.addEventListener('dataadd', (event) => {
+      this.#bindDataEvents(event.dataid);
+    });
+    this.#dataController.addEventListener('dataremove', (event) => {
+      this.#unbindDataEvents(event.dataid);
+    });
     // create stage
     this.#stage = new Stage();
     if (typeof this.#options.binders !== 'undefined') {
       this.#stage.setBinders(this.#options.binders);
+    }
+  }
+
+  /**
+   * Propagate data events.
+   *
+   * @param {string} dataId The data to bind.
+   */
+  #bindDataEvents(dataId) {
+    const data = this.getData(dataId);
+    if (typeof data.annotationGroup !== 'undefined') {
+      data.annotationGroup.addEventListener(
+        'annotationadd', this.#fireEvent);
+      data.annotationGroup.addEventListener(
+        'annotationupdate', this.#fireEvent);
+      data.annotationGroup.addEventListener(
+        'annotationremove', this.#fireEvent);
+    }
+  }
+
+  /**
+   * Stop propagating data events.
+   *
+   * @param {string} dataId The data to unbind.
+   */
+  #unbindDataEvents(dataId) {
+    const data = this.getData(dataId);
+    if (typeof data.annotationGroup !== 'undefined') {
+      data.annotationGroup.removeEventListener(
+        'annotationadd', this.#fireEvent);
+      data.annotationGroup.removeEventListener(
+        'annotationupdate', this.#fireEvent);
+      data.annotationGroup.removeEventListener(
+        'annotationremove', this.#fireEvent);
     }
   }
 
