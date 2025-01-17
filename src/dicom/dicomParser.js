9d1f09f011eed978de0725200a3a9bbe3e1b7d70
ivmartel <ivmartel@gmail.com>
2025-01-17 10:25:18 +0100

    Beta v0.35.0-beta.11

--- src/dicom/dicomParser.js
+++ src/dicom/dicomParser.js
@@ -32,7 +32,7 @@ import {logger} from '../utils/logger';
  * @returns {string} The version of the library.
  */
 export function getDwvVersion() {
-  return '0.35.0-beta.10';
+  return '0.35.0-beta.11';
 }
 
 /**
