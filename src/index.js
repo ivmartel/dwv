0451d7a4034768d383af312063c6cd65533ca26d
ivmartel <ivmartel@gmail.com>
2025-04-14 17:02:37 +0200

    Remove export

--- src/index.js
+++ src/index.js
@@ -91,7 +91,6 @@ import {DeleteSegmentCommand} from './image/deleteSegmentCommand';
 import {ChangeSegmentColourCommand} from './image/changeSegmentColourCommand';
 import {MaskSegmentHelper} from './image/maskSegmentHelper';
 import {MaskSegmentViewHelper} from './image/maskSegmentViewHelper';
-import {LabelingThread} from './image/labelingThread';
 // math
 import {Point, Point2D, Point3D} from './math/point';
 import {Vector3D} from './math/vector';
@@ -142,7 +141,6 @@ export {
   PositionHelper,
   MaskSegmentHelper,
   MaskSegmentViewHelper,
-  LabelingThread,
   DeleteSegmentCommand,
   ChangeSegmentColourCommand,
   ToolboxController,
