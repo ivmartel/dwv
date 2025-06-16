f9ea035b610793552e6469a549aea3e3dae93885
ivmartel <ivmartel@gmail.com>
2025-06-16 16:08:25 +0200

    Remove web target, simplify config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,28 +5,12 @@ import {webpackCommon} from './webpack.common.js';
 export default merge(webpackCommon, {
   mode: 'production',
   devtool: 'source-map',
-  // using 'web' target since it creates a bundle
-  //   that does not reference 'Document', as opposed to
-  //   undefined target (that then uses browserlist)
-  target: 'web',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltIns: false
-                }
-              ]
-            ]
-          }
-        }
+        use: 'babel-loader'
       }
     ]
   },
