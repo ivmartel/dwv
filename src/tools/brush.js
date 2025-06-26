6f66774c249530a45604686fd7d1b29ce84c942a
Joseph Roorda <joseph.roorda@prolucid.ca>
2025-06-26 10:25:36 -0400

    Fix linting

--- src/tools/brush.js
+++ src/tools/brush.js
@@ -1045,8 +1045,8 @@ export class Brush extends EventTarget {
       sliceMeta
     );
 
-    // this.#displayMask causes the position to get reset, 
-    // so we have to restore it or we may not be drawing on 
+    // this.#displayMask causes the position to get reset,
+    // so we have to restore it or we may not be drawing on
     // the correct slice.
     viewController.setCurrentPosition(savedPosition);
     maskVc.setCurrentPosition(savedPosition);
