37cbe35584ccddbfb13c518437814b6fe5bfd307
ivmartel <ivmartel@gmail.com>
2025-07-23 16:08:32 +0200

    Unroll bilinear loop

--- src/image/resamplingFilter.js
+++ src/image/resamplingFilter.js
@@ -1,35 +1,6 @@
 
 import {Matrix33, BIG_EPSILON} from '../math/matrix.js';
 
-/**
- * Get the first value of an array.
- *
- * @param {number[]} values The input array.
- * @returns {number} The first value.
- */
-function get0(values) {
-  return values[0];
-};
-/**
- * Get the second value of an array.
- *
- * @param {number[]} values The input array.
- * @returns {number} The second value.
- */
-function get1(values) {
-  return values[1];
-};
-/**
- * Get a weighting func.
- *
- * @param {number} weight The weight.
- * @returns {Function} The weight function.
- */
-function getWeightFunc(weight) {
-  return (values) =>
-    (values[1] * weight) + (values[0] * (1 - weight));
-}
-
 export class ResamplingFilter {
   /**
    * Simple bilinear sampling function.
@@ -42,61 +13,73 @@ export class ResamplingFilter {
    * @returns {number} The sampled value.
    */
   #bilinearSample(buffer, unitVectors, size, point) {
-    const q0IndexX = Math.floor(point[0]);
-    const q0IndexY = Math.floor(point[1]);
-    const q0IndexZ = Math.floor(point[2]);
+    // base point
+    const q0x = Math.floor(point[0]);
+    const q0y = Math.floor(point[1]);
+    const q0z = Math.floor(point[2]);
 
-    const wx = Math.abs(point[0] - q0IndexX);
-    const wy = Math.abs(point[1] - q0IndexY);
-    const wz = Math.abs(point[2] - q0IndexZ);
+    // bounding points indices
+    const x0 = q0x < 0 ? 0 : q0x;
+    const x1 = q0x + 1 >= size[0] ? q0x : q0x + 1;
+    const y0 = q0y < 0 ? 0 : q0y;
+    const y1 = q0y + 1 >= size[1] ? q0y : q0y + 1;
+    const z0 = q0z < 0 ? 0 : q0z;
+    const z1 = q0z + 1 >= size[2] ? q0z : q0z + 1;
 
-    let getYMean = getWeightFunc(wz);
-    if (q0IndexZ < 0) {
-      getYMean = get1;
-    } else if (q0IndexZ + 1 >= size[2]) {
-      getYMean = get0;
-    }
+    // bounding points offsets
+    const x0v = x0 * unitVectors[0];
+    const x1v = x1 * unitVectors[0];
+    const y0v = y0 * unitVectors[1];
+    const y1v = y1 * unitVectors[1];
+    const z0v = z0 * unitVectors[2];
+    const z1v = z1 * unitVectors[2];
+    const off000 = x0v + y0v + z0v;
+    const off001 = x0v + y0v + z1v;
+    const off010 = x0v + y1v + z0v;
+    const off011 = x0v + y1v + z1v;
+    const off100 = x1v + y0v + z0v;
+    const off101 = x1v + y0v + z1v;
+    const off110 = x1v + y1v + z0v;
+    const off111 = x1v + y1v + z1v;
 
-    let getXMean = getWeightFunc(wy);
-    if (q0IndexY < 0) {
-      getXMean = get1;
-    } else if (q0IndexY + 1 >= size[1]) {
-      getXMean = get0;
-    }
+    // bounding points values
+    const x0ok = x0 >= 0 && x0 < size[0];
+    const x1ok = x1 >= 0 && x1 < size[0];
+    const y0ok = y0 >= 0 && y0 < size[1];
+    const y1ok = y1 >= 0 && y1 < size[1];
+    const z0ok = z0 >= 0 && z0 < size[2];
+    const z1ok = z1 >= 0 && z1 < size[2];
+    const v000 = (x0ok && y0ok && z0ok) ? buffer[off000] : 0;
+    const v001 = (x0ok && y1ok && z1ok) ? buffer[off001] : 0;
+    const v010 = (x0ok && y0ok && z0ok) ? buffer[off010] : 0;
+    const v011 = (x0ok && y1ok && z1ok) ? buffer[off011] : 0;
+    const v100 = (x1ok && y0ok && z0ok) ? buffer[off100] : 0;
+    const v101 = (x1ok && y1ok && z1ok) ? buffer[off101] : 0;
+    const v110 = (x1ok && y0ok && z0ok) ? buffer[off110] : 0;
+    const v111 = (x1ok && y1ok && z1ok) ? buffer[off111] : 0;
 
-    let getMean = getWeightFunc(wx);
-    if (q0IndexX < 0) {
-      getMean = get1;
-    } else if (q0IndexX + 1 >= size[0]) {
-      getMean = get0;
-    }
+    // interpolation weights
+    const wx0 = Math.abs(point[0] - q0x);
+    const wy0 = Math.abs(point[1] - q0y);
+    const wz0 = Math.abs(point[2] - q0z);
+    const wx1 = 1 - wx0;
+    const wy1 = 1 - wy0;
+    const wz1 = 1 - wz0;
+    // per point
+    const w000 = wx1 * wy1 * wz1;
+    const w001 = wx1 * wy1 * wz0;
+    const w010 = wx1 * wy0 * wz1;
+    const w011 = wx1 * wy0 * wz0;
+    const w100 = wx0 * wy1 * wz1;
+    const w101 = wx0 * wy1 * wz0;
+    const w110 = wx0 * wy0 * wz1;
+    const w111 = wx0 * wy0 * wz0;
 
-    let zIndex;
-    let offX, offXY;
-    const xMeans = [0.0, 0.0];
-    let yMeans, zValues;
-    for (let x = 0; x < 2; x++) {
-      offX = (q0IndexX + x) * unitVectors[0];
-      yMeans = [0.0, 0.0];
-      for (let y = 0; y < 2; y++) {
-        offXY = offX + (q0IndexY + y) * unitVectors[1];
-        zValues = [0.0, 0.0];
-        for (let z = 0; z < 2; z++) {
-          zIndex = q0IndexZ + z;
-          if (
-            zIndex >= 0 && zIndex < size[2]
-          ) {
-            zValues[z] = buffer[offXY + zIndex * unitVectors[2]];
-          } else {
-            zValues[z] = 0;
-          }
-        }
-        yMeans[y] = getYMean(zValues);
-      }
-      xMeans[x] = getXMean(yMeans);
-    }
-
-    return getMean(xMeans);
+    // weighted sum
+    return (
+      v000 * w000 + v001 * w001 + v010 * w010 + v011 * w011 +
+      v100 * w100 + v101 * w101 + v110 * w110 + v111 * w111
+    );
   }
 
   /**
