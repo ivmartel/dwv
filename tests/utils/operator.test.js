36b14c0f4dafa2f3f796828d48070f2526cc4608
ivmartel <ivmartel@gmail.com>
2025-09-03 11:31:22 +0200

    Add suffix param to merge, treat id as string

--- tests/utils/operator.test.js
+++ tests/utils/operator.test.js
@@ -13,8 +13,8 @@ QUnit.module('utils');
  * @function module:tests/utils~mergeobjects
  */
 QUnit.test('Merge objects', function (assert) {
-  const obj001 = {id: {value: [0]}, a: {value: [1]}, b: {value: [1]}};
-  const obj002 = {id: {value: [1]}, a: {value: [1]}, b: {value: [2]}};
+  const obj001 = {id: {value: ['0']}, a: {value: [1]}, b: {value: [1]}};
+  const obj002 = {id: {value: ['1']}, a: {value: [1]}, b: {value: [2]}};
 
   // bad id key
   const fbad01 = function () {
@@ -29,7 +29,7 @@ QUnit.test('Merge objects', function (assert) {
   assert.throws(fbad02, 'merge bad value key');
 
   // same id
-  const obj003 = {id: {value: [0]}, a: {value: [1]}, b: {value: [2]}};
+  const obj003 = {id: {value: ['0']}, a: {value: [1]}, b: {value: [2]}};
   const fbad03 = function () {
     mergeObjects(obj001, obj003, 'id', 'value');
   };
@@ -37,7 +37,7 @@ QUnit.test('Merge objects', function (assert) {
 
   // test #00: simple
   const ref00 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: [1]},
     b: {value: {
       0: [1],
@@ -51,10 +51,10 @@ QUnit.test('Merge objects', function (assert) {
     'merge objects 00');
 
   // test #01: array values
-  const obj011 = {id: {value: [0]}, a: {value: [1]}, b: {value: [1]}};
-  const obj012 = {id: {value: [1]}, a: {value: [1]}, b: {value: [2]}};
+  const obj011 = {id: {value: ['0']}, a: {value: [1]}, b: {value: [1]}};
+  const obj012 = {id: {value: ['1']}, a: {value: [1]}, b: {value: [2]}};
   const ref01 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: [1]},
     b: {value: {
       0: [1],
@@ -69,16 +69,16 @@ QUnit.test('Merge objects', function (assert) {
 
   // test #02: merge with already merged
   const obj021 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: [1]},
     b: {value: {
       0: [1],
       1: [2]
     }}
   };
-  const obj022 = {id: {value: [2]}, a: {value: [1]}, b: {value: [2]}};
+  const obj022 = {id: {value: ['2']}, a: {value: [1]}, b: {value: [2]}};
   const ref02 = {
-    id: {value: [0, 1, 2], merged: true},
+    id: {value: ['0', '1', '2'], merged: true},
     a: {value: [1]},
     b: {value: {
       0: [1],
@@ -94,16 +94,16 @@ QUnit.test('Merge objects', function (assert) {
 
   // test #03: merge with already merged that contains a repeated value
   const obj031 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: [1]},
     b: {value: {
       0: [1],
       1: [2]
     }}
   };
-  const obj032 = {id: {value: [2]}, a: {value: [2]}, b: {value: [3]}};
+  const obj032 = {id: {value: ['2']}, a: {value: [2]}, b: {value: [3]}};
   const ref03 = {
-    id: {value: [0, 1, 2], merged: true},
+    id: {value: ['0', '1', '2'], merged: true},
     a: {value: {
       0: [1],
       1: [1],
@@ -122,10 +122,10 @@ QUnit.test('Merge objects', function (assert) {
     'merge objects 03');
 
   // test #10: missing key in first object
-  const obj101 = {id: {value: [0]}, a: {value: [1]}};
-  const obj102 = {id: {value: [1]}, a: {value: [2]}, b: {value: [1]}};
+  const obj101 = {id: {value: ['0']}, a: {value: [1]}};
+  const obj102 = {id: {value: ['1']}, a: {value: [2]}, b: {value: [1]}};
   const ref10 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: {
       0: [1],
       1: [2]
@@ -142,10 +142,10 @@ QUnit.test('Merge objects', function (assert) {
     'merge objects 10');
 
   // test #10: missing key in second object
-  const obj111 = {id: {value: [0]}, a: {value: [1]}, b: {value: [1]}};
-  const obj112 = {id: {value: [1]}, a: {value: [2]}};
+  const obj111 = {id: {value: ['0']}, a: {value: [1]}, b: {value: [1]}};
+  const obj112 = {id: {value: ['1']}, a: {value: [2]}};
   const ref11 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: {
       0: [1],
       1: [2]
@@ -162,10 +162,10 @@ QUnit.test('Merge objects', function (assert) {
     'merge objects 11');
 
   // test #12: missing value in first object
-  const obj121 = {id: {value: [0]}, a: {value: [1]}, b: {}};
-  const obj122 = {id: {value: [1]}, a: {value: [2]}, b: {value: [1]}};
+  const obj121 = {id: {value: ['0']}, a: {value: [1]}, b: {}};
+  const obj122 = {id: {value: ['1']}, a: {value: [2]}, b: {value: [1]}};
   const ref12 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: {
       0: [1],
       1: [2]
@@ -182,10 +182,10 @@ QUnit.test('Merge objects', function (assert) {
     'merge objects 12');
 
   // test #13: missing value in second object
-  const obj131 = {id: {value: [0]}, a: {value: [1]}, b: {value: [1]}};
-  const obj132 = {id: {value: [1]}, a: {value: [2]}, b: {}};
+  const obj131 = {id: {value: ['0']}, a: {value: [1]}, b: {value: [1]}};
+  const obj132 = {id: {value: ['1']}, a: {value: [2]}, b: {}};
   const ref13 = {
-    id: {value: [0, 1], merged: true},
+    id: {value: ['0', '1'], merged: true},
     a: {value: {
       0: [1],
       1: [2]
