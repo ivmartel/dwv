e8734f9243d3f8d4d2c087dee0bade06e8285766
ivmartel <ivmartel@gmail.com>
2026-03-25 18:03:24 +0100

    Add array cast, fixes #2125

--- src/decoders/pdfjs/jpeg2000.worker.js
+++ src/decoders/pdfjs/jpeg2000.worker.js
@@ -9,8 +9,24 @@ self.addEventListener('message', function (event) {
   // decode DICOM buffer
   const decoder = new JpxImage();
   decoder.parse(event.data.buffer);
-  // post decoded data
-  const res = decoder.tiles[0].items;
+  // decoded buffer
+  const decodedBuffer = decoder.tiles[0].items;
+  // cast to proper array
+  let res = null;
+  if (event.data.meta.bitsAllocated === 8) {
+    if (event.data.meta.isSigned) {
+      res = new Int8Array(decodedBuffer);
+    } else {
+      res = new Uint8Array(decodedBuffer);
+    }
+  } else if (event.data.meta.bitsAllocated === 16) {
+    if (event.data.meta.isSigned) {
+      res = new Int16Array(decodedBuffer);
+    } else {
+      res = new Uint16Array(decodedBuffer);
+    }
+  }
+  // post result
   self.postMessage([res]);
 
 }, false);
--- src/decoders/pdfjs/jpegbaseline.worker.js
+++ src/decoders/pdfjs/jpegbaseline.worker.js
@@ -9,8 +9,24 @@ self.addEventListener('message', function (event) {
   // decode DICOM buffer
   const decoder = new JpegImage();
   decoder.parse(event.data.buffer);
-  // post decoded data
-  const res = decoder.getData(decoder.width, decoder.height);
+  // decoded buffer
+  const decodedBuffer = decoder.getData(decoder.width, decoder.height);
+  // cast to proper array
+  let res = null;
+  if (event.data.meta.bitsAllocated === 8) {
+    if (event.data.meta.isSigned) {
+      res = new Int8Array(decodedBuffer);
+    } else {
+      res = new Uint8Array(decodedBuffer);
+    }
+  } else if (event.data.meta.bitsAllocated === 16) {
+    if (event.data.meta.isSigned) {
+      res = new Int16Array(decodedBuffer);
+    } else {
+      res = new Uint16Array(decodedBuffer);
+    }
+  }
+  // post result
   self.postMessage([res]);
 
 }, false);
