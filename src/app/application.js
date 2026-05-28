e03f19173ef1d4223460ea8aa9b27782cfb61dfe
ivmartel <ivmartel@gmail.com>
2026-05-28 11:06:27 +0200

    Fix lint

--- src/app/application.js
+++ src/app/application.js
@@ -1983,8 +1983,8 @@ export class App extends EventTarget {
    */
   #onloadstart = (event) => {
     // create info data
-    if (typeof this.#options.overlayConfig !== 'undefined'
-      && Object.keys(this.#infoDatas).length === 0
+    if (typeof this.#options.overlayConfig !== 'undefined' &&
+      Object.keys(this.#infoDatas).length === 0
     ) {
       this.#infoDatas[event.detail.dataid] = new InfoData(
         this, event.detail.dataid, this.#options.overlayConfig);
